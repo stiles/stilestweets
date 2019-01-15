5dd7bcf0f6b4132624a6e68e528052e8daf43d32
Matt Stiles <mattstiles@gmail.com>
2019-01-15 05:20:11 -0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16648,
-	"created_at": "Tue Jan 15 12:20:13 +0000 2019",
+	"tweets": 16650,
+	"created_at": "Tue Jan 15 13:20:11 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -167,6 +167,283 @@ Grailbird.data.tweets_2019_01 =
 		"contributors": null,
 		"is_quote_status": false,
 		"retweet_count": 8,
+		"favorite_count": 33,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Siri, what’s the opposite of making America great again? https://t.co/qeiYAzKvoJ"
+	},
+	{
+		"created_at": "Mon Jan 14 22:30:03 +0000 2019",
+		"id": 1084940736485744600,
+		"id_str": "1084940736485744644",
+		"full_text": "The @latimes is looking for a reporter to cover one of the most dynamic regions of L.A: the San Gabriel Valley.  https://t.co/R8dshYPs5U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						4,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/R8dshYPs5U",
+					"expanded_url": "https://www.journalismjobs.com/1650016-metro-reporter-san-gabriel-valley-los-angeles-times",
+					"display_url": "journalismjobs.com/1650016-metro-…",
+					"indices": [
+						113,
+						136
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 19074004,
+			"id_str": "19074004",
+			"name": "Hector Becerra",
+			"screen_name": "hbecerraLATimes",
+			"location": "Los Angeles",
+			"description": "City Editor, Los Angeles Times. L.A born and raised. Story tips welcome Hector.becerra@latimes.com Twitter: @hbecerralatimes",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 4602,
+			"friends_count": 401,
+			"listed_count": 190,
+			"created_at": "Fri Jan 16 16:35:59 +0000 2009",
+			"favourites_count": 3828,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 23949,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1052455565367144450/0tEOXg9N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1052455565367144450/0tEOXg9N_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 66,
+		"favorite_count": 77,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The @latimes is looking for a reporter to cover one of the most dynamic regions of L.A: the San Gabriel Valley.  https://t.co/R8dshYPs5U"
+	},
+	{
+		"created_at": "Tue Jan 15 02:57:56 +0000 2019",
+		"id": 1085008152351985700,
+		"id_str": "1085008152351985665",
+		"full_text": "Siri, what’s the opposite of making America great again? https://t.co/qeiYAzKvoJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			56
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1085008147066941400,
+					"id_str": "1085008147066941440",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw65KUBUcAA3hrF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw65KUBUcAA3hrF.jpg",
+					"url": "https://t.co/qeiYAzKvoJ",
+					"display_url": "pic.twitter.com/qeiYAzKvoJ",
+					"expanded_url": "https://twitter.com/JasStanford/status/1085008152351985665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 567,
+							"h": 378,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 567,
+							"h": 378,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 567,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1085008147066941400,
+					"id_str": "1085008147066941440",
+					"indices": [
+						57,
+						80
+					],
+					"media_url": "http://pbs.twimg.com/media/Dw65KUBUcAA3hrF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dw65KUBUcAA3hrF.jpg",
+					"url": "https://t.co/qeiYAzKvoJ",
+					"display_url": "pic.twitter.com/qeiYAzKvoJ",
+					"expanded_url": "https://twitter.com/JasStanford/status/1085008152351985665/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 567,
+							"h": 378,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 567,
+							"h": 378,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 567,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 145450074,
+			"id_str": "145450074",
+			"name": "Jason Stanford",
+			"screen_name": "JasStanford",
+			"location": "Austin, TX",
+			"description": "SVP of Global Comms, @HKStrategies. Ex: @mayoradler & @PPAct. Professional guy behind the guy. @bourbonface's +1. RTs are superdelegates.",
+			"url": "https://t.co/DSvjfbL6Al",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DSvjfbL6Al",
+							"expanded_url": "http://bit.ly/2o0yPc8",
+							"display_url": "bit.ly/2o0yPc8",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7284,
+			"friends_count": 3775,
+			"listed_count": 272,
+			"created_at": "Wed May 19 00:53:36 +0000 2010",
+			"favourites_count": 25392,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 40931,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "022330",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1059779921260802053/5QDiyOEw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1059779921260802053/5QDiyOEw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/145450074/1513385764",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "C0DFEC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 8,
 		"favorite_count": 32,
 		"favorited": false,
 		"retweeted": true,
