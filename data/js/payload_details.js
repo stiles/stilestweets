a3322f2a41b4b29500f15a1a7b95d80a5535d10c
Matt Stiles <mattstiles@gmail.com>
2019-01-24 14:20:13 -0800

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16869,
-	"created_at": "Thu Jan 24 21:20:16 +0000 2019",
+	"tweets": 16872,
+	"created_at": "Thu Jan 24 22:20:12 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -55,6 +55,618 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
+			"followers_count": 141523,
+			"friends_count": 2486,
+			"listed_count": 4196,
+			"created_at": "Wed Aug 26 15:05:15 +0000 2009",
+			"favourites_count": 126639,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 121718,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "383838",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1002554410717757442/ywgIlN3N_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1002554410717757442/ywgIlN3N_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/69004966/1484268255",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "E0E0E0",
+			"profile_text_color": "4F4E4E",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "regular"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1088449615425200100,
+		"quoted_status_id_str": "1088449615425200129",
+		"quoted_status_permalink": {
+			"url": "https://t.co/SV0MThQCPW",
+			"expanded": "https://twitter.com/aterkel/status/1088449615425200129",
+			"display": "twitter.com/aterkel/status…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Jan 24 14:53:05 +0000 2019",
+			"id": 1088449615425200100,
+			"id_str": "1088449615425200129",
+			"full_text": "If you work at a media organization and have job openings, please tweet them out today.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				87
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 15727317,
+				"id_str": "15727317",
+				"name": "Amanda Terkel",
+				"screen_name": "aterkel",
+				"location": "Washington, DC",
+				"description": "Washington Bureau Chief, HuffPost. aterkel at huffpost dot com Sign up for my newsletter: https://t.co/kO4RwneZRf DMs are open.",
+				"url": "https://t.co/xT79unCEKu",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/xT79unCEKu",
+								"expanded_url": "http://www.huffingtonpost.com/amanda-terkel",
+								"display_url": "huffingtonpost.com/amanda-terkel",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/kO4RwneZRf",
+								"expanded_url": "https://aterkel.substack.com/",
+								"display_url": "aterkel.substack.com",
+								"indices": [
+									90,
+									113
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 98504,
+				"friends_count": 5982,
+				"listed_count": 3097,
+				"created_at": "Mon Aug 04 20:24:44 +0000 2008",
+				"favourites_count": 2525,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 46159,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FFC250",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1053320985695449088/7ammrjEF_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1053320985695449088/7ammrjEF_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/15727317/1502639934",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "E0FF92",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1720,
+			"favorite_count": 3854,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 874,
+		"favorite_count": 1215,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The L.A. Times is hiring like crazy — we’ve got 25+ job openings for reporters, editors, visual journalists of all stripes. https://t.co/ew1od88HE4 https://t.co/SV0MThQCPW"
+	},
+	{
+		"created_at": "Thu Jan 24 18:45:55 +0000 2019",
+		"id": 1088508211592740900,
+		"id_str": "1088508211592740865",
+		"full_text": "How colloquial of you, @Microsoft. https://t.co/t0GFJPT2kt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			34
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Microsoft",
+					"name": "Microsoft",
+					"id": 74286565,
+					"id_str": "74286565",
+					"indices": [
+						23,
+						33
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1088508172871000000,
+					"id_str": "1088508172871000064",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/DxsoaoMV4AAy1_k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxsoaoMV4AAy1_k.jpg",
+					"url": "https://t.co/t0GFJPT2kt",
+					"display_url": "pic.twitter.com/t0GFJPT2kt",
+					"expanded_url": "https://twitter.com/stiles/status/1088508211592740865/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 415,
+							"h": 154,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 415,
+							"h": 154,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 415,
+							"h": 154,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1088508172871000000,
+					"id_str": "1088508172871000064",
+					"indices": [
+						35,
+						58
+					],
+					"media_url": "http://pbs.twimg.com/media/DxsoaoMV4AAy1_k.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxsoaoMV4AAy1_k.jpg",
+					"url": "https://t.co/t0GFJPT2kt",
+					"display_url": "pic.twitter.com/t0GFJPT2kt",
+					"expanded_url": "https://twitter.com/stiles/status/1088508211592740865/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 415,
+							"h": 154,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 415,
+							"h": 154,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 415,
+							"h": 154,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "34.0522° N, 118.2437° W",
+			"description": "Reporter @latimes covering LA County. Former Korea correspondent. Stories informed by data analysis, viz, GIS. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10431,
+			"friends_count": 1444,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 4900,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12786,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1548289042",
+			"profile_link_color": "DE9488",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "How colloquial of you, @Microsoft. https://t.co/t0GFJPT2kt"
+	},
+	{
+		"created_at": "Thu Jan 24 17:27:20 +0000 2019",
+		"id": 1088488435894050800,
+		"id_str": "1088488435894050817",
+		"full_text": "For decades, Column One was a showcase for compelling storytelling in the @latimes. After a hiatus, it’s back: https://t.co/i6UXHfQc3l // @skarlamangla has the first installment — a piece about doctors who commute to other states to provide abortions: https://t.co/goSMjgsvK8 https://t.co/UV2QxVuulI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						74,
+						82
+					]
+				},
+				{
+					"screen_name": "skarlamangla",
+					"name": "Soumya",
+					"id": 244701234,
+					"id_str": "244701234",
+					"indices": [
+						138,
+						151
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/i6UXHfQc3l",
+					"expanded_url": "https://lat.ms/2WiwTdQ",
+					"display_url": "lat.ms/2WiwTdQ",
+					"indices": [
+						111,
+						134
+					]
+				},
+				{
+					"url": "https://t.co/goSMjgsvK8",
+					"expanded_url": "https://lat.ms/2WhlWZH",
+					"display_url": "lat.ms/2WhlWZH",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1088488295762321400,
+					"id_str": "1088488295762321408",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DxsWVoMVYAAnTe9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxsWVoMVYAAnTe9.jpg",
+					"url": "https://t.co/UV2QxVuulI",
+					"display_url": "pic.twitter.com/UV2QxVuulI",
+					"expanded_url": "https://twitter.com/stiles/status/1088488435894050817/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1088488295762321400,
+					"id_str": "1088488295762321408",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DxsWVoMVYAAnTe9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DxsWVoMVYAAnTe9.jpg",
+					"url": "https://t.co/UV2QxVuulI",
+					"display_url": "pic.twitter.com/UV2QxVuulI",
+					"expanded_url": "https://twitter.com/stiles/status/1088488435894050817/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "34.0522° N, 118.2437° W",
+			"description": "Reporter @latimes covering LA County. Former Korea correspondent. Stories informed by data analysis, viz, GIS. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10431,
+			"friends_count": 1444,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 4900,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12786,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1548289042",
+			"profile_link_color": "DE9488",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "For decades, Column One was a showcase for compelling storytelling in the @latimes. After a hiatus, it’s back: https://t.co/i6UXHfQc3l // @skarlamangla has the first installment — a piece about doctors who commute to other states to provide abortions: https://t.co/goSMjgsvK8 https://t.co/UV2QxVuulI"
+	},
+	{
+		"created_at": "Thu Jan 24 15:46:19 +0000 2019",
+		"id": 1088463014242578400,
+		"id_str": "1088463014242578432",
+		"full_text": "The L.A. Times is hiring like crazy — we’ve got 25+ job openings for reporters, editors, visual journalists of all stripes. https://t.co/ew1od88HE4 https://t.co/SV0MThQCPW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			147
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ew1od88HE4",
+					"expanded_url": "https://www.journalismjobs.com/job-listings?keywords=Los+Angeles+Times+&industry=&position=&jobType=&salary=&location=&datePosted=&diversity=",
+					"display_url": "journalismjobs.com/job-listings?k…",
+					"indices": [
+						124,
+						147
+					]
+				},
+				{
+					"url": "https://t.co/SV0MThQCPW",
+					"expanded_url": "https://twitter.com/aterkel/status/1088449615425200129",
+					"display_url": "twitter.com/aterkel/status…",
+					"indices": [
+						148,
+						171
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 69004966,
+			"id_str": "69004966",
+			"name": "Matt Pearce 🦅",
+			"screen_name": "mattdpearce",
+			"location": "matt.pearce@latimes.com",
+			"description": "National correspondent for the Los Angeles Times. May the forces of evil become confused on the way to your house.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 141521,
 			"friends_count": 2486,
 			"listed_count": 4200,
