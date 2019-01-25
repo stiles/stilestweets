0d9841ab5aa8b715c7c1320111e6b11862510467
Matt Stiles <mattstiles@gmail.com>
2019-01-25 07:20:20 -0800

    Added 3 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16913,
-	"created_at": "Fri Jan 25 14:20:16 +0000 2019",
+	"tweets": 16916,
+	"created_at": "Fri Jan 25 15:20:19 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_01.js",
 		"year": 2019,
 		"var_name": "tweets_2019_01",
-		"tweet_count": 697,
+		"tweet_count": 700,
 		"month": 1
 	},
 	{
