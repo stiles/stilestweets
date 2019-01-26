c51741f413e4481f6d16c42ce5effd2c93c284ed
Matt Stiles <mattstiles@gmail.com>
2019-01-26 08:20:17 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 16944,
-	"created_at": "Sat Jan 26 05:20:13 +0000 2019",
+	"tweets": 16945,
+	"created_at": "Sat Jan 26 16:20:16 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 26 15:38:00 +0000 2019",
+		"id": 1089185694428381200,
+		"id_str": "1089185694428381184",
+		"full_text": "Trump and his associates had more than 100 contacts with Russians before the inauguration. A visual timeline compiled by @karenyourish and @larrybuch:  https://t.co/c0gQiUWYVo https://t.co/3h2Pme4VPO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			175
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "karenyourish",
+					"name": "Karen Yourish",
+					"id": 18528216,
+					"id_str": "18528216",
+					"indices": [
+						121,
+						134
+					]
+				},
+				{
+					"screen_name": "larrybuch",
+					"name": "Larry Buchanan",
+					"id": 27799349,
+					"id_str": "27799349",
+					"indices": [
+						139,
+						149
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/c0gQiUWYVo",
+					"expanded_url": "https://nyti.ms/2B4k1yU",
+					"display_url": "nyti.ms/2B4k1yU",
+					"indices": [
+						152,
+						175
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1089180839806550000,
+					"id_str": "1089180839806550017",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/Dx2MM_zUwAE3tl0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dx2MM_zUwAE3tl0.jpg",
+					"url": "https://t.co/3h2Pme4VPO",
+					"display_url": "pic.twitter.com/3h2Pme4VPO",
+					"expanded_url": "https://twitter.com/stiles/status/1089185694428381184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 345,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 608,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1038,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1089180839806550000,
+					"id_str": "1089180839806550017",
+					"indices": [
+						176,
+						199
+					],
+					"media_url": "http://pbs.twimg.com/media/Dx2MM_zUwAE3tl0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dx2MM_zUwAE3tl0.jpg",
+					"url": "https://t.co/3h2Pme4VPO",
+					"display_url": "pic.twitter.com/3h2Pme4VPO",
+					"expanded_url": "https://twitter.com/stiles/status/1089185694428381184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 345,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 608,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1038,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "34.0522° N, 118.2437° W",
+			"description": "Reporter @latimes covering LA County. Former Korea correspondent. Stories informed by data analysis, viz, GIS. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10433,
+			"friends_count": 1445,
+			"listed_count": 875,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 4918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12801,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1548443638",
+			"profile_link_color": "51A09E",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Trump and his associates had more than 100 contacts with Russians before the inauguration. A visual timeline compiled by @karenyourish and @larrybuch:  https://t.co/c0gQiUWYVo https://t.co/3h2Pme4VPO"
+	},
+	{
 		"created_at": "Sat Jan 26 04:55:18 +0000 2019",
 		"id": 1089023955216658400,
 		"id_str": "1089023955216658432",
