41dca323e94711a11f02060000feba878ade8092
Matt Stiles <mattstiles@gmail.com>
2019-02-01 08:20:07 -0800

    Added 6 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17478,
-	"created_at": "Fri Feb 01 15:20:06 +0000 2019",
+	"tweets": 17484,
+	"created_at": "Fri Feb 01 16:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -46,6 +46,99 @@ Grailbird.data.tweets_2019_01 =
 				}
 			},
 			"protected": false,
+			"followers_count": 5506,
+			"friends_count": 1710,
+			"listed_count": 247,
+			"created_at": "Wed Aug 17 03:08:56 +0000 2011",
+			"favourites_count": 13997,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 20432,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "131516",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1087142284435501056/CHL7f7Oh_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1087142284435501056/CHL7f7Oh_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/356606989/1533138497",
+			"profile_link_color": "009999",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "EFEFEF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hate crimes in L.A. highest in 10 years, with LBGTQ and African Americans most targeted https://t.co/2DwHTMhw4o"
+	},
+	{
+		"created_at": "Thu Jan 31 22:44:17 +0000 2019",
+		"id": 1091104912941826000,
+		"id_str": "1091104912941826048",
+		"full_text": "Hate crimes in L.A. highest in 10 years, with LBGTQ and African Americans most targeted https://t.co/2DwHTMhw4o",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2DwHTMhw4o",
+					"expanded_url": "https://www.latimes.com/local/lanow/la-me-ln-hate-crime-la-big-cities-20190131-story.html",
+					"display_url": "latimes.com/local/lanow/la…",
+					"indices": [
+						88,
+						111
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 356606989,
+			"id_str": "356606989",
+			"name": "Sarah Parvini 🦅 (سارا)",
+			"screen_name": "sarahparvini",
+			"location": "Los Angeles, CA",
+			"description": "Reporter for @latimes' metro desk covering California's diverse communities and breaking news. Utility player. Iranian American. sarah.parvini@latimes.com",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 5507,
 			"friends_count": 1710,
 			"listed_count": 247,
