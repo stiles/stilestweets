37f5973125184672c3106b28eae61bdd25383ed1
Matt Stiles <mattstiles@gmail.com>
2019-02-11 11:20:08 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 17887,
-	"created_at": "Mon Feb 11 17:20:09 +0000 2019",
+	"tweets": 17888,
+	"created_at": "Mon Feb 11 19:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 11 19:02:37 +0000 2019",
+		"id": 1095035395270631400,
+		"id_str": "1095035395270631424",
+		"full_text": "Jeff Bezos’ journey from private family man to tabloid sensation \"He began getting photographed by paparazzi and appearing publicly with movie stars—and took up with a Hollywood girlfriend.\"  https://t.co/zbk78KjYsQ https://t.co/9YLIRDsXTt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			215
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zbk78KjYsQ",
+					"expanded_url": "https://on.wsj.com/2USnoAe",
+					"display_url": "on.wsj.com/2USnoAe",
+					"indices": [
+						192,
+						215
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1095035130576396300,
+					"id_str": "1095035130576396293",
+					"indices": [
+						216,
+						239
+					],
+					"media_url": "http://pbs.twimg.com/media/DzJYpkaUYAUjRcP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzJYpkaUYAUjRcP.jpg",
+					"url": "https://t.co/9YLIRDsXTt",
+					"display_url": "pic.twitter.com/9YLIRDsXTt",
+					"expanded_url": "https://twitter.com/stiles/status/1095035395270631424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 540,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1274,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 953,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1095035130576396300,
+					"id_str": "1095035130576396293",
+					"indices": [
+						216,
+						239
+					],
+					"media_url": "http://pbs.twimg.com/media/DzJYpkaUYAUjRcP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzJYpkaUYAUjRcP.jpg",
+					"url": "https://t.co/9YLIRDsXTt",
+					"display_url": "pic.twitter.com/9YLIRDsXTt",
+					"expanded_url": "https://twitter.com/stiles/status/1095035395270631424/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 540,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1274,
+							"h": 1012,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 953,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "34.0522° N, 118.2437° W",
+			"description": "Reporter @latimes covering LA County. Former Korea correspondent. Stories informed by data analysis, viz, GIS. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10440,
+			"friends_count": 1405,
+			"listed_count": 875,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5075,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12932,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1549905666",
+			"profile_link_color": "A63144",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Jeff Bezos’ journey from private family man to tabloid sensation \"He began getting photographed by paparazzi and appearing publicly with movie stars—and took up with a Hollywood girlfriend.\"  https://t.co/zbk78KjYsQ https://t.co/9YLIRDsXTt"
+	},
+	{
 		"created_at": "Mon Feb 11 17:10:55 +0000 2019",
 		"id": 1095007285405831200,
 		"id_str": "1095007285405831168",
