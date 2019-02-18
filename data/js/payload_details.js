94a96d375cf12cd69955b2f56f5657200c8aa036
Matt Stiles <mattstiles@gmail.com>
2019-02-17 20:20:10 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18208,
-	"created_at": "Mon Feb 18 03:20:10 +0000 2019",
+	"tweets": 18209,
+	"created_at": "Mon Feb 18 04:20:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Mon Feb 18 03:34:26 +0000 2019",
+		"id": 1097338522992689200,
+		"id_str": "1097338522992689152",
+		"full_text": "This LAX-adjacent ghost town is now ‘priceless coastal real estate’ for a growing list of protected and endangered species. A @LouisSahagun tale of legless lizards and windswept dunes: https://t.co/kKc12I0EUC https://t.co/ciGegfnQ7p",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			208
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LouisSahagun",
+					"name": "Louis Sahagun",
+					"id": 439549149,
+					"id_str": "439549149",
+					"indices": [
+						126,
+						139
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/kKc12I0EUC",
+					"expanded_url": "https://www.latimes.com/local/california/la-me-ln-lax-ghost-town-preserve-20190218-story.html",
+					"display_url": "latimes.com/local/californ…",
+					"indices": [
+						185,
+						208
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1097338318423875600,
+					"id_str": "1097338318423875586",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/DzqHYvwUYAIxzud.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzqHYvwUYAIxzud.jpg",
+					"url": "https://t.co/ciGegfnQ7p",
+					"display_url": "pic.twitter.com/ciGegfnQ7p",
+					"expanded_url": "https://twitter.com/stiles/status/1097338522992689152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1097338318423875600,
+					"id_str": "1097338318423875586",
+					"indices": [
+						209,
+						232
+					],
+					"media_url": "http://pbs.twimg.com/media/DzqHYvwUYAIxzud.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzqHYvwUYAIxzud.jpg",
+					"url": "https://t.co/ciGegfnQ7p",
+					"display_url": "pic.twitter.com/ciGegfnQ7p",
+					"expanded_url": "https://twitter.com/stiles/status/1097338522992689152/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 450,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "34.0522° N, 118.2437° W",
+			"description": "Data journalist @latimes. Telling stories about LA County gov’t with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10456,
+			"friends_count": 1414,
+			"listed_count": 874,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5132,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 12984,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1549905666",
+			"profile_link_color": "A63144",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This LAX-adjacent ghost town is now ‘priceless coastal real estate’ for a growing list of protected and endangered species. A @LouisSahagun tale of legless lizards and windswept dunes: https://t.co/kKc12I0EUC https://t.co/ciGegfnQ7p"
+	},
+	{
 		"created_at": "Mon Feb 18 02:22:41 +0000 2019",
 		"id": 1097320466371342300,
 		"id_str": "1097320466371342336",
