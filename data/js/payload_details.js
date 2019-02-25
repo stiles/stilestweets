a36a163c03206fa2036a8459efcdc8b80e3ba64b
Matt Stiles <mattstiles@gmail.com>
2019-02-25 14:20:20 -0800

    Added 54 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 18601,
-	"created_at": "Mon Feb 25 21:20:16 +0000 2019",
+	"tweets": 18655,
+	"created_at": "Mon Feb 25 22:20:18 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweet_index.js
+++ data/js/tweet_index.js
@@ -3,7 +3,7 @@ var tweet_index = [
 		"file_name": "data/js/tweets/2019_02.js",
 		"year": 2019,
 		"var_name": "tweets_2019_02",
-		"tweet_count": 835,
+		"tweet_count": 889,
 		"month": 2
 	},
 	{
