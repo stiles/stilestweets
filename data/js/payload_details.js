598e95350654bacae05bb99e8141a810e3aacd69
Matt Stiles <mattstiles@gmail.com>
2019-03-01 18:20:05 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19657,
-	"created_at": "Sat Mar 02 01:20:04 +0000 2019",
+	"tweets": 19658,
+	"created_at": "Sat Mar 02 02:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -49,6 +49,101 @@ Grailbird.data.tweets_2019_03 =
 				}
 			},
 			"protected": false,
+			"followers_count": 52823,
+			"friends_count": 876,
+			"listed_count": 1015,
+			"created_at": "Tue Aug 11 18:02:27 +0000 2009",
+			"favourites_count": 16235,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 15833,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/922545046758371328/j7lLoh6Y_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/922545046758371328/j7lLoh6Y_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/64783035/1456476537",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F3FAF0",
+			"profile_text_color": "141414",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 141,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "i know i'm not that young but I'm the same age as Jared Kushner &amp; I can't figure out where he's found the time to get into this much trouble already"
+	},
+	{
+		"created_at": "Sat Mar 02 00:35:19 +0000 2019",
+		"id": 1101642103950532600,
+		"id_str": "1101642103950532608",
+		"full_text": "i know i'm not that young but I'm the same age as Jared Kushner &amp; I can't figure out where he's found the time to get into this much trouble already",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			152
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 64783035,
+			"id_str": "64783035",
+			"name": "jon klassen",
+			"screen_name": "burstofbeaden",
+			"location": "los angeles, ca. 🇨🇦",
+			"description": "book illustration, mostly. I Want My Hat Back, This Is Not My Hat & We Found A Hat. Latest is Square, the sequel to Triangle by Mac Barnett & me",
+			"url": "https://t.co/TJ5jJ9FUqU",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TJ5jJ9FUqU",
+							"expanded_url": "https://www.instagram.com/jonklassen/",
+							"display_url": "instagram.com/jonklassen/",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
 			"followers_count": 52822,
 			"friends_count": 876,
 			"listed_count": 1015,
