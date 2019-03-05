c31a0f45b5c481ce059d4ad81115dce78942b5ea
Matt Stiles <mattstiles@gmail.com>
2019-03-04 17:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19687,
-	"created_at": "Tue Mar 05 00:20:05 +0000 2019",
+	"tweets": 19688,
+	"created_at": "Tue Mar 05 01:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,129 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 05 00:28:41 +0000 2019",
+		"id": 1102727399328886800,
+		"id_str": "1102727598495289344",
+		"full_text": "@stiles @Wendys @kenyanmcduffie @WaltHickey Someone's going to start a rumor that AOC wants the Wendy's removed.",
+		"truncated": false,
+		"display_text_range": [
+			44,
+			112
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stiles",
+					"name": "🌴 Matt Stiles 🌴",
+					"id": 14790314,
+					"id_str": "14790314",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "Wendys",
+					"name": "Wendy's",
+					"id": 59553554,
+					"id_str": "59553554",
+					"indices": [
+						8,
+						15
+					]
+				},
+				{
+					"screen_name": "kenyanmcduffie",
+					"name": "Kenyan McDuffie",
+					"id": 89517777,
+					"id_str": "89517777",
+					"indices": [
+						16,
+						31
+					]
+				},
+				{
+					"screen_name": "WaltHickey",
+					"name": "Walter Hickey",
+					"id": 235700566,
+					"id_str": "235700566",
+					"indices": [
+						32,
+						43
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1102580157980659700,
+		"in_reply_to_status_id_str": "1102580157980659712",
+		"in_reply_to_user_id": 14790314,
+		"in_reply_to_user_id_str": "14790314",
+		"in_reply_to_screen_name": "stiles",
+		"user": {
+			"id": 871778223499206700,
+			"id_str": "871778223499206659",
+			"name": "Billyjoe",
+			"screen_name": "Billeeejo",
+			"location": "",
+			"description": "",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 40,
+			"friends_count": 115,
+			"listed_count": 4,
+			"created_at": "Mon Jun 05 17:18:20 +0000 2017",
+			"favourites_count": 4164,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 6668,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/871780481221742592/gADf_gPO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/871780481221742592/gADf_gPO_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/871778223499206659/1496684337",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "@stiles @Wendys @kenyanmcduffie @WaltHickey Someone's going to start a rumor that AOC wants the Wendy's removed."
+	},
+	{
 		"created_at": "Tue Mar 05 00:17:04 +0000 2019",
 		"id": 1102724672200728600,
 		"id_str": "1102724672200728576",
