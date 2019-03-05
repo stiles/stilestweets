348b105f0fdcd538f85013fb142ef466b127ef06
Matt Stiles <mattstiles@gmail.com>
2019-03-04 23:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19696,
-	"created_at": "Tue Mar 05 06:20:04 +0000 2019",
+	"tweets": 19697,
+	"created_at": "Tue Mar 05 07:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 05 06:52:38 +0000 2019",
+		"id": 1102824222756241400,
+		"id_str": "1102824222756241409",
+		"full_text": "Speaking of the LA car #pursuit, a super famous one — Rodney King's — ended at this Lake View Terrace intersection 28 years ago. George Holliday shot the first viral video from his apartment balcony across the street. The fence in the video is still there. https://t.co/l75H1mNvq3 https://t.co/E6x7zBjyfm",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "pursuit",
+					"indices": [
+						23,
+						31
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/l75H1mNvq3",
+					"expanded_url": "http://bit.ly/2UkyIWb",
+					"display_url": "bit.ly/2UkyIWb",
+					"indices": [
+						257,
+						280
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1102823073944752100,
+					"id_str": "1102823073944752128",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D04DviSU0AAUPas.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D04DviSU0AAUPas.jpg",
+					"url": "https://t.co/E6x7zBjyfm",
+					"display_url": "pic.twitter.com/E6x7zBjyfm",
+					"expanded_url": "https://twitter.com/stiles/status/1102824222756241409/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 749,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1278,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 424,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1102823073944752100,
+					"id_str": "1102823073944752128",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D04DviSU0AAUPas.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D04DviSU0AAUPas.jpg",
+					"url": "https://t.co/E6x7zBjyfm",
+					"display_url": "pic.twitter.com/E6x7zBjyfm",
+					"expanded_url": "https://twitter.com/stiles/status/1102824222756241409/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 749,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1278,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 424,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "🌴 Matt Stiles 🌴",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Data journalist @latimes. Telling stories about LA County gov’t with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10489,
+			"friends_count": 1456,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5269,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13149,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1551769110",
+			"profile_link_color": "C72831",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Speaking of the LA car #pursuit, a super famous one — Rodney King's — ended at this Lake View Terrace intersection 28 years ago. George Holliday shot the first viral video from his apartment balcony across the street. The fence in the video is still there. https://t.co/l75H1mNvq3 https://t.co/E6x7zBjyfm"
+	},
+	{
 		"created_at": "Tue Mar 05 05:37:57 +0000 2019",
 		"id": 1102557101857992700,
 		"id_str": "1102805427807375360",
