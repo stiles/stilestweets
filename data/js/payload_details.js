6064cfc0775e13681d037805328d9f17fe342271
Matt Stiles <mattstiles@gmail.com>
2019-03-10 21:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19717,
-	"created_at": "Sun Mar 10 16:20:05 +0000 2019",
+	"tweets": 19718,
+	"created_at": "Mon Mar 11 04:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,168 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 11 03:56:48 +0000 2019",
+		"id": 1104917416973295600,
+		"id_str": "1104954298905985024",
+		"full_text": "The three hands-on classes I helped teach at #NICAR19 are fully scripted and free for anyone.\n\n📔 First @observablehq notebook \n\nhttps://t.co/E6UeIc7OVY\n\n🐍 First Python notebook \n\nhttps://t.co/gdpD4lgs8S\n\n📈 First graphics app\n\nhttps://t.co/WSjAolxYjA\n\nGo forth. Get nerdy.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NICAR19",
+					"indices": [
+						45,
+						53
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "observablehq",
+					"name": "Observable",
+					"id": 905255756789825500,
+					"id_str": "905255756789825536",
+					"indices": [
+						103,
+						116
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/E6UeIc7OVY",
+					"expanded_url": "https://observablehq.com/@palewire/first-observable-notebook",
+					"display_url": "observablehq.com/@palewire/firs…",
+					"indices": [
+						128,
+						151
+					]
+				},
+				{
+					"url": "https://t.co/gdpD4lgs8S",
+					"expanded_url": "https://www.firstpythonnotebook.org",
+					"display_url": "firstpythonnotebook.org",
+					"indices": [
+						179,
+						202
+					]
+				},
+				{
+					"url": "https://t.co/WSjAolxYjA",
+					"expanded_url": "https://www.firstgraphics.app",
+					"display_url": "firstgraphics.app",
+					"indices": [
+						226,
+						249
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14559196,
+			"id_str": "14559196",
+			"name": "Ben Welsh",
+			"screen_name": "palewire",
+			"location": "Live and direct",
+			"description": "Editor, @LATdatadesk at @latimes · Organizer, https://t.co/lZvqTdDOmO · Archivist, https://t.co/CaO6K276g3 · Send tips to ben.welsh@latimes.com",
+			"url": "https://t.co/kogJ0fJceo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kogJ0fJceo",
+							"expanded_url": "http://palewi.re/who-is-ben-welsh/",
+							"display_url": "palewi.re/who-is-ben-wel…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/lZvqTdDOmO",
+							"expanded_url": "http://www.californiacivicdata.org",
+							"display_url": "californiacivicdata.org",
+							"indices": [
+								46,
+								69
+							]
+						},
+						{
+							"url": "https://t.co/CaO6K276g3",
+							"expanded_url": "http://pastpages.org",
+							"display_url": "pastpages.org",
+							"indices": [
+								83,
+								106
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 8629,
+			"friends_count": 3044,
+			"listed_count": 696,
+			"created_at": "Sun Apr 27 16:10:55 +0000 2008",
+			"favourites_count": 7,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 53849,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14559196/1423764862",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDDDDD",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 17,
+		"favorite_count": 64,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The three hands-on classes I helped teach at #NICAR19 are fully scripted and free for anyone.\n\n📔 First @observablehq notebook \n\nhttps://t.co/E6UeIc7OVY\n\n🐍 First Python notebook \n\nhttps://t.co/gdpD4lgs8S\n\n📈 First graphics app\n\nhttps://t.co/WSjAolxYjA\n\nGo forth. Get nerdy."
+	},
+	{
 		"created_at": "Sun Mar 10 15:38:08 +0000 2019",
 		"id": 1104763659949989900,
 		"id_str": "1104768405570150400",
