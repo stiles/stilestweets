7caf912140002ac3338fa0f6fa76c164bc9c699a
Matt Stiles <mattstiles@gmail.com>
2019-03-14 16:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19736,
-	"created_at": "Thu Mar 14 22:20:06 +0000 2019",
+	"tweets": 19737,
+	"created_at": "Thu Mar 14 23:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Thu Mar 14 22:32:18 +0000 2019",
+		"id": 1106322187760238600,
+		"id_str": "1106322187760238592",
+		"full_text": "1. idea\n2. https://t.co/QtWuZ28P43\n3. https://t.co/DFP34Swel0\n4. win\n\nthanks, @googlechrome and @googledocs. https://t.co/5nNbUMDfHG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			108
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "googlechrome",
+					"name": "Google Chrome",
+					"id": 56505125,
+					"id_str": "56505125",
+					"indices": [
+						78,
+						91
+					]
+				},
+				{
+					"screen_name": "googledocs",
+					"name": "Google Docs",
+					"id": 488683847,
+					"id_str": "488683847",
+					"indices": [
+						96,
+						107
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/QtWuZ28P43",
+					"expanded_url": "http://bit.ly/2T5VzDl",
+					"display_url": "bit.ly/2T5VzDl",
+					"indices": [
+						11,
+						34
+					]
+				},
+				{
+					"url": "https://t.co/DFP34Swel0",
+					"expanded_url": "http://bit.ly/2yGyLCG",
+					"display_url": "bit.ly/2yGyLCG",
+					"indices": [
+						38,
+						61
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1106321717918494700,
+					"id_str": "1106321717918494720",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/D1pxvavU8AApbP5.png",
+					"media_url_https": "https://pbs.twimg.com/media/D1pxvavU8AApbP5.png",
+					"url": "https://t.co/5nNbUMDfHG",
+					"display_url": "pic.twitter.com/5nNbUMDfHG",
+					"expanded_url": "https://twitter.com/stiles/status/1106322187760238592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 583,
+							"h": 303,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 583,
+							"h": 303,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 583,
+							"h": 303,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1106321717918494700,
+					"id_str": "1106321717918494720",
+					"indices": [
+						109,
+						132
+					],
+					"media_url": "http://pbs.twimg.com/media/D1pxvavU8AApbP5.png",
+					"media_url_https": "https://pbs.twimg.com/media/D1pxvavU8AApbP5.png",
+					"url": "https://t.co/5nNbUMDfHG",
+					"display_url": "pic.twitter.com/5nNbUMDfHG",
+					"expanded_url": "https://twitter.com/stiles/status/1106322187760238592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 583,
+							"h": 303,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 583,
+							"h": 303,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 583,
+							"h": 303,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Data journalist @latimes. Telling stories about LA County gov’t with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10544,
+			"friends_count": 1469,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5323,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13198,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1552448061",
+			"profile_link_color": "EE4C06",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "1. idea\n2. https://t.co/QtWuZ28P43\n3. https://t.co/DFP34Swel0\n4. win\n\nthanks, @googlechrome and @googledocs. https://t.co/5nNbUMDfHG"
+	},
+	{
 		"created_at": "Thu Mar 14 22:18:06 +0000 2019",
 		"id": 1105510952592502800,
 		"id_str": "1106318612971700224",
