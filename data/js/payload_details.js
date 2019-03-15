de18232df2068e1c009857e0013ef56c1e321dc8
Matt Stiles <mattstiles@gmail.com>
2019-03-15 15:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 19744,
-	"created_at": "Fri Mar 15 20:20:08 +0000 2019",
+	"tweets": 19745,
+	"created_at": "Fri Mar 15 22:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 15 22:14:17 +0000 2019",
+		"id": 1106680042224930800,
+		"id_str": "1106680042224930816",
+		"full_text": "@dannydb @SurveyMonkey i'm helping it danny! https://t.co/dvYU0ei7Mo",
+		"truncated": false,
+		"display_text_range": [
+			23,
+			44
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dannydb",
+					"name": "Danny DeBelius",
+					"id": 14823197,
+					"id_str": "14823197",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "SurveyMonkey",
+					"name": "SurveyMonkey",
+					"id": 31267104,
+					"id_str": "31267104",
+					"indices": [
+						9,
+						22
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1106679863354617900,
+					"id_str": "1106679863354617857",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/D1u3eOdUcAE0TaT.png",
+					"media_url_https": "https://pbs.twimg.com/media/D1u3eOdUcAE0TaT.png",
+					"url": "https://t.co/dvYU0ei7Mo",
+					"display_url": "pic.twitter.com/dvYU0ei7Mo",
+					"expanded_url": "https://twitter.com/stiles/status/1106680042224930816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 536,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 575,
+							"h": 730,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 575,
+							"h": 730,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1106679863354617900,
+					"id_str": "1106679863354617857",
+					"indices": [
+						45,
+						68
+					],
+					"media_url": "http://pbs.twimg.com/media/D1u3eOdUcAE0TaT.png",
+					"media_url_https": "https://pbs.twimg.com/media/D1u3eOdUcAE0TaT.png",
+					"url": "https://t.co/dvYU0ei7Mo",
+					"display_url": "pic.twitter.com/dvYU0ei7Mo",
+					"expanded_url": "https://twitter.com/stiles/status/1106680042224930816/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 536,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 575,
+							"h": 730,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 575,
+							"h": 730,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1106679620001132500,
+		"in_reply_to_status_id_str": "1106679620001132544",
+		"in_reply_to_user_id": 14823197,
+		"in_reply_to_user_id_str": "14823197",
+		"in_reply_to_screen_name": "dannydb",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Data journalist @latimes. Telling stories about LA County gov’t with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10546,
+			"friends_count": 1470,
+			"listed_count": 879,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5334,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13206,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1552448061",
+			"profile_link_color": "EE4C06",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@dannydb @SurveyMonkey i'm helping it danny! https://t.co/dvYU0ei7Mo"
+	},
+	{
 		"created_at": "Fri Mar 15 19:58:33 +0000 2019",
 		"id": 1106645884358422500,
 		"id_str": "1106645884358422528",
