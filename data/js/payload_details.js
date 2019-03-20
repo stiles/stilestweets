995f683020668dc99dcb5a54564a76f39c2be53f
Matt Stiles <mattstiles@gmail.com>
2019-03-20 07:20:14 -0700

    Added 5 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
-var payload_details =  {
-  "tweets" : 13238,
-  "created_at" : "2019-03-20 06:21:21 +0000",
-  "lang" : "en"
+var payload_details = {
+	"tweets": 13243,
+	"created_at": "Wed Mar 20 14:20:09 +0000 2019",
+	"lang": "en"
 }
