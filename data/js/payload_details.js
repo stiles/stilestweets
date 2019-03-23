488a842f6d010ab0fc2b7be2f9f7565ac1289583
Matt Stiles <mattstiles@gmail.com>
2019-03-22 17:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13266,
-	"created_at": "Fri Mar 22 23:20:06 +0000 2019",
+	"tweets": 13267,
+	"created_at": "Sat Mar 23 00:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Fri Mar 22 23:43:48 +0000 2019",
+		"id": 1109239283536191500,
+		"id_str": "1109239283536191488",
+		"full_text": "Yes. https://t.co/VjFJIKwzBs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			4
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1109239247645495300,
+					"id_str": "1109239247645495296",
+					"indices": [
+						5,
+						28
+					],
+					"media_url": "http://pbs.twimg.com/media/D2TPN_7U0AABhSe.png",
+					"media_url_https": "https://pbs.twimg.com/media/D2TPN_7U0AABhSe.png",
+					"url": "https://t.co/VjFJIKwzBs",
+					"display_url": "pic.twitter.com/VjFJIKwzBs",
+					"expanded_url": "https://twitter.com/stiles/status/1109239283536191488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 112,
+							"h": 112,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 252,
+							"h": 112,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 252,
+							"h": 112,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 252,
+							"h": 112,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1109239247645495300,
+					"id_str": "1109239247645495296",
+					"indices": [
+						5,
+						28
+					],
+					"media_url": "http://pbs.twimg.com/media/D2TPN_7U0AABhSe.png",
+					"media_url_https": "https://pbs.twimg.com/media/D2TPN_7U0AABhSe.png",
+					"url": "https://t.co/VjFJIKwzBs",
+					"display_url": "pic.twitter.com/VjFJIKwzBs",
+					"expanded_url": "https://twitter.com/stiles/status/1109239283536191488/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 112,
+							"h": 112,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 252,
+							"h": 112,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 252,
+							"h": 112,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 252,
+							"h": 112,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Data journalist @latimes. Telling stories about LA County gov’t with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10566,
+			"friends_count": 1488,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5393,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13276,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1553270452",
+			"profile_link_color": "365977",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "Yes. https://t.co/VjFJIKwzBs"
+	},
+	{
 		"created_at": "Fri Mar 22 22:23:22 +0000 2019",
 		"id": 1109219041032310800,
 		"id_str": "1109219041032310784",
