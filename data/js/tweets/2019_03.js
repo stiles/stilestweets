c210b15b33803ad39940f321e0bfbc625cea6c8b
Matt Stiles <mattstiles@gmail.com>
2019-03-25 07:20:09 -0700

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,110 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 25 13:59:19 +0000 2019",
+		"id": 1110179177242591200,
+		"id_str": "1110179356196786176",
+		"full_text": "When gloss meets glitz: Great @DaveMontero read on Beto's weekend in Vegas https://t.co/5IX802ZHwy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			98
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DaveMontero",
+					"name": "David Montero",
+					"id": 48838480,
+					"id_str": "48838480",
+					"indices": [
+						30,
+						42
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5IX802ZHwy",
+					"expanded_url": "https://www.latimes.com/nation/la-na-beto-orourke-nevada-20190325-story.html",
+					"display_url": "latimes.com/nation/la-na-b…",
+					"indices": [
+						75,
+						98
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 294270520,
+			"id_str": "294270520",
+			"name": "Mark Z. Barabak",
+			"screen_name": "markzbarabak",
+			"location": "",
+			"description": "Non-Academy Award winning political writer for the LA Times. Proud California native, happiest when in the Sierra. If I knew the way, I would take you home.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12603,
+			"friends_count": 543,
+			"listed_count": 640,
+			"created_at": "Fri May 06 20:53:16 +0000 2011",
+			"favourites_count": 16563,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13202,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ABB8C2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1095729671348473857/FU3qWttz_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1095729671348473857/FU3qWttz_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/294270520/1551298994",
+			"profile_link_color": "981CEB",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "When gloss meets glitz: Great @DaveMontero read on Beto's weekend in Vegas https://t.co/5IX802ZHwy"
+	},
+	{
 		"created_at": "Mon Mar 25 02:51:13 +0000 2019",
 		"id": 1110011224945623000,
 		"id_str": "1110011224945623040",
