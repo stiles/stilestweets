7b5624a1f2e7789d4890eac2284a7b78c26a6b22
Matt Stiles <mattstiles@gmail.com>
2019-03-25 20:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13283,
-	"created_at": "Mon Mar 25 22:20:05 +0000 2019",
+	"tweets": 13284,
+	"created_at": "Tue Mar 26 03:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 26 02:33:29 +0000 2019",
+		"id": 1110369151254589400,
+		"id_str": "1110369151254589440",
+		"full_text": "n̸̥̐̃̑ó̵̼̟̭̐͘ ̵̘̠̅̇̓c̸͓̈́̈̄o̸͎̼͠l̷̝͗̃̍l̶̲̐̐͘u̴͉̠̐͌s̶̙͎͍̑̑̚i̴͈̖̽ǫ̷̨̧̾͊n̴̛̻̠̽ ̸̲̈́t̷͖̓o̸̡̮͋̒t̶̩̘̯̊̀̃a̶̫̝͕͆̐ĺ̴̝̔ ̷̧̆e̶̬̓͒x̸̯̩̓ŏ̸̦̳n̴̘̊̈́͠ė̴̤̠̜ŗ̷͌̊͠a̵̰̩̝͆̋͛t̵͔̆i̴̭̭͒̓̕o̶̟͍̕ṇ̶͖͉̽̄",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			174
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Data journalist @latimes. Telling stories about LA County gov’t with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10564,
+			"friends_count": 1492,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5405,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13298,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1553270452",
+			"profile_link_color": "365977",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "und",
+		"text": "n̸̥̐̃̑ó̵̼̟̭̐͘ ̵̘̠̅̇̓c̸͓̈́̈̄o̸͎̼͠l̷̝͗̃̍l̶̲̐̐͘u̴͉̠̐͌s̶̙͎͍̑̑̚i̴͈̖̽ǫ̷̨̧̾͊n̴̛̻̠̽ ̸̲̈́t̷͖̓o̸̡̮͋̒t̶̩̘̯̊̀̃a̶̫̝͕͆̐ĺ̴̝̔ ̷̧̆e̶̬̓͒x̸̯̩̓ŏ̸̦̳n̴̘̊̈́͠ė̴̤̠̜ŗ̷͌̊͠a̵̰̩̝͆̋͛t̵͔̆i̴̭̭͒̓̕o̶̟͍̕ṇ̶͖͉̽̄"
+	},
+	{
 		"created_at": "Mon Mar 25 22:16:44 +0000 2019",
 		"id": 1110304534511779800,
 		"id_str": "1110304534511779840",
