f18034e7f7b15fe8cd85ce16cb78b9170f0e01f7
Matt Stiles <mattstiles@gmail.com>
2019-03-26 20:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13289,
-	"created_at": "Wed Mar 27 01:20:05 +0000 2019",
+	"tweets": 13290,
+	"created_at": "Wed Mar 27 03:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 27 03:16:32 +0000 2019",
+		"id": 1110724588650950700,
+		"id_str": "1110742371124178944",
+		"full_text": "We just finalized a significant update to our County codes to more effectively address threats to public health from cigarettes, vaping, and the smoking of cannabis. https://t.co/KqFUb7lgDA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			189
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/KqFUb7lgDA",
+					"expanded_url": "https://www.latimes.com/local/lanow/la-me-smoking-ban-beaches-vape-cannabis-20190326-story.html",
+					"display_url": "latimes.com/local/lanow/la…",
+					"indices": [
+						166,
+						189
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1327401170,
+			"id_str": "1327401170",
+			"name": "Sheila Kuehl",
+			"screen_name": "SheilaKuehl",
+			"location": "",
+			"description": "Los Angeles County Supervisor from the 3rd District. http://t.co/GBomGgZ6Wk",
+			"url": "http://t.co/ysUTgnatDe",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/ysUTgnatDe",
+							"expanded_url": "http://www.supervisorkuehl.com",
+							"display_url": "supervisorkuehl.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "http://t.co/GBomGgZ6Wk",
+							"expanded_url": "http://www.Facebook.com/sjkuehl",
+							"display_url": "Facebook.com/sjkuehl",
+							"indices": [
+								53,
+								75
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 13401,
+			"friends_count": 6500,
+			"listed_count": 249,
+			"created_at": "Thu Apr 04 17:14:12 +0000 2013",
+			"favourites_count": 164,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1792,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1017193216074895360/Lvzk5LML_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1017193216074895360/Lvzk5LML_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1327401170/1401399866",
+			"profile_link_color": "005596",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We just finalized a significant update to our County codes to more effectively address threats to public health from cigarettes, vaping, and the smoking of cannabis. https://t.co/KqFUb7lgDA"
+	},
+	{
 		"created_at": "Wed Mar 27 00:41:07 +0000 2019",
 		"id": 1110689715525550100,
 		"id_str": "1110703257272213504",
