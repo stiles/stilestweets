8adb415b51e966f3d62a775bc9170b5c59e37ca1
Matt Stiles <mattstiles@gmail.com>
2019-04-01 11:20:05 -0700

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 01 17:31:50 +0000 2019",
+		"id": 1112760967958421500,
+		"id_str": "1112769555053441024",
+		"full_text": "Want to earn extra income while helping your community? The U.S. Census Bureau is recruiting thousands of people across the country to assist with the #2020Census count. Apply at https://t.co/sNMkpgx7ZN #MondayMotivation https://t.co/EWtDk96Vqo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			220
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "2020Census",
+					"indices": [
+						151,
+						162
+					]
+				},
+				{
+					"text": "MondayMotivation",
+					"indices": [
+						203,
+						220
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sNMkpgx7ZN",
+					"expanded_url": "http://2020census.gov/en/jobs",
+					"display_url": "2020census.gov/en/jobs",
+					"indices": [
+						179,
+						202
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1112760590957600800,
+					"id_str": "1112760590957600768",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/D3FR3KAU4AAn5NB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3FR3KAU4AAn5NB.jpg",
+					"url": "https://t.co/EWtDk96Vqo",
+					"display_url": "pic.twitter.com/EWtDk96Vqo",
+					"expanded_url": "https://twitter.com/CountyofLA/status/1112760967958421510/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1112760590957600800,
+					"id_str": "1112760590957600768",
+					"indices": [
+						221,
+						244
+					],
+					"media_url": "http://pbs.twimg.com/media/D3FR3KAU4AAn5NB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3FR3KAU4AAn5NB.jpg",
+					"url": "https://t.co/EWtDk96Vqo",
+					"display_url": "pic.twitter.com/EWtDk96Vqo",
+					"expanded_url": "https://twitter.com/CountyofLA/status/1112760967958421510/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1365,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 193771633,
+			"id_str": "193771633",
+			"name": "Los Angeles County",
+			"screen_name": "CountyofLA",
+			"location": "Los Angeles County",
+			"description": "Official account of Los Angeles County",
+			"url": "http://t.co/7Z6JLrLfYn",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/7Z6JLrLfYn",
+							"expanded_url": "http://lacounty.gov",
+							"display_url": "lacounty.gov",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 33953,
+			"friends_count": 4745,
+			"listed_count": 632,
+			"created_at": "Wed Sep 22 17:08:50 +0000 2010",
+			"favourites_count": 4114,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11646,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "89C9FA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/982387985214681088/IPG_rb_A_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/982387985214681088/IPG_rb_A_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/193771633/1553098987",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 6,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Want to earn extra income while helping your community? The U.S. Census Bureau is recruiting thousands of people across the country to assist with the #2020Census count. Apply at https://t.co/sNMkpgx7ZN #MondayMotivation https://t.co/EWtDk96Vqo"
+	},
+	{
 		"created_at": "Mon Apr 01 16:42:13 +0000 2019",
 		"id": 1105283419011977200,
 		"id_str": "1112757067968712704",
