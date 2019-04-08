7ac3c70dbad0c90847b1f8b1a4ed72106e0fc6f3
Matt Stiles <mattstiles@gmail.com>
2019-04-08 16:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13350,
-	"created_at": "Mon Apr 08 22:20:04 +0000 2019",
+	"tweets": 13351,
+	"created_at": "Mon Apr 08 23:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,216 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 08 22:27:29 +0000 2019",
+		"id": 1115380671214571500,
+		"id_str": "1115380671214571520",
+		"full_text": "(The land used to build) Disneyland, 1947 and today https://t.co/he9JrBFpGI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			51
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1115380543309279200,
+					"id_str": "1115380543309279232",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/D3qgsdFU8AAuLmh.png",
+					"media_url_https": "https://pbs.twimg.com/media/D3qgsdFU8AAuLmh.png",
+					"url": "https://t.co/he9JrBFpGI",
+					"display_url": "pic.twitter.com/he9JrBFpGI",
+					"expanded_url": "https://twitter.com/stiles/status/1115380671214571520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 643,
+							"h": 796,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 643,
+							"h": 796,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 549,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1115380543309279200,
+					"id_str": "1115380543309279232",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/D3qgsdFU8AAuLmh.png",
+					"media_url_https": "https://pbs.twimg.com/media/D3qgsdFU8AAuLmh.png",
+					"url": "https://t.co/he9JrBFpGI",
+					"display_url": "pic.twitter.com/he9JrBFpGI",
+					"expanded_url": "https://twitter.com/stiles/status/1115380671214571520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 643,
+							"h": 796,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 643,
+							"h": 796,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 549,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1115380543325999100,
+					"id_str": "1115380543325999104",
+					"indices": [
+						52,
+						75
+					],
+					"media_url": "http://pbs.twimg.com/media/D3qgsdJUEAAw_8o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3qgsdJUEAAw_8o.jpg",
+					"url": "https://t.co/he9JrBFpGI",
+					"display_url": "pic.twitter.com/he9JrBFpGI",
+					"expanded_url": "https://twitter.com/stiles/status/1115380671214571520/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1107,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1120,
+							"h": 1214,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 627,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes. Telling stories about @CountyofLA with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10601,
+			"friends_count": 1516,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5482,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13385,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1554606613",
+			"profile_link_color": "CA7A47",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "(The land used to build) Disneyland, 1947 and today https://t.co/he9JrBFpGI"
+	},
+	{
 		"created_at": "Mon Apr 08 22:12:34 +0000 2019",
 		"id": 1115376916184219600,
 		"id_str": "1115376916184219648",
