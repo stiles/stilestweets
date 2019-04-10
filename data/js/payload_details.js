2d50cdcb358976bbafa1e389c114b73388cdfb72
Matt Stiles <mattstiles@gmail.com>
2019-04-10 15:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13368,
-	"created_at": "Wed Apr 10 20:20:05 +0000 2019",
+	"tweets": 13369,
+	"created_at": "Wed Apr 10 22:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,234 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 10 22:04:59 +0000 2019",
+		"id": 1116099783049928700,
+		"id_str": "1116099783049928707",
+		"full_text": "Brad Pitt wasn’t the coolest thing I saw at Tuesday’s Board of Supervisors meeting ⁦@CountyofLA⁩. ⁦@KBSnews⁩ was in the house, too, for a commemoration of the 100th anniversary of Korea’s provisional government in Shanghai (proclaiming independence from Japan). https://t.co/AlYqTG0MJC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CountyofLA",
+					"name": "Los Angeles County",
+					"id": 193771633,
+					"id_str": "193771633",
+					"indices": [
+						84,
+						95
+					]
+				},
+				{
+					"screen_name": "KBSnews",
+					"name": "KBS 뉴스",
+					"id": 164509454,
+					"id_str": "164509454",
+					"indices": [
+						99,
+						107
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1116099778775924700,
+					"id_str": "1116099778775924736",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/D30u1d_UwAAuiT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D30u1d_UwAAuiT3.jpg",
+					"url": "https://t.co/AlYqTG0MJC",
+					"display_url": "pic.twitter.com/AlYqTG0MJC",
+					"expanded_url": "https://twitter.com/stiles/status/1116099783049928707/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1116099778775924700,
+					"id_str": "1116099778775924736",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/D30u1d_UwAAuiT3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D30u1d_UwAAuiT3.jpg",
+					"url": "https://t.co/AlYqTG0MJC",
+					"display_url": "pic.twitter.com/AlYqTG0MJC",
+					"expanded_url": "https://twitter.com/stiles/status/1116099783049928707/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes. Telling stories about @CountyofLA with words, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10626,
+			"friends_count": 1518,
+			"listed_count": 875,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5523,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13409,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1554921049",
+			"profile_link_color": "CA7A47",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0e260c0b60147001",
+			"url": "https://api.twitter.com/1.1/geo/id/0e260c0b60147001.json",
+			"place_type": "poi",
+			"name": "Los Angeles Times",
+			"full_name": "Los Angeles Times",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.3825,
+							33.930454
+						],
+						[
+							-118.3825,
+							33.930454
+						],
+						[
+							-118.3825,
+							33.930454
+						],
+						[
+							-118.3825,
+							33.930454
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Brad Pitt wasn’t the coolest thing I saw at Tuesday’s Board of Supervisors meeting ⁦@CountyofLA⁩. ⁦@KBSnews⁩ was in the house, too, for a commemoration of the 100th anniversary of Korea’s provisional government in Shanghai (proclaiming independence from Japan). https://t.co/AlYqTG0MJC"
+	},
+	{
 		"created_at": "Wed Apr 10 19:28:12 +0000 2019",
 		"id": 1116059646551179300,
 		"id_str": "1116060328016498688",
@@ -148,7 +376,7 @@ Grailbird.data.tweets_2019_04 =
 			"name": "Jon Schleuss 🦅",
 			"screen_name": "gaufre",
 			"location": "Los Angeles, CA",
-			"description": "Data/graphics/coding journalist @latimes, activist @latguild, educator @USCAnnenberg. From Arkansas. Running for @news_guild president. he/him ✊🏳️‍🌈",
+			"description": "Data/graphics/coding journalist @latimes, activist @latguild, educator @USCAnnenberg. From Arkansas. Running for @news_guild president. he/him ✊🏳️🌈",
 			"url": "https://t.co/H19cgwNPKn",
 			"entities": {
 				"url": {
