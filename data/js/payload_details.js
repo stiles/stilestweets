06ed01d5eafd39d637772693a4fe9e356839c0ad
Matt Stiles <mattstiles@gmail.com>
2019-04-12 07:20:08 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13385,
-	"created_at": "Fri Apr 12 06:20:05 +0000 2019",
+	"tweets": 13386,
+	"created_at": "Fri Apr 12 14:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,332 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 12 13:54:54 +0000 2019",
+		"id": 1115627763300483100,
+		"id_str": "1116701229051043840",
+		"full_text": "Lucky has become code for something awful.\nLucky Cricket: Andrew Zimmern's new Chinese restaurant (dragged by @hooleil) \nLucky Cat: Gordon Ramsay's upcoming \"authentic Asian\" restaurant, with no Asian chef\nLucky Lee's: nutritionist Arielle Haspel's \"clean\" Chinese restaurant https://t.co/McAqc6BTAu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "hooleil",
+					"name": "Soleil Ho",
+					"id": 4857702829,
+					"id_str": "4857702829",
+					"indices": [
+						110,
+						118
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/McAqc6BTAu",
+					"expanded_url": "https://twitter.com/EaterNY/status/1115618693243863041",
+					"display_url": "twitter.com/EaterNY/status…",
+					"indices": [
+						276,
+						299
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21876836,
+			"id_str": "21876836",
+			"name": "Cathy Erway",
+			"screen_name": "cathyerway",
+			"location": "Brooklyn, NY",
+			"description": "Author https://t.co/9U6zGe4LEb & https://t.co/gjPQ54kSPO Host of @SelfEvidentShow & @Heritage_Radio #EatYourWords. Formerly @GimletCreative #WhyWeEat",
+			"url": "http://t.co/I1X57ZeOpX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/I1X57ZeOpX",
+							"expanded_url": "http://noteatingoutinny.com",
+							"display_url": "noteatingoutinny.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/9U6zGe4LEb",
+							"expanded_url": "http://TheFoodofTaiwan.com",
+							"display_url": "TheFoodofTaiwan.com",
+							"indices": [
+								7,
+								30
+							]
+						},
+						{
+							"url": "https://t.co/gjPQ54kSPO",
+							"expanded_url": "http://TheArtofEatingIn.com",
+							"display_url": "TheArtofEatingIn.com",
+							"indices": [
+								33,
+								56
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9190,
+			"friends_count": 4969,
+			"listed_count": 490,
+			"created_at": "Wed Feb 25 15:34:39 +0000 2009",
+			"favourites_count": 7650,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7443,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/518758778720645124/0-R9qGMI_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/518758778720645124/0-R9qGMI_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21876836/1399471976",
+			"profile_link_color": "00A7B3",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1115618693243863000,
+		"quoted_status_id_str": "1115618693243863041",
+		"quoted_status_permalink": {
+			"url": "https://t.co/McAqc6BTAu",
+			"expanded": "https://twitter.com/EaterNY/status/1115618693243863041",
+			"display": "twitter.com/EaterNY/status…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Apr 09 14:13:18 +0000 2019",
+			"id": 1115618693243863000,
+			"id_str": "1115618693243863041",
+			"full_text": "New NYC Chinese restaurant Lucky Lee’s is facing backlash to racist language, like an Instagram post that claimed dishes like lo mein at Chinese-American restaurants leave diners \"bloated and icky\" https://t.co/9rvjzChaef https://t.co/uX2xLtQpWF",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				221
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/9rvjzChaef",
+						"expanded_url": "https://ny.eater.com/2019/4/9/18301861/lucky-lees-chinese-open-controversy-nyc?utm_campaign=ny.eater&utm_content=chorus&utm_medium=social&utm_source=twitter",
+						"display_url": "ny.eater.com/2019/4/9/18301…",
+						"indices": [
+							198,
+							221
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1115618691494948900,
+						"id_str": "1115618691494948864",
+						"indices": [
+							222,
+							245
+						],
+						"media_url": "http://pbs.twimg.com/media/D3t5SgNXsAAw9Fp.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D3t5SgNXsAAw9Fp.jpg",
+						"url": "https://t.co/uX2xLtQpWF",
+						"display_url": "pic.twitter.com/uX2xLtQpWF",
+						"expanded_url": "https://twitter.com/EaterNY/status/1115618693243863041/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1115618691494948900,
+						"id_str": "1115618691494948864",
+						"indices": [
+							222,
+							245
+						],
+						"media_url": "http://pbs.twimg.com/media/D3t5SgNXsAAw9Fp.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D3t5SgNXsAAw9Fp.jpg",
+						"url": "https://t.co/uX2xLtQpWF",
+						"display_url": "pic.twitter.com/uX2xLtQpWF",
+						"expanded_url": "https://twitter.com/EaterNY/status/1115618693243863041/photo/1",
+						"type": "photo",
+						"sizes": {
+							"large": {
+								"w": 2048,
+								"h": 1365,
+								"resize": "fit"
+							},
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 453,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 800,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://www.voxmedia.com\" rel=\"nofollow\">Vox Media</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 9917652,
+				"id_str": "9917652",
+				"name": "Eater NY",
+				"screen_name": "EaterNY",
+				"location": "New York, NY",
+				"description": "Food news and dining guides for New York City.",
+				"url": "https://t.co/fbdsK5O5WP",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/fbdsK5O5WP",
+								"expanded_url": "https://ny.eater.com/2017/4/4/15180666/eater-ny-newsletter",
+								"display_url": "ny.eater.com/2017/4/4/15180…",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 436793,
+				"friends_count": 201,
+				"listed_count": 3824,
+				"created_at": "Sat Nov 03 16:15:19 +0000 2007",
+				"favourites_count": 513,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 43122,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "FDF7F7",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/513844477476081665/tgVPjKD1_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/513844477476081665/tgVPjKD1_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/9917652/1418407817",
+				"profile_link_color": "B40041",
+				"profile_sidebar_border_color": "FDF7F7",
+				"profile_sidebar_fill_color": "FFFFFF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 35,
+			"favorite_count": 94,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 145,
+		"favorite_count": 357,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Lucky has become code for something awful.\nLucky Cricket: Andrew Zimmern's new Chinese restaurant (dragged by @hooleil) \nLucky Cat: Gordon Ramsay's upcoming \"authentic Asian\" restaurant, with no Asian chef\nLucky Lee's: nutritionist Arielle Haspel's \"clean\" Chinese restaurant https://t.co/McAqc6BTAu"
+	},
+	{
 		"created_at": "Fri Apr 12 06:00:51 +0000 2019",
 		"id": 1115982533177610200,
 		"id_str": "1116581929267019779",
