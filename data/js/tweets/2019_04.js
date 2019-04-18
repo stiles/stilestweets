f3c2b23506c6ba5e787d662d353a596dd35dc2fe
Matt Stiles <mattstiles@gmail.com>
2019-04-18 10:20:07 -0700

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Thu Apr 18 17:02:27 +0000 2019",
+		"id": 1118922753208270800,
+		"id_str": "1118922753208270849",
+		"full_text": "I'm slowly creating a massive database of area street trees using public records requests and open data from dozens of Los Angeles County cities. Today I received a list of 13,000 from Rancho Palos Verdes. Thanks, @cityofrpv! https://t.co/uKNmLgfVUD",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			225
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CityofRPV",
+					"name": "Rancho Palos Verdes",
+					"id": 70765141,
+					"id_str": "70765141",
+					"indices": [
+						214,
+						224
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1118921621949956100,
+					"id_str": "1118921621949956096",
+					"indices": [
+						226,
+						249
+					],
+					"media_url": "http://pbs.twimg.com/media/D4c1SW_UwAA69J8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4c1SW_UwAA69J8.jpg",
+					"url": "https://t.co/uKNmLgfVUD",
+					"display_url": "pic.twitter.com/uKNmLgfVUD",
+					"expanded_url": "https://twitter.com/stiles/status/1118922753208270849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 446,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1027,
+							"h": 673,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1027,
+							"h": 673,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1118921621949956100,
+					"id_str": "1118921621949956096",
+					"indices": [
+						226,
+						249
+					],
+					"media_url": "http://pbs.twimg.com/media/D4c1SW_UwAA69J8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D4c1SW_UwAA69J8.jpg",
+					"url": "https://t.co/uKNmLgfVUD",
+					"display_url": "pic.twitter.com/uKNmLgfVUD",
+					"expanded_url": "https://twitter.com/stiles/status/1118922753208270849/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 446,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1027,
+							"h": 673,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1027,
+							"h": 673,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes. Covering the @CountyofLA beat with words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10632,
+			"friends_count": 1533,
+			"listed_count": 875,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5566,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13469,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1555392722",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I'm slowly creating a massive database of area street trees using public records requests and open data from dozens of Los Angeles County cities. Today I received a list of 13,000 from Rancho Palos Verdes. Thanks, @cityofrpv! https://t.co/uKNmLgfVUD"
+	},
+	{
 		"created_at": "Thu Apr 18 15:31:54 +0000 2019",
 		"id": 1118866058767679500,
 		"id_str": "1118899965701414913",
