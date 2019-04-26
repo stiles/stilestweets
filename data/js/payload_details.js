30c2f4b6bd80d1a64566f81eae372194057a19dd
Matt Stiles <mattstiles@gmail.com>
2019-04-25 18:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13487,
-	"created_at": "Thu Apr 25 23:20:06 +0000 2019",
+	"tweets": 13488,
+	"created_at": "Fri Apr 26 01:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Fri Apr 26 00:20:05 +0000 2019",
+		"id": 1121184412585082900,
+		"id_str": "1121569602494967809",
+		"full_text": "5 jobs I've had: \n\n1) =INDEX MATCH\n2) =VLOOKUP\n3) =COUNT\n4) TEXTING NELLY\n5) =IF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			80
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://prod2.sprinklr.com\" rel=\"nofollow\">Sprinklr Publisher</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15934026,
+			"id_str": "15934026",
+			"name": "Microsoft Excel",
+			"screen_name": "msexcel",
+			"location": "Redmond, WA",
+			"description": "Master your data to achieve more with Microsoft Excel.",
+			"url": "http://t.co/HcFOqOg4FX",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/HcFOqOg4FX",
+							"expanded_url": "http://office.microsoft.com/excel",
+							"display_url": "office.microsoft.com/excel",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 69559,
+			"friends_count": 180,
+			"listed_count": 1042,
+			"created_at": "Thu Aug 21 17:27:45 +0000 2008",
+			"favourites_count": 1729,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 60527,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "4A913C",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/876908558532595712/nnq9YGg4_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/876908558532595712/nnq9YGg4_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15934026/1507677923",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3347,
+		"favorite_count": 11478,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "5 jobs I've had: \n\n1) =INDEX MATCH\n2) =VLOOKUP\n3) =COUNT\n4) TEXTING NELLY\n5) =IF"
+	},
+	{
 		"created_at": "Thu Apr 25 23:14:34 +0000 2019",
 		"id": 1121449100098957300,
 		"id_str": "1121553114631294976",
