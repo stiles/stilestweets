f32fea16d49cb37948270ae3075762e0200c11a1
Matt Stiles <mattstiles@gmail.com>
2019-04-29 08:20:17 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13511,
-	"created_at": "Mon Apr 29 03:20:07 +0000 2019",
+	"tweets": 13512,
+	"created_at": "Mon Apr 29 15:20:10 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 29 14:51:37 +0000 2019",
+		"id": 1122862707647160300,
+		"id_str": "1122876094661533696",
+		"full_text": "Have a little fun with this map and see how common your birthday is: https://t.co/mrYyY1wA4w via @stiles",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			104
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "stiles",
+					"name": "Matt Stiles",
+					"id": 14790314,
+					"id_str": "14790314",
+					"indices": [
+						97,
+						104
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/mrYyY1wA4w",
+					"expanded_url": "http://thedailyviz.com/2016/09/17/how-common-is-your-birthday-dailyviz/",
+					"display_url": "thedailyviz.com/2016/09/17/how…",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1027597160681025500,
+			"id_str": "1027597160681025536",
+			"name": "Susan M. Giusti",
+			"screen_name": "LibrarianSusanG",
+			"location": "",
+			"description": "The ABCs.. Aglianico 🍷 Biblioteca 📚 Caponare l'ancor 🐱🐾  \nOpinions are my own and not the views of my employer.",
+			"url": "https://t.co/m0kNsPLf7v",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/m0kNsPLf7v",
+							"expanded_url": "https://commonwealthlaw.widener.edu/current-students/law-library/",
+							"display_url": "commonwealthlaw.widener.edu/current-studen…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 45,
+			"friends_count": 120,
+			"listed_count": 0,
+			"created_at": "Thu Aug 09 16:47:09 +0000 2018",
+			"favourites_count": 25,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 257,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1043226705433772033/P2H-ysRp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1043226705433772033/P2H-ysRp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1027597160681025536/1533913776",
+			"profile_link_color": "FF691F",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Have a little fun with this map and see how common your birthday is: https://t.co/mrYyY1wA4w via @stiles"
+	},
+	{
 		"created_at": "Mon Apr 29 02:52:08 +0000 2019",
 		"id": 1122695031213551600,
 		"id_str": "1122695031213551617",
