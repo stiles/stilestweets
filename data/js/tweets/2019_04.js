99c57ea31c7e49d9427c3614b07e8858e29776f3
Matt Stiles <mattstiles@gmail.com>
2019-04-28 19:20:08 -0700

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Mon Apr 29 02:02:04 +0000 2019",
+		"id": 1122255864507682800,
+		"id_str": "1122682430681280513",
+		"full_text": "I AM running for the LA County Board of Supervisors in your 2nd district! I need your help to get elected. My goal is to impact our community with opportunities. I am willing to go against the status quo and earn my seat at the table but I can’t do it without your help! https://t.co/D7araMKYru",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1122255858396618800,
+					"id_str": "1122255858396618752",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/D5MNwdrU4AAZNT7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5MNwdrU4AAZNT7.jpg",
+					"url": "https://t.co/D7araMKYru",
+					"display_url": "pic.twitter.com/D7araMKYru",
+					"expanded_url": "https://twitter.com/SharisRhodes/status/1122255864507682818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1224,
+							"h": 1695,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 867,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 491,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1122255858396618800,
+					"id_str": "1122255858396618752",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/D5MNwdrU4AAZNT7.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5MNwdrU4AAZNT7.jpg",
+					"url": "https://t.co/D7araMKYru",
+					"display_url": "pic.twitter.com/D7araMKYru",
+					"expanded_url": "https://twitter.com/SharisRhodes/status/1122255864507682818/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1224,
+							"h": 1695,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 867,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 491,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 902554170430898200,
+			"id_str": "902554170430898178",
+			"name": "Sharis Rhodes",
+			"screen_name": "SharisRhodes",
+			"location": "Los Angeles, CA",
+			"description": "Mother. Educator. Millennial Civic Leader. “Keep your eyes on the prize”",
+			"url": "https://t.co/NnsMsZifzV",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/NnsMsZifzV",
+							"expanded_url": "Http://www.sharisrhodes.net",
+							"display_url": "sharisrhodes.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 29,
+			"friends_count": 47,
+			"listed_count": 0,
+			"created_at": "Tue Aug 29 15:30:57 +0000 2017",
+			"favourites_count": 73,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 100,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1096989792426815488/CRS6xdyp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1096989792426815488/CRS6xdyp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/902554170430898178/1556489709",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I AM running for the LA County Board of Supervisors in your 2nd district! I need your help to get elected. My goal is to impact our community with opportunities. I am willing to go against the status quo and earn my seat at the table but I can’t do it without your help! https://t.co/D7araMKYru"
+	},
+	{
 		"created_at": "Sun Apr 28 15:17:25 +0000 2019",
 		"id": 1122346432961241100,
 		"id_str": "1122520198047604736",
