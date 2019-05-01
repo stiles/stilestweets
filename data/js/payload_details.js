9bcf7d526703d866efa81c69b55c714d1ac24fc2
Matt Stiles <mattstiles@gmail.com>
2019-04-30 21:20:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13530,
-	"created_at": "Wed May 01 03:20:06 +0000 2019",
+	"tweets": 13531,
+	"created_at": "Wed May 01 04:20:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 01 04:17:59 +0000 2019",
+		"id": 1123441412580749300,
+		"id_str": "1123441412580749317",
+		"full_text": "@DevinNunes https://t.co/dNlKe9lV7T",
+		"truncated": false,
+		"display_text_range": [
+			12,
+			35
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DevinNunes",
+					"name": "Devin Nunes",
+					"id": 54412900,
+					"id_str": "54412900",
+					"indices": [
+						0,
+						11
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dNlKe9lV7T",
+					"expanded_url": "https://www.google.com/search?hl=en&tbs=simg:CAESwAIJe9QR6vQDWVEatAILEKjU2AQaBggXCD0IQgwLELCMpwgaYgpgCAMSKP0K8BTmEMUa7hCaGJccwBWbHOkfyzqwLtM60DqtLqsuhTvROqgvsy4aMA9yuckvgRcZ7DWIv9E7xcyj3cK215DalHHKPtV7VWUIzmaVVzBHa7Rpb66yIcI0WiAEDAsQjq7-CBoKCggIARIEjwMgOAwLEJ3twQkanQEKJwoVc3BvcnQgdXRpbGl0eSB2ZWhpY2xl2qWI9gMKCggvbS8wY3RycAoXCgNnbWPapYj2AwwKCi9tLzBqNzJweG0KFgoEcm9hZNqliPYDCgoIL20vMDZnZmoKGQoGZmVuZGVy2qWI9gMLCgkvbS8wZDc0ZHgKJgoTcGVyc29uYWwgbHV4dXJ5IGNhctqliPYDCwoJL20vMDF3NzFmDA&q=sport+utility+vehicle&tbm=isch&sa=X&ved=2ahUKEwjYt6TNvPnhAhVyFjQIHXU0A20Qsw56BAgCEAE&biw=1440&bih=749",
+					"display_url": "google.com/search?hl=en&t…",
+					"indices": [
+						12,
+						35
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": 1123244821571936300,
+		"in_reply_to_status_id_str": "1123244821571936257",
+		"in_reply_to_user_id": 54412900,
+		"in_reply_to_user_id_str": "54412900",
+		"in_reply_to_screen_name": "DevinNunes",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes. Covering the @CountyofLA beat with words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10666,
+			"friends_count": 1559,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5691,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13611,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1556461491",
+			"profile_link_color": "8E8AB2",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@DevinNunes https://t.co/dNlKe9lV7T"
+	},
+	{
 		"created_at": "Wed May 01 02:55:19 +0000 2019",
 		"id": 1123420607930986500,
 		"id_str": "1123420607930986496",
