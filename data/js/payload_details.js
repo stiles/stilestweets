0dfc9d2ba003154c1de2841034eb80f9cdb4de34
Matt Stiles <mattstiles@gmail.com>
2019-05-02 12:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13537,
-	"created_at": "Thu May 02 16:20:06 +0000 2019",
+	"tweets": 13538,
+	"created_at": "Thu May 02 19:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Thu May 02 19:00:10 +0000 2019",
+		"id": 1124025806408364000,
+		"id_str": "1124025806408364033",
+		"full_text": "So @Skittles says \"No two rainbows are the same. Neither are two packs of Skittles.\" \n\nNot true. But it took this guy 27,000 Skittles to figure that out.\n\n(FYI: The mean number of Skittles per bag is 59.273). \n\nhttps://t.co/OmqDcCI0My\n\nh/t @flowingdata https://t.co/rxBGI1yYqI",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			252
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Skittles",
+					"name": "Skittles",
+					"id": 105145017,
+					"id_str": "105145017",
+					"indices": [
+						3,
+						12
+					]
+				},
+				{
+					"screen_name": "flowingdata",
+					"name": "Nathan Yau",
+					"id": 14109167,
+					"id_str": "14109167",
+					"indices": [
+						240,
+						252
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OmqDcCI0My",
+					"expanded_url": "http://bit.ly/2VJymfu",
+					"display_url": "bit.ly/2VJymfu",
+					"indices": [
+						211,
+						234
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124024889436983300,
+					"id_str": "1124024889436983301",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/D5lWrn2W0AUgOHU.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5lWrn2W0AUgOHU.png",
+					"url": "https://t.co/rxBGI1yYqI",
+					"display_url": "pic.twitter.com/rxBGI1yYqI",
+					"expanded_url": "https://twitter.com/stiles/status/1124025806408364033/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 371,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1212,
+							"h": 662,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124024889436983300,
+					"id_str": "1124024889436983301",
+					"indices": [
+						253,
+						276
+					],
+					"media_url": "http://pbs.twimg.com/media/D5lWrn2W0AUgOHU.png",
+					"media_url_https": "https://pbs.twimg.com/media/D5lWrn2W0AUgOHU.png",
+					"url": "https://t.co/rxBGI1yYqI",
+					"display_url": "pic.twitter.com/rxBGI1yYqI",
+					"expanded_url": "https://twitter.com/stiles/status/1124025806408364033/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 371,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1212,
+							"h": 662,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 655,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes. Covering the @CountyofLA beat with words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10669,
+			"friends_count": 1561,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5701,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13620,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1556461491",
+			"profile_link_color": "8E8AB2",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "So @Skittles says \"No two rainbows are the same. Neither are two packs of Skittles.\" \n\nNot true. But it took this guy 27,000 Skittles to figure that out.\n\n(FYI: The mean number of Skittles per bag is 59.273). \n\nhttps://t.co/OmqDcCI0My\n\nh/t @flowingdata https://t.co/rxBGI1yYqI"
+	},
+	{
 		"created_at": "Thu May 02 15:34:09 +0000 2019",
 		"id": 1123787126020235300,
 		"id_str": "1123973960801169411",
