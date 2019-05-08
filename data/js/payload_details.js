55672494c19b9e623eb55bd795065966055af9e1
Matt Stiles <mattstiles@gmail.com>
2019-05-07 18:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13573,
-	"created_at": "Wed May 08 00:20:07 +0000 2019",
+	"tweets": 13574,
+	"created_at": "Wed May 08 01:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,133 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Wed May 08 01:00:59 +0000 2019",
+		"id": 1125770482958196700,
+		"id_str": "1125928549691445248",
+		"full_text": "It’s hard to report on the future. How do you find a reliable source? But if anyone can do it, it’s @elisewho, beginning her series Future You:\nhttps://t.co/NPgQxAP47g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			167
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "elisewho",
+					"name": "Elise Hu",
+					"id": 16001350,
+					"id_str": "16001350",
+					"indices": [
+						100,
+						109
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NPgQxAP47g",
+					"expanded_url": "https://n.pr/2vQiLwp",
+					"display_url": "n.pr/2vQiLwp",
+					"indices": [
+						144,
+						167
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 115710058,
+			"id_str": "115710058",
+			"name": "Steve Inskeep",
+			"screen_name": "NPRinskeep",
+			"location": "Washington, DC",
+			"description": "@MorningEdition, @UpFirst; #Jacksonland: https://t.co/rbiz4bc7VC      Coming 1/14/20: Imperfect Union: John Fremont, Jessie Fremont, and the Identity of America",
+			"url": "https://t.co/7Pui2wJIuI",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/7Pui2wJIuI",
+							"expanded_url": "http://steveinskeep.com",
+							"display_url": "steveinskeep.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/rbiz4bc7VC",
+							"expanded_url": "http://bit.ly/1Qr78zE",
+							"display_url": "bit.ly/1Qr78zE",
+							"indices": [
+								41,
+								64
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 254478,
+			"friends_count": 2404,
+			"listed_count": 4306,
+			"created_at": "Fri Feb 19 16:55:11 +0000 2010",
+			"favourites_count": 7388,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 33844,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/916328931095138312/2id9cypw_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/916328931095138312/2id9cypw_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/115710058/1401726711",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 22,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It’s hard to report on the future. How do you find a reliable source? But if anyone can do it, it’s @elisewho, beginning her series Future You:\nhttps://t.co/NPgQxAP47g"
+	},
+	{
 		"created_at": "Tue May 07 23:40:30 +0000 2019",
 		"id": 1125908294533505000,
 		"id_str": "1125908294533505025",
