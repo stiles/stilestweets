228eb6a5ae5c8673daebd2977d58edc56a357387
Matt Stiles <mattstiles@gmail.com>
2019-05-12 08:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13598,
-	"created_at": "Sun May 12 14:20:05 +0000 2019",
+	"tweets": 13599,
+	"created_at": "Sun May 12 15:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,292 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 12 15:08:31 +0000 2019",
+		"id": 1127591388126822400,
+		"id_str": "1127591388126822401",
+		"full_text": "Risky https://t.co/2EjSyXkl29",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			5
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2EjSyXkl29",
+					"expanded_url": "https://twitter.com/IHOP/status/1127559090903945217",
+					"display_url": "twitter.com/IHOP/status/11…",
+					"indices": [
+						6,
+						29
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10676,
+			"friends_count": 1571,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5795,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13699,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1557610576",
+			"profile_link_color": "7DAFA0",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1127559090903945200,
+		"quoted_status_id_str": "1127559090903945217",
+		"quoted_status_permalink": {
+			"url": "https://t.co/2EjSyXkl29",
+			"expanded": "https://twitter.com/IHOP/status/1127559090903945217",
+			"display": "twitter.com/IHOP/status/11…"
+		},
+		"quoted_status": {
+			"created_at": "Sun May 12 13:00:10 +0000 2019",
+			"id": 1127559090903945200,
+			"id_str": "1127559090903945217",
+			"full_text": "If you have pancakes in your tum tum, does that make you a pancake mum mum? Happy Mother’s Day to ALL the moms out there! https://t.co/wauH9sYvDb",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				121
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1127559089029025800,
+						"id_str": "1127559089029025792",
+						"indices": [
+							122,
+							145
+						],
+						"media_url": "http://pbs.twimg.com/media/D6XlBHTWkAAwmuc.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D6XlBHTWkAAwmuc.jpg",
+						"url": "https://t.co/wauH9sYvDb",
+						"display_url": "pic.twitter.com/wauH9sYvDb",
+						"expanded_url": "https://twitter.com/IHOP/status/1127559090903945217/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1127559089029025800,
+						"id_str": "1127559089029025792",
+						"indices": [
+							122,
+							145
+						],
+						"media_url": "http://pbs.twimg.com/media/D6XlBHTWkAAwmuc.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D6XlBHTWkAAwmuc.jpg",
+						"url": "https://t.co/wauH9sYvDb",
+						"display_url": "pic.twitter.com/wauH9sYvDb",
+						"expanded_url": "https://twitter.com/IHOP/status/1127559090903945217/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"large": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 680,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 1200,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://www.spredfast.com/\" rel=\"nofollow\">Spredfast app</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 475199073,
+				"id_str": "475199073",
+				"name": "IHOP",
+				"screen_name": "IHOP",
+				"location": "",
+				"description": "If at first you don't pancake... pancake, pancake again.",
+				"url": "https://t.co/eGn86fU9MC",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/eGn86fU9MC",
+								"expanded_url": "http://ihop.com",
+								"display_url": "ihop.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 387091,
+				"friends_count": 1406,
+				"listed_count": 1159,
+				"created_at": "Thu Jan 26 20:03:48 +0000 2012",
+				"favourites_count": 179978,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 150016,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "1465A9",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1016368420969799680/zxWgFYZ7_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1016368420969799680/zxWgFYZ7_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/475199073/1551105419",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 50,
+			"favorite_count": 247,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Risky https://t.co/2EjSyXkl29"
+	},
+	{
 		"created_at": "Sun May 12 14:03:55 +0000 2019",
 		"id": 1127575134158504000,
 		"id_str": "1127575134158503936",
