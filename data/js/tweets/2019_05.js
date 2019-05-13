f6d5005b3250a3c28fe6767a092464f4472a33c7
Matt Stiles <mattstiles@gmail.com>
2019-05-13 10:20:13 -0700

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,119 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 13 16:50:20 +0000 2019",
+		"id": 1127969370271912000,
+		"id_str": "1127979402204082181",
+		"full_text": "The L.A. County sheriff's decision to bring back fired deputies is now drawing concern among some of the cities that pay for sheriff's department patrol services. From @mayalau and @stiles https://t.co/BrhhgjVOtt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			212
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mayalau",
+					"name": "Maya Lau 🦅",
+					"id": 74304212,
+					"id_str": "74304212",
+					"indices": [
+						168,
+						176
+					]
+				},
+				{
+					"screen_name": "stiles",
+					"name": "Matt Stiles",
+					"id": 14790314,
+					"id_str": "14790314",
+					"indices": [
+						181,
+						188
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BrhhgjVOtt",
+					"expanded_url": "https://www.latimes.com/local/lanow/la-me-sheriff-villanueva-contract-cities-20190512-story.html",
+					"display_url": "latimes.com/local/lanow/la…",
+					"indices": [
+						189,
+						212
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 236508396,
+			"id_str": "236508396",
+			"name": "Richard Martin",
+			"screen_name": "richardmar10",
+			"location": "Los Angeles, CA",
+			"description": "Los Angeles Times Law Enforcement Editor by day, devoted husband and father/runner/tennis junkie/coffee lover and everything else by night.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1010,
+			"friends_count": 1205,
+			"listed_count": 38,
+			"created_at": "Mon Jan 10 19:08:34 +0000 2011",
+			"favourites_count": 236,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 1158,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2582356363/21tud9axfq56w9bp05zl_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2582356363/21tud9axfq56w9bp05zl_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The L.A. County sheriff's decision to bring back fired deputies is now drawing concern among some of the cities that pay for sheriff's department patrol services. From @mayalau and @stiles https://t.co/BrhhgjVOtt"
+	},
+	{
 		"created_at": "Mon May 13 04:15:56 +0000 2019",
 		"id": 1127789550116327400,
 		"id_str": "1127789550116327424",
