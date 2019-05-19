1e9d2e94abb267a549f970e42d07728c49853a07
Matt Stiles <mattstiles@gmail.com>
2019-05-19 08:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13629,
-	"created_at": "Sat May 18 21:20:06 +0000 2019",
+	"tweets": 13630,
+	"created_at": "Sun May 19 15:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,289 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sun May 19 14:35:38 +0000 2019",
+		"id": 1126303248514736100,
+		"id_str": "1130119827924738048",
+		"full_text": "In the past three years, more kids have been killed in school shootings in our country than American military troops have been killed in combat over the same time period. https://t.co/DCYieqApub",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			170
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/DCYieqApub",
+					"expanded_url": "https://twitter.com/TaskandPurpose/status/1126194021938941954",
+					"display_url": "twitter.com/TaskandPurpose…",
+					"indices": [
+						171,
+						194
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2783231383,
+			"id_str": "2783231383",
+			"name": "Amy McGrath",
+			"screen_name": "AmyMcGrathKY",
+			"location": "Georgetown, KY",
+			"description": "mother of 3 (all potty trained now), wife, retired Marine, F/A-18 pilot, Kentuckian, loves dogs, #CountryoverParty",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 87092,
+			"friends_count": 1797,
+			"listed_count": 525,
+			"created_at": "Mon Sep 01 00:58:43 +0000 2014",
+			"favourites_count": 3679,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 3526,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1083389610477129728/DNXNRBxp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1083389610477129728/DNXNRBxp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2783231383/1547842274",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1126194021938942000,
+		"quoted_status_id_str": "1126194021938941954",
+		"quoted_status_permalink": {
+			"url": "https://t.co/DCYieqApub",
+			"expanded": "https://twitter.com/TaskandPurpose/status/1126194021938941954",
+			"display": "twitter.com/TaskandPurpose…"
+		},
+		"quoted_status": {
+			"created_at": "Wed May 08 18:35:53 +0000 2019",
+			"id": 1126194021938942000,
+			"id_str": "1126194021938941954",
+			"full_text": "Young military hopefuls are willing to go to war for their country.\n\n Instead, many are losing their lives in school shootings https://t.co/EipMPOJPt4 https://t.co/oqtbEUfXuO",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				150
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/EipMPOJPt4",
+						"expanded_url": "http://bit.ly/2HdZEBo",
+						"display_url": "bit.ly/2HdZEBo",
+						"indices": [
+							127,
+							150
+						]
+					}
+				],
+				"media": [
+					{
+						"id": 1126194020496093200,
+						"id_str": "1126194020496093185",
+						"indices": [
+							151,
+							174
+						],
+						"media_url": "http://pbs.twimg.com/media/D6ELfqvWsAEHF1q.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D6ELfqvWsAEHF1q.jpg",
+						"url": "https://t.co/oqtbEUfXuO",
+						"display_url": "pic.twitter.com/oqtbEUfXuO",
+						"expanded_url": "https://twitter.com/TaskandPurpose/status/1126194021938941954/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 630,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 357,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 630,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1126194020496093200,
+						"id_str": "1126194020496093185",
+						"indices": [
+							151,
+							174
+						],
+						"media_url": "http://pbs.twimg.com/media/D6ELfqvWsAEHF1q.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D6ELfqvWsAEHF1q.jpg",
+						"url": "https://t.co/oqtbEUfXuO",
+						"display_url": "pic.twitter.com/oqtbEUfXuO",
+						"expanded_url": "https://twitter.com/TaskandPurpose/status/1126194021938941954/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 630,
+								"resize": "fit"
+							},
+							"small": {
+								"w": 680,
+								"h": 357,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 1200,
+								"h": 630,
+								"resize": "fit"
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"https://buffer.com\" rel=\"nofollow\">Buffer</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 2374466929,
+				"id_str": "2374466929",
+				"name": "Task & Purpose",
+				"screen_name": "TaskandPurpose",
+				"location": "Global",
+				"description": "News, culture, and analysis by and for the military community. Talk to us at tips@taskandpurpose.com or slide into our DM's.",
+				"url": "https://t.co/Oohz9DBan9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/Oohz9DBan9",
+								"expanded_url": "https://www.taskandpurpose.com",
+								"display_url": "taskandpurpose.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 33431,
+				"friends_count": 0,
+				"listed_count": 674,
+				"created_at": "Wed Mar 05 23:04:12 +0000 2014",
+				"favourites_count": 1824,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 45424,
+				"lang": "en",
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/468484928095535104/-7y8-9Pu_normal.png",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/468484928095535104/-7y8-9Pu_normal.png",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/2374466929/1554826118",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 911,
+			"favorite_count": 1851,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 22031,
+		"favorite_count": 45939,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In the past three years, more kids have been killed in school shootings in our country than American military troops have been killed in combat over the same time period. https://t.co/DCYieqApub"
+	},
+	{
 		"created_at": "Sat May 18 20:29:24 +0000 2019",
 		"id": 1129780705213517800,
 		"id_str": "1129846469828079616",
