a1f6026951451af727eec7a618290408c81c8522
Matt Stiles <mattstiles@gmail.com>
2019-05-21 15:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13647,
-	"created_at": "Tue May 21 21:20:07 +0000 2019",
+	"tweets": 13648,
+	"created_at": "Tue May 21 22:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,209 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Tue May 21 21:22:39 +0000 2019",
+		"id": 1130947032233865200,
+		"id_str": "1130947032233865216",
+		"full_text": "Today is Take Your Child to Work Day @CountyofLA. Perfect time for the expletives to start flying again during public comment at the Board of Supervisors. The transcript has 17 today, which excludes many shouted off mic. Wrote about this recently:  https://t.co/oeAaIpv4o1 #tyctwd https://t.co/HvH4pNVJla",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "tyctwd",
+					"indices": [
+						273,
+						280
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CountyofLA",
+					"name": "Los Angeles County",
+					"id": 193771633,
+					"id_str": "193771633",
+					"indices": [
+						37,
+						48
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/oeAaIpv4o1",
+					"expanded_url": "https://www.latimes.com/local/lanow/la-me-city-county-meetings-expletive-curse-racial-slur-language-20190419-story.html",
+					"display_url": "latimes.com/local/lanow/la…",
+					"indices": [
+						249,
+						272
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1130947024008835100,
+					"id_str": "1130947024008835072",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D7HuU4nU8AAkTTd.png",
+					"media_url_https": "https://pbs.twimg.com/media/D7HuU4nU8AAkTTd.png",
+					"url": "https://t.co/HvH4pNVJla",
+					"display_url": "pic.twitter.com/HvH4pNVJla",
+					"expanded_url": "https://twitter.com/stiles/status/1130947032233865216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 593,
+							"h": 670,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 593,
+							"h": 670,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 593,
+							"h": 670,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1130947024008835100,
+					"id_str": "1130947024008835072",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/D7HuU4nU8AAkTTd.png",
+					"media_url_https": "https://pbs.twimg.com/media/D7HuU4nU8AAkTTd.png",
+					"url": "https://t.co/HvH4pNVJla",
+					"display_url": "pic.twitter.com/HvH4pNVJla",
+					"expanded_url": "https://twitter.com/stiles/status/1130947032233865216/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 593,
+							"h": 670,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 593,
+							"h": 670,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 593,
+							"h": 670,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10763,
+			"friends_count": 1591,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5891,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13761,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1558236714",
+			"profile_link_color": "FF8254",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today is Take Your Child to Work Day @CountyofLA. Perfect time for the expletives to start flying again during public comment at the Board of Supervisors. The transcript has 17 today, which excludes many shouted off mic. Wrote about this recently:  https://t.co/oeAaIpv4o1 #tyctwd https://t.co/HvH4pNVJla"
+	},
+	{
 		"created_at": "Tue May 21 20:45:19 +0000 2019",
 		"id": 1130937638934810600,
 		"id_str": "1130937638934810624",
