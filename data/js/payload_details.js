8aed6b8a30ab439bfd23d0e127459e0e1c940022
Matt Stiles <mattstiles@gmail.com>
2019-05-21 17:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13649,
-	"created_at": "Tue May 21 23:20:07 +0000 2019",
+	"tweets": 13650,
+	"created_at": "Wed May 22 00:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Tue May 21 23:30:45 +0000 2019",
+		"id": 1130979269763448800,
+		"id_str": "1130979269763448832",
+		"full_text": "Fascinating look by @DaveSheinin and @emamd at how velocity has changed pro baseball: https://t.co/CyNPQbHnmt\n\nOne reason: The median pitcher has gone from 6'0\"/178 lbs in 1920 to 6'3\"/215 lbs today. \n\n(Density plots inspired by @veltman) https://t.co/Q4JmCaTh8T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			238
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DaveSheinin",
+					"name": "Dave Sheinin",
+					"id": 94169802,
+					"id_str": "94169802",
+					"indices": [
+						20,
+						32
+					]
+				},
+				{
+					"screen_name": "emamd",
+					"name": "Armand Emamdjomeh",
+					"id": 2228091,
+					"id_str": "2228091",
+					"indices": [
+						37,
+						43
+					]
+				},
+				{
+					"screen_name": "veltman",
+					"name": "Noah Veltman",
+					"id": 17673012,
+					"id_str": "17673012",
+					"indices": [
+						229,
+						237
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/CyNPQbHnmt",
+					"expanded_url": "https://www.washingtonpost.com/sports/2019/05/21/velocity-is-strangling-baseball-its-grip-keeps-tightening",
+					"display_url": "washingtonpost.com/sports/2019/05…",
+					"indices": [
+						86,
+						109
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1130979261165101000,
+					"id_str": "1130979261165101056",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/media/D7ILpVYUYAAT007.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7ILpVYUYAAT007.jpg",
+					"url": "https://t.co/Q4JmCaTh8T",
+					"display_url": "pic.twitter.com/Q4JmCaTh8T",
+					"expanded_url": "https://twitter.com/stiles/status/1130979269763448832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 484,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1026,
+							"h": 730,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1026,
+							"h": 730,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1130979261165101000,
+					"id_str": "1130979261165101056",
+					"indices": [
+						239,
+						262
+					],
+					"media_url": "http://pbs.twimg.com/media/D7ILpVYUYAAT007.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7ILpVYUYAAT007.jpg",
+					"url": "https://t.co/Q4JmCaTh8T",
+					"display_url": "pic.twitter.com/Q4JmCaTh8T",
+					"expanded_url": "https://twitter.com/stiles/status/1130979269763448832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 484,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1026,
+							"h": 730,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1026,
+							"h": 730,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10765,
+			"friends_count": 1591,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5892,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1558480371",
+			"profile_link_color": "4E5438",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Fascinating look by @DaveSheinin and @emamd at how velocity has changed pro baseball: https://t.co/CyNPQbHnmt\n\nOne reason: The median pitcher has gone from 6'0\"/178 lbs in 1920 to 6'3\"/215 lbs today. \n\n(Density plots inspired by @veltman) https://t.co/Q4JmCaTh8T"
+	},
+	{
 		"created_at": "Tue May 21 22:37:44 +0000 2019",
 		"id": 1130964415765856300,
 		"id_str": "1130965927661412353",
