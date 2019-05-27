0a80c5876ac23bf82a1cb541f7e76f38ce6a951e
Matt Stiles <mattstiles@gmail.com>
2019-05-26 21:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13682,
-	"created_at": "Mon May 27 01:20:05 +0000 2019",
+	"tweets": 13683,
+	"created_at": "Mon May 27 04:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,288 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Mon May 27 04:02:49 +0000 2019",
+		"id": 1132859677258133500,
+		"id_str": "1132859677258133504",
+		"full_text": "These voter surname maps are much more interesting with the Board of Supervisors district boundaries on top. These maps show Chinese, Indian, Spanish and non-classified surnames (which have varying proportions and scales). https://t.co/bq0pFNzyox",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			222
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1132859648489377800,
+					"id_str": "1132859648489377792",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i52SCVUAAV0fu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i52SCVUAAV0fu.jpg",
+					"url": "https://t.co/bq0pFNzyox",
+					"display_url": "pic.twitter.com/bq0pFNzyox",
+					"expanded_url": "https://twitter.com/stiles/status/1132859677258133504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 489,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1473,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 863,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1132859648489377800,
+					"id_str": "1132859648489377792",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i52SCVUAAV0fu.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i52SCVUAAV0fu.jpg",
+					"url": "https://t.co/bq0pFNzyox",
+					"display_url": "pic.twitter.com/bq0pFNzyox",
+					"expanded_url": "https://twitter.com/stiles/status/1132859677258133504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 489,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1473,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 863,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1132859648497729500,
+					"id_str": "1132859648497729536",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i52SEUwAAkz-Y.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i52SEUwAAkz-Y.jpg",
+					"url": "https://t.co/bq0pFNzyox",
+					"display_url": "pic.twitter.com/bq0pFNzyox",
+					"expanded_url": "https://twitter.com/stiles/status/1132859677258133504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 856,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1461,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1132859648493609000,
+					"id_str": "1132859648493608961",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i52SDV4AE9xKa.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i52SDV4AE9xKa.jpg",
+					"url": "https://t.co/bq0pFNzyox",
+					"display_url": "pic.twitter.com/bq0pFNzyox",
+					"expanded_url": "https://twitter.com/stiles/status/1132859677258133504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 885,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2042,
+							"h": 1506,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 502,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1132859648485183500,
+					"id_str": "1132859648485183489",
+					"indices": [
+						223,
+						246
+					],
+					"media_url": "http://pbs.twimg.com/media/D7i52SBVUAEfdM-.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7i52SBVUAEfdM-.jpg",
+					"url": "https://t.co/bq0pFNzyox",
+					"display_url": "pic.twitter.com/bq0pFNzyox",
+					"expanded_url": "https://twitter.com/stiles/status/1132859677258133504/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 2048,
+							"h": 1488,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 494,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 872,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1132817634192371700,
+		"in_reply_to_status_id_str": "1132817634192371712",
+		"in_reply_to_user_id": 14790314,
+		"in_reply_to_user_id_str": "14790314",
+		"in_reply_to_screen_name": "stiles",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10796,
+			"friends_count": 1594,
+			"listed_count": 875,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5934,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13805,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1558880563",
+			"profile_link_color": "C33826",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "These voter surname maps are much more interesting with the Board of Supervisors district boundaries on top. These maps show Chinese, Indian, Spanish and non-classified surnames (which have varying proportions and scales). https://t.co/bq0pFNzyox"
+	},
+	{
 		"created_at": "Mon May 27 01:15:45 +0000 2019",
 		"id": 1132817634192371700,
 		"id_str": "1132817634192371712",
