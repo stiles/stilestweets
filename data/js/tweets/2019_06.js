3351b81ff14b18f38a7c05fb03b85023ef1b1878
Matt Stiles <mattstiles@gmail.com>
2019-05-31 20:20:06 -0700

    Added 2 tweets

---
+++ data/js/tweets/2019_06.js
@@ -0,0 +1,339 @@
+Grailbird.data.tweets_2019_06 = 
+[
+	{
+		"created_at": "Sat Jun 01 02:42:43 +0000 2019",
+		"id": 1134649727872430100,
+		"id_str": "1134651461789290497",
+		"full_text": "If you missed it, Watch @LaTimes @Stiles on @SpecNews1SoCal on #JUSTICE #Kids 10pm https://t.co/zuXshRZhVc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "JUSTICE",
+					"indices": [
+						63,
+						71
+					]
+				},
+				{
+					"text": "Kids",
+					"indices": [
+						72,
+						77
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						24,
+						32
+					]
+				},
+				{
+					"screen_name": "stiles",
+					"name": "Matt Stiles",
+					"id": 14790314,
+					"id_str": "14790314",
+					"indices": [
+						33,
+						40
+					]
+				},
+				{
+					"screen_name": "SpecNews1SoCal",
+					"name": "Spectrum News 1 SoCal",
+					"id": 1046792822039482400,
+					"id_str": "1046792822039482368",
+					"indices": [
+						44,
+						59
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zuXshRZhVc",
+					"expanded_url": "https://www.latimes.com/local/california/la-me-juvenile-halls-doj-justice-department-inspection-20190523-story.html",
+					"display_url": "latimes.com/local/californ…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 392207105,
+			"id_str": "392207105",
+			"name": "Lisa McRee",
+			"screen_name": "Lisa_McRee",
+			"location": "Los Angeles, CA",
+			"description": "Anchor @SpecNews1SoCal AM & LA TIMES TODAY 7pm. Past lives @GMA @ABCWNN #PBSCaliforniaConnected @KABC @WFAA Emmy & @DupontAwards winner Most proud being a Mom 😎",
+			"url": "https://t.co/QSRaQml3UW",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/QSRaQml3UW",
+							"expanded_url": "http://SpectrumNews1.com",
+							"display_url": "SpectrumNews1.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1323,
+			"friends_count": 689,
+			"listed_count": 34,
+			"created_at": "Sun Oct 16 18:03:16 +0000 2011",
+			"favourites_count": 4994,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 5453,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme4/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1101714670354817025/ldJeM_1E_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1101714670354817025/ldJeM_1E_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/392207105/1518471123",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If you missed it, Watch @LaTimes @Stiles on @SpecNews1SoCal on #JUSTICE #Kids 10pm https://t.co/zuXshRZhVc"
+	},
+	{
+		"created_at": "Sat Jun 01 02:42:15 +0000 2019",
+		"id": 1134651343514169300,
+		"id_str": "1134651343514169344",
+		"full_text": "Was a North Korean official executed? Mystery may cloud new talks with the U.S. https://t.co/rVBX0CQVkS\n\nMaybe not.\n👇👇👇 https://t.co/4UNIJhMsXk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			119
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/rVBX0CQVkS",
+					"expanded_url": "https://www.washingtonpost.com/world/2019/05/31/report-north-korean-officials-execution-prompts-skepticism-concern-about-gaps-knowledge/",
+					"display_url": "washingtonpost.com/world/2019/05/…",
+					"indices": [
+						80,
+						103
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1134651339433173000,
+					"id_str": "1134651339433172992",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/D78XYa7V4AAUXAz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D78XYa7V4AAUXAz.jpg",
+					"url": "https://t.co/4UNIJhMsXk",
+					"display_url": "pic.twitter.com/4UNIJhMsXk",
+					"expanded_url": "https://twitter.com/stiles/status/1134651343514169344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 237,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 418,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1606,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1134651339433173000,
+					"id_str": "1134651339433172992",
+					"indices": [
+						120,
+						143
+					],
+					"media_url": "http://pbs.twimg.com/media/D78XYa7V4AAUXAz.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D78XYa7V4AAUXAz.jpg",
+					"url": "https://t.co/4UNIJhMsXk",
+					"display_url": "pic.twitter.com/4UNIJhMsXk",
+					"expanded_url": "https://twitter.com/stiles/status/1134651343514169344/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 237,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 418,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1606,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10816,
+			"friends_count": 1599,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 5999,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13860,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1559277869",
+			"profile_link_color": "2264BA",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Was a North Korean official executed? Mystery may cloud new talks with the U.S. https://t.co/rVBX0CQVkS\n\nMaybe not.\n👇👇👇 https://t.co/4UNIJhMsXk"
+	}
+]
