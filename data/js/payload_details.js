a8960f29f66d7490d403ed8cf67c32c672177b97
Matt Stiles <mattstiles@gmail.com>
2019-06-01 14:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13728,
-	"created_at": "Sat Jun 01 16:20:05 +0000 2019",
+	"tweets": 13729,
+	"created_at": "Sat Jun 01 21:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sat Jun 01 21:10:44 +0000 2019",
+		"id": 1134833685142749200,
+		"id_str": "1134930302797721600",
+		"full_text": "My latest City Beat is ready for some old-school reading in the paper today. #mydayinla https://t.co/iWrTD20BsT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			87
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "mydayinla",
+					"indices": [
+						77,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1134833678691913700,
+					"id_str": "1134833678691913733",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/D7-9N9mU0AUFEKv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7-9N9mU0AUFEKv.jpg",
+					"url": "https://t.co/iWrTD20BsT",
+					"display_url": "pic.twitter.com/iWrTD20BsT",
+					"expanded_url": "https://twitter.com/LATimescitybeat/status/1134833685142749184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 571,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1008,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1134833678691913700,
+					"id_str": "1134833678691913733",
+					"indices": [
+						88,
+						111
+					],
+					"media_url": "http://pbs.twimg.com/media/D7-9N9mU0AUFEKv.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7-9N9mU0AUFEKv.jpg",
+					"url": "https://t.co/iWrTD20BsT",
+					"display_url": "pic.twitter.com/iWrTD20BsT",
+					"expanded_url": "https://twitter.com/LATimescitybeat/status/1134833685142749184/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 571,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1008,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1720,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 20642650,
+			"id_str": "20642650",
+			"name": "Nita Lelyveld",
+			"screen_name": "LATimescitybeat",
+			"location": "Los Angeles, CA",
+			"description": "I'm Nita Lelyveld, an @latimes columnist. I created a community around #mydayinla,  to make a big city feel like a small town. Explore & use the hashtag. I RT.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 50088,
+			"friends_count": 10272,
+			"listed_count": 2717,
+			"created_at": "Thu Feb 12 00:21:37 +0000 2009",
+			"favourites_count": 20801,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 137212,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "0E0D0F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/472406825141538816/cA2xd_Fa_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/472406825141538816/cA2xd_Fa_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/20642650/1392153514",
+			"profile_link_color": "4463A6",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 38,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My latest City Beat is ready for some old-school reading in the paper today. #mydayinla https://t.co/iWrTD20BsT"
+	},
+	{
 		"created_at": "Sat Jun 01 15:36:29 +0000 2019",
 		"id": 1134495428496547800,
 		"id_str": "1134846183598223360",
@@ -128,7 +297,7 @@ Grailbird.data.tweets_2019_06 =
 		"created_at": "Sat Jun 01 15:31:56 +0000 2019",
 		"id": 1134727685467398100,
 		"id_str": "1134845041610936321",
-		"full_text": ".@ladygaga at Seoul Pride 🏳️‍🌈 https://t.co/uRwuONSsux",
+		"full_text": ".@ladygaga at Seoul Pride 🏳️🌈 https://t.co/uRwuONSsux",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -357,7 +526,7 @@ Grailbird.data.tweets_2019_06 =
 		"retweeted": true,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": ".@ladygaga at Seoul Pride 🏳️‍🌈 https://t.co/uRwuONSsux"
+		"text": ".@ladygaga at Seoul Pride 🏳️🌈 https://t.co/uRwuONSsux"
 	},
 	{
 		"created_at": "Sat Jun 01 15:18:13 +0000 2019",
