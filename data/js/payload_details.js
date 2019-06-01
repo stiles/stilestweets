3db6b8b374e6b8cc8b129153ca67d06df3d0389c
Matt Stiles <mattstiles@gmail.com>
2019-06-01 07:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13724,
-	"created_at": "Sat Jun 01 03:20:05 +0000 2019",
+	"tweets": 13725,
+	"created_at": "Sat Jun 01 14:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,243 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sat Jun 01 13:20:25 +0000 2019",
+		"id": 1134811942571241500,
+		"id_str": "1134811942571241473",
+		"full_text": "Interesting piece about this by @ShannonWHall in @NYTScience: https://t.co/RFkCrLM2bF https://t.co/v8PKkMllmw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			85
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "ShannonWHall",
+					"name": "Shannon Hall",
+					"id": 2149663698,
+					"id_str": "2149663698",
+					"indices": [
+						32,
+						45
+					]
+				},
+				{
+					"screen_name": "NYTScience",
+					"name": "NYT Science",
+					"id": 1435461,
+					"id_str": "1435461",
+					"indices": [
+						49,
+						60
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RFkCrLM2bF",
+					"expanded_url": "https://www.nytimes.com/2019/06/01/science/starlink-spacex-astronomers.html",
+					"display_url": "nytimes.com/2019/06/01/sci…",
+					"indices": [
+						62,
+						85
+					]
+				},
+				{
+					"url": "https://t.co/v8PKkMllmw",
+					"expanded_url": "https://twitter.com/Alex_Parker/status/1132163931378610178",
+					"display_url": "twitter.com/Alex_Parker/st…",
+					"indices": [
+						86,
+						109
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10816,
+			"friends_count": 1601,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6000,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13861,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1559277869",
+			"profile_link_color": "2264BA",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1132163931378610200,
+		"quoted_status_id_str": "1132163931378610178",
+		"quoted_status_permalink": {
+			"url": "https://t.co/v8PKkMllmw",
+			"expanded": "https://twitter.com/Alex_Parker/status/1132163931378610178",
+			"display": "twitter.com/Alex_Parker/st…"
+		},
+		"quoted_status": {
+			"created_at": "Sat May 25 05:58:10 +0000 2019",
+			"id": 1132163931378610200,
+			"id_str": "1132163931378610178",
+			"full_text": "I know people are excited about those images of the train of SpaceX Starlink satellites, but it gives me pause.\n\nThey’re bright, and there are going to be a lot of them. \n\nIf SpaceX launches all 12,000, they will outnumber stars visible to the naked eye.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				254
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 18644734,
+				"id_str": "18644734",
+				"name": "Alex Parker",
+				"screen_name": "Alex_Parker",
+				"location": "Boulder, Colorado",
+				"description": "Planetary astronomer and artist exploring the origin and history of our solar system. Director of @ESPRESSO_SSERVI. If in doubt, I could probably laser it.",
+				"url": "https://t.co/45w2HA4olE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/45w2HA4olE",
+								"expanded_url": "http://www.alexharrisonparker.com",
+								"display_url": "alexharrisonparker.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 30217,
+				"friends_count": 668,
+				"listed_count": 792,
+				"created_at": "Mon Jan 05 20:34:50 +0000 2009",
+				"favourites_count": 15760,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 24351,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/616729283768266752/es8ydAzc_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/616729283768266752/es8ydAzc_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/18644734/1497035875",
+				"profile_link_color": "0084B4",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "EFEFEF",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1255,
+			"favorite_count": 3002,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Interesting piece about this by @ShannonWHall in @NYTScience: https://t.co/RFkCrLM2bF https://t.co/v8PKkMllmw"
+	},
+	{
 		"created_at": "Sat Jun 01 02:42:43 +0000 2019",
 		"id": 1134649727872430100,
 		"id_str": "1134651461789290497",
