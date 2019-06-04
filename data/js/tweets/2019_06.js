2c212bca0526194b40813069cbcbb3f2ac767084
Matt Stiles <mattstiles@gmail.com>
2019-06-03 23:20:10 -0700

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 04 06:14:30 +0000 2019",
+		"id": 1135734025392349200,
+		"id_str": "1135791920536379393",
+		"full_text": "Tank Man, a global symbol of freedom and defiance, is a fixture in pop culture three decades after Tiananmen. But he is still a mystery. https://t.co/QxyGXlfaT8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			160
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/QxyGXlfaT8",
+					"expanded_url": "https://nyti.ms/2MrXWSP",
+					"display_url": "nyti.ms/2MrXWSP",
+					"indices": [
+						137,
+						160
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 23836537,
+			"id_str": "23836537",
+			"name": "Javier C. Hernández",
+			"screen_name": "HernandezJavier",
+			"location": "Beijing",
+			"description": "China correspondent, The New York Times.",
+			"url": "https://t.co/DoxN7RV4xK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/DoxN7RV4xK",
+							"expanded_url": "http://nyti.ms/1gjStaF",
+							"display_url": "nyti.ms/1gjStaF",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19069,
+			"friends_count": 1107,
+			"listed_count": 641,
+			"created_at": "Wed Mar 11 21:45:47 +0000 2009",
+			"favourites_count": 381,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 934,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1105495988901314560/0cvdjYLS_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1105495988901314560/0cvdjYLS_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/23836537/1552405837",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 13,
+		"favorite_count": 10,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Tank Man, a global symbol of freedom and defiance, is a fixture in pop culture three decades after Tiananmen. But he is still a mystery. https://t.co/QxyGXlfaT8"
+	},
+	{
 		"created_at": "Tue Jun 04 00:00:16 +0000 2019",
 		"id": 1135697743488282600,
 		"id_str": "1135697743488282624",
