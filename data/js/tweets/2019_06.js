672392373c982ccddfc7069a0a348c2f2bbee2e2
Matt Stiles <mattstiles@gmail.com>
2019-06-04 21:20:05 -0700

    Added 1 tweets

--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Wed Jun 05 03:47:29 +0000 2019",
+		"id": 1136115258441863200,
+		"id_str": "1136117310115069952",
+		"full_text": "First batch of results not looking good for @austinbeutner's Measure EE.\n\nThe bid to raise money for LA's school system needs a 2/3 majority to pass.\n\nIt's far short of that so far. \n\nhttps://t.co/dorPjWFfRa https://t.co/uoDXt2m0CZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			207
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "austinbeutner",
+					"name": "Austin Beutner",
+					"id": 91225807,
+					"id_str": "91225807",
+					"indices": [
+						44,
+						58
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/dorPjWFfRa",
+					"expanded_url": "https://www.latimes.com/projects/la-me-live-la-election-results-20190604/?foo=hjk",
+					"display_url": "latimes.com/projects/la-me…",
+					"indices": [
+						184,
+						207
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1136115247276675100,
+					"id_str": "1136115247276675072",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/D8RKzEnU0AAQY0v.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8RKzEnU0AAQY0v.png",
+					"url": "https://t.co/uoDXt2m0CZ",
+					"display_url": "pic.twitter.com/uoDXt2m0CZ",
+					"expanded_url": "https://twitter.com/palewire/status/1136115258441863173/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 314,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 314,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 314,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1136115247276675100,
+					"id_str": "1136115247276675072",
+					"indices": [
+						208,
+						231
+					],
+					"media_url": "http://pbs.twimg.com/media/D8RKzEnU0AAQY0v.png",
+					"media_url_https": "https://pbs.twimg.com/media/D8RKzEnU0AAQY0v.png",
+					"url": "https://t.co/uoDXt2m0CZ",
+					"display_url": "pic.twitter.com/uoDXt2m0CZ",
+					"expanded_url": "https://twitter.com/palewire/status/1136115258441863173/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 314,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 314,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 314,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14559196,
+			"id_str": "14559196",
+			"name": "Ben Welsh",
+			"screen_name": "palewire",
+			"location": "Live and direct",
+			"description": "Editor, @LATdatadesk at @latimes · Organizer, https://t.co/lZvqTdDOmO · Archivist, https://t.co/CaO6K276g3 · Send tips to ben.welsh@latimes.com",
+			"url": "https://t.co/kogJ0fJceo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kogJ0fJceo",
+							"expanded_url": "http://palewi.re/who-is-ben-welsh/",
+							"display_url": "palewi.re/who-is-ben-wel…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/lZvqTdDOmO",
+							"expanded_url": "http://www.californiacivicdata.org",
+							"display_url": "californiacivicdata.org",
+							"indices": [
+								46,
+								69
+							]
+						},
+						{
+							"url": "https://t.co/CaO6K276g3",
+							"expanded_url": "http://pastpages.org",
+							"display_url": "pastpages.org",
+							"indices": [
+								83,
+								106
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9021,
+			"friends_count": 3060,
+			"listed_count": 706,
+			"created_at": "Sun Apr 27 16:10:55 +0000 2008",
+			"favourites_count": 8,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 54728,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14559196/1423764862",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDDDDD",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "First batch of results not looking good for @austinbeutner's Measure EE.\n\nThe bid to raise money for LA's school system needs a 2/3 majority to pass.\n\nIt's far short of that so far. \n\nhttps://t.co/dorPjWFfRa https://t.co/uoDXt2m0CZ"
+	},
+	{
 		"created_at": "Wed Jun 05 03:18:33 +0000 2019",
 		"id": 1136110029998776300,
 		"id_str": "1136110029998776321",
