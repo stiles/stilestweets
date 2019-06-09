b12c2e980817da957b6bdf080fc2c8c344395338
Matt Stiles <mattstiles@gmail.com>
2019-06-08 17:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13778,
-	"created_at": "Sat Jun 08 22:20:04 +0000 2019",
+	"tweets": 13779,
+	"created_at": "Sun Jun 09 00:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,245 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sun Jun 09 00:14:37 +0000 2019",
+		"id": 1137513293604577300,
+		"id_str": "1137513293604577280",
+		"full_text": "Clever. @googlemaps styled Santa Monica Blvd. in rainbow colors for the #LAPride festival and parade. https://t.co/NDGGtlQWgj https://t.co/VhhrYinhn8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			125
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LAPride",
+					"indices": [
+						72,
+						80
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "googlemaps",
+					"name": "Google Maps",
+					"id": 31311757,
+					"id_str": "31311757",
+					"indices": [
+						8,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/NDGGtlQWgj",
+					"expanded_url": "https://www.google.com/maps/@34.0861169,-118.3800647,15.79z",
+					"display_url": "google.com/maps/@34.08611…",
+					"indices": [
+						102,
+						125
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1137513281248096300,
+					"id_str": "1137513281248096256",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/D8lCTXAU8AAPT2d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8lCTXAU8AAPT2d.jpg",
+					"url": "https://t.co/VhhrYinhn8",
+					"display_url": "pic.twitter.com/VhhrYinhn8",
+					"expanded_url": "https://twitter.com/stiles/status/1137513293604577280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 532,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 940,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1742,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1137513281248096300,
+					"id_str": "1137513281248096256",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/D8lCTXAU8AAPT2d.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8lCTXAU8AAPT2d.jpg",
+					"url": "https://t.co/VhhrYinhn8",
+					"display_url": "pic.twitter.com/VhhrYinhn8",
+					"expanded_url": "https://twitter.com/stiles/status/1137513293604577280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 532,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 940,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1742,
+							"h": 1364,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1137513281248096300,
+					"id_str": "1137513281248096257",
+					"indices": [
+						126,
+						149
+					],
+					"media_url": "http://pbs.twimg.com/media/D8lCTXAU8AEwtpr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8lCTXAU8AEwtpr.jpg",
+					"url": "https://t.co/VhhrYinhn8",
+					"display_url": "pic.twitter.com/VhhrYinhn8",
+					"expanded_url": "https://twitter.com/stiles/status/1137513293604577280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 516,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 910,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1232,
+							"h": 934,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Pastor: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10834,
+			"friends_count": 1611,
+			"listed_count": 875,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6077,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13929,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1559716076",
+			"profile_link_color": "7C816C",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Clever. @googlemaps styled Santa Monica Blvd. in rainbow colors for the #LAPride festival and parade. https://t.co/NDGGtlQWgj https://t.co/VhhrYinhn8"
+	},
+	{
 		"created_at": "Sat Jun 08 21:23:23 +0000 2019",
 		"id": 1137470200893198300,
 		"id_str": "1137470200893198337",
