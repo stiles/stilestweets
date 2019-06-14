a7b6f518efde60124a85d70379cb33c819a3742a
Matt Stiles <mattstiles@gmail.com>
2019-06-14 08:20:09 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13813,
-	"created_at": "Fri Jun 14 14:20:06 +0000 2019",
+	"tweets": 13814,
+	"created_at": "Fri Jun 14 15:20:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Fri Jun 14 14:27:40 +0000 2019",
+		"id": 1139539908123430900,
+		"id_str": "1139539908123430912",
+		"full_text": "\"I could invert the photograph and enlarge the text so you could read it,\" @PeteSouza  writes about @BarackObama reading a memo in the Red Room in 2009. \"But let me save you the time and the trouble:⁣ It doesn’t include some fake deal with Mexico.\" https://t.co/RxCrw4QaDj https://t.co/Xwg4tQcgyE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "PeteSouza",
+					"name": "Pete Souza",
+					"id": 821782476515409900,
+					"id_str": "821782476515409920",
+					"indices": [
+						75,
+						85
+					]
+				},
+				{
+					"screen_name": "BarackObama",
+					"name": "Barack Obama",
+					"id": 813286,
+					"id_str": "813286",
+					"indices": [
+						100,
+						112
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/RxCrw4QaDj",
+					"expanded_url": "https://www.instagram.com/p/BypyE9fl3S3",
+					"display_url": "instagram.com/p/BypyE9fl3S3",
+					"indices": [
+						249,
+						272
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1139539903086096400,
+					"id_str": "1139539903086096384",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/D9B1gRrUcAAb5dR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D9B1gRrUcAAb5dR.jpg",
+					"url": "https://t.co/Xwg4tQcgyE",
+					"display_url": "pic.twitter.com/Xwg4tQcgyE",
+					"expanded_url": "https://twitter.com/stiles/status/1139539908123430912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 873,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 550,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 873,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1139539903086096400,
+					"id_str": "1139539903086096384",
+					"indices": [
+						273,
+						296
+					],
+					"media_url": "http://pbs.twimg.com/media/D9B1gRrUcAAb5dR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D9B1gRrUcAAb5dR.jpg",
+					"url": "https://t.co/Xwg4tQcgyE",
+					"display_url": "pic.twitter.com/Xwg4tQcgyE",
+					"expanded_url": "https://twitter.com/stiles/status/1139539908123430912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1080,
+							"h": 873,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 550,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1080,
+							"h": 873,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10831,
+			"friends_count": 1614,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6138,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13972,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1560056446",
+			"profile_link_color": "0046D3",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"I could invert the photograph and enlarge the text so you could read it,\" @PeteSouza  writes about @BarackObama reading a memo in the Red Room in 2009. \"But let me save you the time and the trouble:⁣ It doesn’t include some fake deal with Mexico.\" https://t.co/RxCrw4QaDj https://t.co/Xwg4tQcgyE"
+	},
+	{
 		"created_at": "Fri Jun 14 13:41:15 +0000 2019",
 		"id": 1139528226416390100,
 		"id_str": "1139528226416390147",
