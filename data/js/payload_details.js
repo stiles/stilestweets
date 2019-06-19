c34245fd32ed387011c0ef376398df79a3bb2967
Matt Stiles <mattstiles@gmail.com>
2019-06-18 17:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13840,
-	"created_at": "Tue Jun 18 14:20:08 +0000 2019",
+	"tweets": 13841,
+	"created_at": "Wed Jun 19 00:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Tue Jun 18 23:52:37 +0000 2019",
+		"id": 1141131632973189100,
+		"id_str": "1141131632973189120",
+		"full_text": "In famously overworked Japan, there's a new hit TV show about whether workers can make leave the office on time. “I Will Not Work Overtime, Period!” Fun but revealing piece by @benjamindooley and @eimiyamamitsu.   https://t.co/xQ6aLiZAvy https://t.co/Ia7dbKH0ie",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "BenjaminDooley",
+					"name": "Ben Dooley",
+					"id": 305130750,
+					"id_str": "305130750",
+					"indices": [
+						176,
+						191
+					]
+				},
+				{
+					"screen_name": "eimiyamamitsu",
+					"name": "Eimi Yamamitsu | 山光瑛美",
+					"id": 4481535434,
+					"id_str": "4481535434",
+					"indices": [
+						196,
+						210
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/xQ6aLiZAvy",
+					"expanded_url": "https://www.nytimes.com/2019/06/18/business/japan-work-overtime-tv-show.html",
+					"display_url": "nytimes.com/2019/06/18/bus…",
+					"indices": [
+						214,
+						237
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1141131628716023800,
+					"id_str": "1141131628716023808",
+					"indices": [
+						238,
+						261
+					],
+					"media_url": "http://pbs.twimg.com/media/D9YdK5uU4AA9kpp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D9YdK5uU4AA9kpp.jpg",
+					"url": "https://t.co/Ia7dbKH0ie",
+					"display_url": "pic.twitter.com/Ia7dbKH0ie",
+					"expanded_url": "https://twitter.com/stiles/status/1141131632973189120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 683,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 683,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1141131628716023800,
+					"id_str": "1141131628716023808",
+					"indices": [
+						238,
+						261
+					],
+					"media_url": "http://pbs.twimg.com/media/D9YdK5uU4AA9kpp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D9YdK5uU4AA9kpp.jpg",
+					"url": "https://t.co/Ia7dbKH0ie",
+					"display_url": "pic.twitter.com/Ia7dbKH0ie",
+					"expanded_url": "https://twitter.com/stiles/status/1141131632973189120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 454,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1024,
+							"h": 683,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1024,
+							"h": 683,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10849,
+			"friends_count": 1625,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6180,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14009,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1560865290",
+			"profile_link_color": "7E3E3F",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In famously overworked Japan, there's a new hit TV show about whether workers can make leave the office on time. “I Will Not Work Overtime, Period!” Fun but revealing piece by @benjamindooley and @eimiyamamitsu.   https://t.co/xQ6aLiZAvy https://t.co/Ia7dbKH0ie"
+	},
+	{
 		"created_at": "Tue Jun 18 13:44:05 +0000 2019",
 		"id": 1140972807121231900,
 		"id_str": "1140978492886020096",
