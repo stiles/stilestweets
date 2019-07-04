7c5998d293c9361b5125ef67b534bd268eedc93a
Matt Stiles <mattstiles@gmail.com>
2019-07-04 12:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13881,
-	"created_at": "Thu Jul 04 18:20:05 +0000 2019",
+	"tweets": 13882,
+	"created_at": "Thu Jul 04 19:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,99 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 04 18:25:40 +0000 2019",
+		"id": 1146843295424774100,
+		"id_str": "1146847559811391488",
+		"full_text": "Woke me up! Happy 4th. https://t.co/EIuRxHAiJO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			46
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/EIuRxHAiJO",
+					"expanded_url": "https://www.latimes.com/local/lanow/la-me-earthquake-california-shake-quake-20190704-story.html",
+					"display_url": "latimes.com/local/lanow/la…",
+					"indices": [
+						23,
+						46
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 53286916,
+			"id_str": "53286916",
+			"name": "Casey Miller",
+			"screen_name": "caseymmiller",
+			"location": "Los Angeles, CA",
+			"description": "Disasters @LATdatadesk. Previously @Mapbox & @voxmediainc. Proud @UNCMJSchool Alum. Unofficial Bojangles’ brand rep. Stevie Nicks 5ever 🕊️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1603,
+			"friends_count": 924,
+			"listed_count": 77,
+			"created_at": "Fri Jul 03 03:52:17 +0000 2009",
+			"favourites_count": 9329,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7239,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ABB8C2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1128851060322447361/vQaIXD97_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1128851060322447361/vQaIXD97_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/53286916/1489677936",
+			"profile_link_color": "A14EE5",
+			"profile_sidebar_border_color": "E5C99F",
+			"profile_sidebar_fill_color": "4E164A",
+			"profile_text_color": "C2A781",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Woke me up! Happy 4th. https://t.co/EIuRxHAiJO"
+	},
+	{
 		"created_at": "Thu Jul 04 18:00:25 +0000 2019",
 		"id": 1146841207617671200,
 		"id_str": "1146841207617671168",
