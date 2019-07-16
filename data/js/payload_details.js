c04fd473a3f3a5a759075ded21b3622cc015ff63
Matt Stiles <mattstiles@gmail.com>
2019-07-15 21:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13927,
-	"created_at": "Mon Jul 15 14:20:05 +0000 2019",
+	"tweets": 13928,
+	"created_at": "Tue Jul 16 04:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,211 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Tue Jul 16 03:31:58 +0000 2019",
+		"id": 1150971308319969300,
+		"id_str": "1150971308319969280",
+		"full_text": "I was on S. Korean radio today with @RoyRivenburg. Stumbled upon his 1993 Column One feature in @latimes about the 'city of cemeteries' Colma, CA. This lede is so good... https://t.co/Wq4vHUNQsW https://t.co/U02yGQ8ZxV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			194
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "RoyRivenburg",
+					"name": "Roy Rivenburg",
+					"id": 4657732921,
+					"id_str": "4657732921",
+					"indices": [
+						36,
+						49
+					]
+				},
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						96,
+						104
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/Wq4vHUNQsW",
+					"expanded_url": "https://www.latimes.com/archives/la-xpm-1993-11-12-mn-56136-story.html",
+					"display_url": "latimes.com/archives/la-xp…",
+					"indices": [
+						171,
+						194
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1150971303383322600,
+					"id_str": "1150971303383322624",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/D_kSTWTU0AAgpL0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D_kSTWTU0AAgpL0.jpg",
+					"url": "https://t.co/U02yGQ8ZxV",
+					"display_url": "pic.twitter.com/U02yGQ8ZxV",
+					"expanded_url": "https://twitter.com/stiles/status/1150971308319969280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1554,
+							"h": 786,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 607,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 344,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1150971303383322600,
+					"id_str": "1150971303383322624",
+					"indices": [
+						195,
+						218
+					],
+					"media_url": "http://pbs.twimg.com/media/D_kSTWTU0AAgpL0.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D_kSTWTU0AAgpL0.jpg",
+					"url": "https://t.co/U02yGQ8ZxV",
+					"display_url": "pic.twitter.com/U02yGQ8ZxV",
+					"expanded_url": "https://twitter.com/stiles/status/1150971308319969280/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1554,
+							"h": 786,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 607,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 344,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10870,
+			"friends_count": 1655,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6284,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14103,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1562644645",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I was on S. Korean radio today with @RoyRivenburg. Stumbled upon his 1993 Column One feature in @latimes about the 'city of cemeteries' Colma, CA. This lede is so good... https://t.co/Wq4vHUNQsW https://t.co/U02yGQ8ZxV"
+	},
+	{
 		"created_at": "Mon Jul 15 14:09:40 +0000 2019",
 		"id": 1150625764158480400,
 		"id_str": "1150769404562399232",
