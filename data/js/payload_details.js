9c28de8021c96e53d9cc5e12d20af7e6d98bda55
Matt Stiles <mattstiles@gmail.com>
2019-07-23 15:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13961,
-	"created_at": "Tue Jul 23 20:20:04 +0000 2019",
+	"tweets": 13962,
+	"created_at": "Tue Jul 23 22:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Tue Jul 23 22:03:18 +0000 2019",
+		"id": 1153787701142777900,
+		"id_str": "1153787701142777856",
+		"full_text": "Oh, there's a fancy and bubbly new color palette in @googledocs. https://t.co/lWzWikg3D2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			64
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "googledocs",
+					"name": "Google Docs",
+					"id": 488683847,
+					"id_str": "488683847",
+					"indices": [
+						52,
+						63
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1153787695543382000,
+					"id_str": "1153787695543382018",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/EAMTy8sUcAI1iWg.png",
+					"media_url_https": "https://pbs.twimg.com/media/EAMTy8sUcAI1iWg.png",
+					"url": "https://t.co/lWzWikg3D2",
+					"display_url": "pic.twitter.com/lWzWikg3D2",
+					"expanded_url": "https://twitter.com/stiles/status/1153787701142777856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 314,
+							"h": 241,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 314,
+							"h": 241,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 241,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1153787695543382000,
+					"id_str": "1153787695543382018",
+					"indices": [
+						65,
+						88
+					],
+					"media_url": "http://pbs.twimg.com/media/EAMTy8sUcAI1iWg.png",
+					"media_url_https": "https://pbs.twimg.com/media/EAMTy8sUcAI1iWg.png",
+					"url": "https://t.co/lWzWikg3D2",
+					"display_url": "pic.twitter.com/lWzWikg3D2",
+					"expanded_url": "https://twitter.com/stiles/status/1153787701142777856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 314,
+							"h": 241,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 314,
+							"h": 241,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 314,
+							"h": 241,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10875,
+			"friends_count": 1657,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6329,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14148,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1562644645",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Oh, there's a fancy and bubbly new color palette in @googledocs. https://t.co/lWzWikg3D2"
+	},
+	{
 		"created_at": "Tue Jul 23 19:41:19 +0000 2019",
 		"id": 1153751967723712500,
 		"id_str": "1153751967723712512",
