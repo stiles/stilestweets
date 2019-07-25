b4708432f0457619114623820566cba7ad9aa874
Matt Stiles <mattstiles@gmail.com>
2019-07-25 10:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13969,
-	"created_at": "Thu Jul 25 02:20:04 +0000 2019",
+	"tweets": 13970,
+	"created_at": "Thu Jul 25 17:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 25 16:57:28 +0000 2019",
+		"id": 1154206856635535400,
+		"id_str": "1154435508149207040",
+		"full_text": "We did it, guys. \n\nThe @USGS shakemap of the 7.1 magnitude July 5 earthquake printed in 3D.\n\nTotal cost: $0.00.\n\nAll thanks to the free resources at @laplcentral's Octavia Lab. https://t.co/ESiTJFL4y5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			176
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "USGS",
+					"name": "USGS",
+					"id": 14505838,
+					"id_str": "14505838",
+					"indices": [
+						23,
+						28
+					]
+				},
+				{
+					"screen_name": "laplcentral",
+					"name": "Central Library",
+					"id": 81743882,
+					"id_str": "81743882",
+					"indices": [
+						149,
+						161
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1154206846342684700,
+					"id_str": "1154206846342684674",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/EASRAvGUYAIhH9v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EASRAvGUYAIhH9v.jpg",
+					"url": "https://t.co/ESiTJFL4y5",
+					"display_url": "pic.twitter.com/ESiTJFL4y5",
+					"expanded_url": "https://twitter.com/palewire/status/1154206856635535360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 496,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 876,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1495,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1154206846342684700,
+					"id_str": "1154206846342684674",
+					"indices": [
+						177,
+						200
+					],
+					"media_url": "http://pbs.twimg.com/media/EASRAvGUYAIhH9v.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EASRAvGUYAIhH9v.jpg",
+					"url": "https://t.co/ESiTJFL4y5",
+					"display_url": "pic.twitter.com/ESiTJFL4y5",
+					"expanded_url": "https://twitter.com/palewire/status/1154206856635535360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 496,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 876,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1495,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1152696094498807800,
+		"in_reply_to_status_id_str": "1152696094498807808",
+		"in_reply_to_user_id": 14559196,
+		"in_reply_to_user_id_str": "14559196",
+		"in_reply_to_screen_name": "palewire",
+		"user": {
+			"id": 14559196,
+			"id_str": "14559196",
+			"name": "Ben Welsh",
+			"screen_name": "palewire",
+			"location": "Live and direct",
+			"description": "Editor, @LATdatadesk at @latimes · Organizer, https://t.co/lZvqTdDOmO · Archivist, https://t.co/CaO6K276g3 · Send tips to ben.welsh@latimes.com",
+			"url": "https://t.co/kogJ0fJceo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kogJ0fJceo",
+							"expanded_url": "http://palewi.re/who-is-ben-welsh/",
+							"display_url": "palewi.re/who-is-ben-wel…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/lZvqTdDOmO",
+							"expanded_url": "http://www.californiacivicdata.org",
+							"display_url": "californiacivicdata.org",
+							"indices": [
+								46,
+								69
+							]
+						},
+						{
+							"url": "https://t.co/CaO6K276g3",
+							"expanded_url": "http://pastpages.org",
+							"display_url": "pastpages.org",
+							"indices": [
+								83,
+								106
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9206,
+			"friends_count": 3068,
+			"listed_count": 712,
+			"created_at": "Sun Apr 27 16:10:55 +0000 2008",
+			"favourites_count": 8,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 55189,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14559196/1423764862",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDDDDD",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 45,
+		"favorite_count": 195,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We did it, guys. \n\nThe @USGS shakemap of the 7.1 magnitude July 5 earthquake printed in 3D.\n\nTotal cost: $0.00.\n\nAll thanks to the free resources at @laplcentral's Octavia Lab. https://t.co/ESiTJFL4y5"
+	},
+	{
 		"created_at": "Thu Jul 25 01:33:11 +0000 2019",
 		"id": 1154135884809134000,
 		"id_str": "1154202904485494784",
