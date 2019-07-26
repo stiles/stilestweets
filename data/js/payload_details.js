00e7ee1d5959064c32ce750f6485f1def92cb432
Matt Stiles <mattstiles@gmail.com>
2019-07-25 17:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 13974,
-	"created_at": "Thu Jul 25 20:20:05 +0000 2019",
+	"tweets": 13975,
+	"created_at": "Fri Jul 26 00:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,257 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 25 23:55:18 +0000 2019",
+		"id": 1154540660654895000,
+		"id_str": "1154540660654895104",
+		"full_text": "@lomikriel @joe_kehoskie @keribla https://t.co/FMgp27q3kk",
+		"truncated": false,
+		"display_text_range": [
+			33,
+			33
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "lomikriel",
+					"name": "lomikriel",
+					"id": 15098932,
+					"id_str": "15098932",
+					"indices": [
+						0,
+						10
+					]
+				},
+				{
+					"screen_name": "joe_kehoskie",
+					"name": "Joe Kehoskie",
+					"id": 41037605,
+					"id_str": "41037605",
+					"indices": [
+						11,
+						24
+					]
+				},
+				{
+					"screen_name": "keribla",
+					"name": "Keri Blakinger",
+					"id": 71424847,
+					"id_str": "71424847",
+					"indices": [
+						25,
+						33
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1154540651188379600,
+					"id_str": "1154540651188379648",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EAXAmvDU4AAnPor.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EAXAmvDU4AAnPor.jpg",
+					"url": "https://t.co/FMgp27q3kk",
+					"display_url": "pic.twitter.com/FMgp27q3kk",
+					"expanded_url": "https://twitter.com/stiles/status/1154540660654895104/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 476,
+							"h": 252,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 476,
+							"h": 252,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 476,
+							"h": 252,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1154540651188379600,
+					"id_str": "1154540651188379648",
+					"indices": [
+						34,
+						57
+					],
+					"media_url": "http://pbs.twimg.com/tweet_video_thumb/EAXAmvDU4AAnPor.jpg",
+					"media_url_https": "https://pbs.twimg.com/tweet_video_thumb/EAXAmvDU4AAnPor.jpg",
+					"url": "https://t.co/FMgp27q3kk",
+					"display_url": "pic.twitter.com/FMgp27q3kk",
+					"expanded_url": "https://twitter.com/stiles/status/1154540660654895104/photo/1",
+					"type": "animated_gif",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 476,
+							"h": 252,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 476,
+							"h": 252,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 476,
+							"h": 252,
+							"resize": "fit"
+						}
+					},
+					"video_info": {
+						"aspect_ratio": [
+							17,
+							9
+						],
+						"variants": [
+							{
+								"bitrate": 0,
+								"content_type": "video/mp4",
+								"url": "https://video.twimg.com/tweet_video/EAXAmvDU4AAnPor.mp4"
+							}
+						]
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1154539881303031800,
+		"in_reply_to_status_id_str": "1154539881303031808",
+		"in_reply_to_user_id": 15098932,
+		"in_reply_to_user_id_str": "15098932",
+		"in_reply_to_screen_name": "lomikriel",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10873,
+			"friends_count": 1658,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6346,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14164,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1562644645",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "38ea9782c8d83a4b",
+			"url": "https://api.twitter.com/1.1/geo/id/38ea9782c8d83a4b.json",
+			"place_type": "city",
+			"name": "Marina del Rey",
+			"full_name": "Marina del Rey, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.463481,
+							33.964019
+						],
+						[
+							-118.4321992,
+							33.964019
+						],
+						[
+							-118.4321992,
+							33.98647
+						],
+						[
+							-118.463481,
+							33.98647
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "und",
+		"text": "@lomikriel @joe_kehoskie @keribla https://t.co/FMgp27q3kk"
+	},
+	{
 		"created_at": "Thu Jul 25 19:32:23 +0000 2019",
 		"id": 1154474497447084000,
 		"id_str": "1154474497447084033",
