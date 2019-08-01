88fb0fb65ba214af77ea468b4f22f754877b3e88
Matt Stiles <mattstiles@gmail.com>
2019-08-01 16:20:04 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14008,
-	"created_at": "Thu Aug 01 20:20:05 +0000 2019",
+	"tweets": 14009,
+	"created_at": "Thu Aug 01 23:20:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Thu Aug 01 22:40:26 +0000 2019",
+		"id": 1157058533348634600,
+		"id_str": "1157058533348634624",
+		"full_text": "The family of Anthony Avalos alleges that social workers willfully disregarded concerns about abuse and failed protect the boy before his 2018 death. Now they're suing @CountyofLA. My latest story on the county's troubled child welfare agency, DCFS: https://t.co/rucPZRqdQe https://t.co/KWmevEWnjd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CountyofLA",
+					"name": "Los Angeles County",
+					"id": 193771633,
+					"id_str": "193771633",
+					"indices": [
+						168,
+						179
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rucPZRqdQe",
+					"expanded_url": "https://www.latimes.com/california/story/2019-08-01/dcfs-lawsuit-child-welfare-death-anthony-avalos",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						250,
+						273
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1157058529636712400,
+					"id_str": "1157058529636712448",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/EA6ymjNVAAAXZgA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA6ymjNVAAAXZgA.jpg",
+					"url": "https://t.co/KWmevEWnjd",
+					"display_url": "pic.twitter.com/KWmevEWnjd",
+					"expanded_url": "https://twitter.com/stiles/status/1157058533348634624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157058529636712400,
+					"id_str": "1157058529636712448",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/EA6ymjNVAAAXZgA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA6ymjNVAAAXZgA.jpg",
+					"url": "https://t.co/KWmevEWnjd",
+					"display_url": "pic.twitter.com/KWmevEWnjd",
+					"expanded_url": "https://twitter.com/stiles/status/1157058533348634624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10884,
+			"friends_count": 1668,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6388,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14205,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1564124417",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The family of Anthony Avalos alleges that social workers willfully disregarded concerns about abuse and failed protect the boy before his 2018 death. Now they're suing @CountyofLA. My latest story on the county's troubled child welfare agency, DCFS: https://t.co/rucPZRqdQe https://t.co/KWmevEWnjd"
+	},
+	{
 		"created_at": "Thu Aug 01 19:36:09 +0000 2019",
 		"id": 1157012159684997000,
 		"id_str": "1157012159684997120",
