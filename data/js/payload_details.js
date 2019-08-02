d0c785b29a13f737d64ce34fdd3b55a746023b33
Matt Stiles <mattstiles@gmail.com>
2019-08-01 18:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14009,
-	"created_at": "Thu Aug 01 23:20:03 +0000 2019",
+	"tweets": 14010,
+	"created_at": "Fri Aug 02 01:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,227 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 02 01:13:32 +0000 2019",
+		"id": 1157081702008672300,
+		"id_str": "1157097064095866880",
+		"full_text": "Heavily-redacted docs (finally) released by DCFS indicated the alleged abuse against Noah Cuatro began almost immediately after he was born. @KNX1070 https://t.co/BRhtmqcuPu",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			149
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KNX1070",
+					"name": "KNX 1070 NEWSRADIO",
+					"id": 18149465,
+					"id_str": "18149465",
+					"indices": [
+						141,
+						149
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1157081688901468200,
+					"id_str": "1157081688901468161",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/EA7HqmMUwAEwnz4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA7HqmMUwAEwnz4.jpg",
+					"url": "https://t.co/BRhtmqcuPu",
+					"display_url": "pic.twitter.com/BRhtmqcuPu",
+					"expanded_url": "https://twitter.com/ReporterClaudia/status/1157081702008672257/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 746,
+							"h": 557,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 746,
+							"h": 557,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 508,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157081688901468200,
+					"id_str": "1157081688901468161",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/EA7HqmMUwAEwnz4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA7HqmMUwAEwnz4.jpg",
+					"url": "https://t.co/BRhtmqcuPu",
+					"display_url": "pic.twitter.com/BRhtmqcuPu",
+					"expanded_url": "https://twitter.com/ReporterClaudia/status/1157081702008672257/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 746,
+							"h": 557,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 746,
+							"h": 557,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 508,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1157081688922448000,
+					"id_str": "1157081688922447874",
+					"indices": [
+						150,
+						173
+					],
+					"media_url": "http://pbs.twimg.com/media/EA7HqmRU4AIB_jG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA7HqmRU4AIB_jG.jpg",
+					"url": "https://t.co/BRhtmqcuPu",
+					"display_url": "pic.twitter.com/BRhtmqcuPu",
+					"expanded_url": "https://twitter.com/ReporterClaudia/status/1157081702008672257/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 750,
+							"h": 532,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 482,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 750,
+							"h": 532,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 301189264,
+			"id_str": "301189264",
+			"name": "Claudia Peschiutta",
+			"screen_name": "ReporterClaudia",
+			"location": "Los Angeles",
+			"description": "News with a side of humor. I work @KNX1070. RTs are not endorsements.",
+			"url": "https://t.co/nJac11hlei",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nJac11hlei",
+							"expanded_url": "http://knx1070.com",
+							"display_url": "knx1070.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 8781,
+			"friends_count": 1367,
+			"listed_count": 377,
+			"created_at": "Thu May 19 01:33:28 +0000 2011",
+			"favourites_count": 11911,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 32992,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1106781873223553025/BOU1Q9Ij_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1106781873223553025/BOU1Q9Ij_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/301189264/1407910303",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Heavily-redacted docs (finally) released by DCFS indicated the alleged abuse against Noah Cuatro began almost immediately after he was born. @KNX1070 https://t.co/BRhtmqcuPu"
+	},
+	{
 		"created_at": "Thu Aug 01 22:40:26 +0000 2019",
 		"id": 1157058533348634600,
 		"id_str": "1157058533348634624",
