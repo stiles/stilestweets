eb386b2ea8fde9503347c0c07a1a94ac22e671e4
Matt Stiles <mattstiles@gmail.com>
2019-08-07 09:20:08 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14040,
-	"created_at": "Wed Aug 07 03:20:05 +0000 2019",
+	"tweets": 14041,
+	"created_at": "Wed Aug 07 16:20:07 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 07 15:52:25 +0000 2019",
+		"id": 1158958937531658200,
+		"id_str": "1159130179890925570",
+		"full_text": "24 hours ago, in front of a teleprompter, Trump said this:\n\n\"Now is the time to set destructive partisanship aside, so destructive, and find the courage to answer hatred with unity, devotion, and love.\"\n\nTonight on Twitter, he said this: https://t.co/Lur9v90nzo",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			237
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Lur9v90nzo",
+					"expanded_url": "https://twitter.com/realDonaldTrump/status/1158950196560171014",
+					"display_url": "twitter.com/realDonaldTrum…",
+					"indices": [
+						238,
+						261
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 282545479,
+			"id_str": "282545479",
+			"name": "Michael Barbaro",
+			"screen_name": "mikiebarb",
+			"location": "New York, NY",
+			"description": "Host of The Daily.",
+			"url": "https://t.co/8JpN3gLjgE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8JpN3gLjgE",
+							"expanded_url": "https://www.nytimes.com/newsletters/the-daily",
+							"display_url": "nytimes.com/newsletters/th…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 179294,
+			"friends_count": 1644,
+			"listed_count": 3123,
+			"created_at": "Fri Apr 15 12:18:28 +0000 2011",
+			"favourites_count": 17445,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 46228,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093249745957634050/qtkKg82m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093249745957634050/qtkKg82m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282545479/1538770528",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1158950196560171000,
+		"quoted_status_id_str": "1158950196560171014",
+		"quoted_status_permalink": {
+			"url": "https://t.co/Lur9v90nzo",
+			"expanded": "https://twitter.com/realDonaldTrump/status/1158950196560171014",
+			"display": "twitter.com/realDonaldTrum…"
+		},
+		"quoted_status": {
+			"created_at": "Wed Aug 07 03:57:13 +0000 2019",
+			"id": 1158950196560171000,
+			"id_str": "1158950196560171014",
+			"full_text": "Beto (phony name to indicate Hispanic heritage) O’Rourke, who is embarrassed by my last visit to the Great State of Texas, where I trounced him, and is now even more embarrassed by polling at 1% in the Democrat Primary, should respect the victims &amp; law enforcement - &amp; be quiet!",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				286
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 25073877,
+				"id_str": "25073877",
+				"name": "Donald J. Trump",
+				"screen_name": "realDonaldTrump",
+				"location": "Washington, DC",
+				"description": "45th President of the United States of America🇺🇸",
+				"url": "https://t.co/OMxB0x7xC5",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/OMxB0x7xC5",
+								"expanded_url": "http://www.Instagram.com/realDonaldTrump",
+								"display_url": "Instagram.com/realDonaldTrump",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 62806958,
+				"friends_count": 47,
+				"listed_count": 106463,
+				"created_at": "Wed Mar 18 13:46:38 +0000 2009",
+				"favourites_count": 8,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 43355,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": true,
+				"profile_background_color": "6D5C18",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/874276197357596672/kUuht00m_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/25073877/1560920145",
+				"profile_link_color": "1B95E0",
+				"profile_sidebar_border_color": "BDDCAD",
+				"profile_sidebar_fill_color": "C5CEC0",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": true,
+				"translator_type": "regular"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 25575,
+			"favorite_count": 107363,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 5119,
+		"favorite_count": 15527,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "24 hours ago, in front of a teleprompter, Trump said this:\n\n\"Now is the time to set destructive partisanship aside, so destructive, and find the courage to answer hatred with unity, devotion, and love.\"\n\nTonight on Twitter, he said this: https://t.co/Lur9v90nzo"
+	},
+	{
 		"created_at": "Wed Aug 07 02:43:37 +0000 2019",
 		"id": 1158797162513236000,
 		"id_str": "1158931674270294018",
