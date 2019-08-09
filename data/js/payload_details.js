8e89de1cefd2f0fbb136ac251f0aadc686e5c014
Matt Stiles <mattstiles@gmail.com>
2019-08-09 09:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14050,
-	"created_at": "Fri Aug 09 15:20:06 +0000 2019",
+	"tweets": 14051,
+	"created_at": "Fri Aug 09 16:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,195 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 09 15:23:28 +0000 2019",
+		"id": 1159847669767913500,
+		"id_str": "1159847669767913475",
+		"full_text": "@dancow @jashkenas @mbostock @tmcw @observablehq @LATdatadesk That's what I get for tweeting it. Oops.",
+		"truncated": false,
+		"display_text_range": [
+			62,
+			102
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "dancow",
+					"name": "Dan Nguyen",
+					"id": 14335332,
+					"id_str": "14335332",
+					"indices": [
+						0,
+						7
+					]
+				},
+				{
+					"screen_name": "jashkenas",
+					"name": "Jeremy Ashkenas",
+					"id": 123323498,
+					"id_str": "123323498",
+					"indices": [
+						8,
+						18
+					]
+				},
+				{
+					"screen_name": "mbostock",
+					"name": "Mike Bostock",
+					"id": 43593,
+					"id_str": "43593",
+					"indices": [
+						19,
+						28
+					]
+				},
+				{
+					"screen_name": "tmcw",
+					"name": "Tom MacWright",
+					"id": 1458271,
+					"id_str": "1458271",
+					"indices": [
+						29,
+						34
+					]
+				},
+				{
+					"screen_name": "observablehq",
+					"name": "Observable",
+					"id": 905255756789825500,
+					"id_str": "905255756789825536",
+					"indices": [
+						35,
+						48
+					]
+				},
+				{
+					"screen_name": "LATdatadesk",
+					"name": "The Data Desk",
+					"id": 17918860,
+					"id_str": "17918860",
+					"indices": [
+						49,
+						61
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1159846851073495000,
+		"in_reply_to_status_id_str": "1159846851073495040",
+		"in_reply_to_user_id": 14335332,
+		"in_reply_to_user_id_str": "14335332",
+		"in_reply_to_screen_name": "dancow",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10907,
+			"friends_count": 1677,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6438,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14259,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1564124417",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3b77caf94bfc81fe",
+			"url": "https://api.twitter.com/1.1/geo/id/3b77caf94bfc81fe.json",
+			"place_type": "city",
+			"name": "Los Angeles",
+			"full_name": "Los Angeles, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.668404,
+							33.704538
+						],
+						[
+							-118.155409,
+							33.704538
+						],
+						[
+							-118.155409,
+							34.337041
+						],
+						[
+							-118.668404,
+							34.337041
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@dancow @jashkenas @mbostock @tmcw @observablehq @LATdatadesk That's what I get for tweeting it. Oops."
+	},
+	{
 		"created_at": "Fri Aug 09 15:03:24 +0000 2019",
 		"id": 1159842621897859000,
 		"id_str": "1159842621897859072",
