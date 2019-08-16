b6a51165c4328da32d20800bce4ee4506c30e6b8
Matt Stiles <mattstiles@gmail.com>
2019-08-15 22:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14076,
-	"created_at": "Fri Aug 16 03:20:06 +0000 2019",
+	"tweets": 14077,
+	"created_at": "Fri Aug 16 05:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 16 05:11:21 +0000 2019",
+		"id": 1162230340548567000,
+		"id_str": "1162230340548567041",
+		"full_text": "Trump, banking on strong economy to win reelection, frets over a possible downturn https://t.co/tdWXAG1aT8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			106
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tdWXAG1aT8",
+					"expanded_url": "https://wapo.st/2P13zss?tid=ss_tw",
+					"display_url": "wapo.st/2P13zss?tid=ss…",
+					"indices": [
+						83,
+						106
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Digital muckraker @latimes, covering the @CountyofLA beat w/ words, data, visuals & code. Former Korea correspondent. Texan. Past: @wsj @NPR @TexasTribune",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10936,
+			"friends_count": 1688,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6470,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14294,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1564124417",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Trump, banking on strong economy to win reelection, frets over a possible downturn https://t.co/tdWXAG1aT8"
+	},
+	{
 		"created_at": "Fri Aug 16 02:43:59 +0000 2019",
 		"id": 1162103800372445200,
 		"id_str": "1162193254734942210",
@@ -297,7 +403,7 @@ Grailbird.data.tweets_2019_08 =
 				"name": "Anthony Pesce 🦅",
 				"screen_name": "anthonyjpesce",
 				"location": "Los Angeles",
-				"description": "Data Journalist @latimes & @latdatadesk, co-chair of @latguild, he/him 🏳️‍🌈. Public key: https://t.co/pO61farAsH",
+				"description": "Data Journalist @latimes & @latdatadesk, co-chair of @latguild, he/him 🏳️🌈. Public key: https://t.co/pO61farAsH",
 				"url": "https://t.co/zcMKx1eqZx",
 				"entities": {
 					"url": {
