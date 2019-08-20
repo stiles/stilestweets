0a07ab6010b7223a5f12a5fd9ff81976d33d20d5
Matt Stiles <mattstiles@gmail.com>
2019-08-20 09:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14097,
-	"created_at": "Tue Aug 20 06:20:05 +0000 2019",
+	"tweets": 14098,
+	"created_at": "Tue Aug 20 16:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,185 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Tue Aug 20 15:55:53 +0000 2019",
+		"id": 1163842097008431000,
+		"id_str": "1163842097008431105",
+		"full_text": "@chimeraelf @TheBradBlog @latimes @LACountyRRCC @DCLogan LA will have countywide mail ballots in a couple of cycles. Meanwhile, anyone who wants one can I get a mail ballot now. Not everyone wants one, though. Maybe that will change soon. Maybe not.",
+		"truncated": false,
+		"display_text_range": [
+			57,
+			249
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "chimeraelf",
+					"name": "boricuabot #AbolishLaJunta",
+					"id": 82538231,
+					"id_str": "82538231",
+					"indices": [
+						0,
+						11
+					]
+				},
+				{
+					"screen_name": "TheBradBlog",
+					"name": "Brad Friedman",
+					"id": 24268066,
+					"id_str": "24268066",
+					"indices": [
+						12,
+						24
+					]
+				},
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						25,
+						33
+					]
+				},
+				{
+					"screen_name": "LACountyRRCC",
+					"name": "Dean Logan, RR/CC",
+					"id": 21807663,
+					"id_str": "21807663",
+					"indices": [
+						34,
+						47
+					]
+				},
+				{
+					"screen_name": "DCLogan",
+					"name": "Dean Logan",
+					"id": 23175052,
+					"id_str": "23175052",
+					"indices": [
+						48,
+						56
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": 1163788359657054200,
+		"in_reply_to_status_id_str": "1163788359657054209",
+		"in_reply_to_user_id": 82538231,
+		"in_reply_to_user_id_str": "82538231",
+		"in_reply_to_screen_name": "chimeraelf",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA. Fan of structured beat reporting with data, visuals & code. Past: @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10949,
+			"friends_count": 1695,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6480,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14319,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1564124417",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3b77caf94bfc81fe",
+			"url": "https://api.twitter.com/1.1/geo/id/3b77caf94bfc81fe.json",
+			"place_type": "city",
+			"name": "Los Angeles",
+			"full_name": "Los Angeles, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.668404,
+							33.704538
+						],
+						[
+							-118.155409,
+							33.704538
+						],
+						[
+							-118.155409,
+							34.337041
+						],
+						[
+							-118.668404,
+							34.337041
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@chimeraelf @TheBradBlog @latimes @LACountyRRCC @DCLogan LA will have countywide mail ballots in a couple of cycles. Meanwhile, anyone who wants one can I get a mail ballot now. Not everyone wants one, though. Maybe that will change soon. Maybe not."
+	},
+	{
 		"created_at": "Tue Aug 20 05:36:56 +0000 2019",
 		"id": 1163665066375274500,
 		"id_str": "1163686330947080193",
