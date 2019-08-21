80c4a6c18845884d9957491febe79e40af337027
Matt Stiles <mattstiles@gmail.com>
2019-08-21 15:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14102,
-	"created_at": "Wed Aug 21 20:20:05 +0000 2019",
+	"tweets": 14103,
+	"created_at": "Wed Aug 21 22:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 21 21:50:21 +0000 2019",
+		"id": 1164293687674695700,
+		"id_str": "1164293687674695680",
+		"full_text": "The GIS-plosion that happens when users make a correct choice on the @nytimes'  pictures quiz is the greatest. Is that StateFace or something? https://t.co/GCcsZmYyFz https://t.co/0K1tTTCysG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nytimes",
+					"name": "The New York Times",
+					"id": 807095,
+					"id_str": "807095",
+					"indices": [
+						69,
+						77
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/GCcsZmYyFz",
+					"expanded_url": "https://www.nytimes.com/interactive/2019/travel/50-states-photo-quiz.html",
+					"display_url": "nytimes.com/interactive/20…",
+					"indices": [
+						143,
+						166
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1164293683493007400,
+					"id_str": "1164293683493007360",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EChm77rU8AA4b4x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EChm77rU8AA4b4x.jpg",
+					"url": "https://t.co/0K1tTTCysG",
+					"display_url": "pic.twitter.com/0K1tTTCysG",
+					"expanded_url": "https://twitter.com/stiles/status/1164293687674695680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 541,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 921,
+							"h": 733,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 921,
+							"h": 733,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1164293683493007400,
+					"id_str": "1164293683493007360",
+					"indices": [
+						167,
+						190
+					],
+					"media_url": "http://pbs.twimg.com/media/EChm77rU8AA4b4x.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EChm77rU8AA4b4x.jpg",
+					"url": "https://t.co/0K1tTTCysG",
+					"display_url": "pic.twitter.com/0K1tTTCysG",
+					"expanded_url": "https://twitter.com/stiles/status/1164293687674695680/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 541,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 921,
+							"h": 733,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 921,
+							"h": 733,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA. Fan of structured beat reporting with data, visuals & code. Past: @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10950,
+			"friends_count": 1696,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6487,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14326,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1564124417",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The GIS-plosion that happens when users make a correct choice on the @nytimes'  pictures quiz is the greatest. Is that StateFace or something? https://t.co/GCcsZmYyFz https://t.co/0K1tTTCysG"
+	},
+	{
 		"created_at": "Wed Aug 21 19:53:03 +0000 2019",
 		"id": 1164264167127171000,
 		"id_str": "1164264167127171073",
