b4c32af302ca3a03c0fd1ae9c2fc3ae0fb6dfeb9
Matt Stiles <mattstiles@gmail.com>
2019-08-21 17:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14103,
-	"created_at": "Wed Aug 21 22:20:05 +0000 2019",
+	"tweets": 14104,
+	"created_at": "Thu Aug 22 00:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 21 23:33:24 +0000 2019",
+		"id": 1164319621417095200,
+		"id_str": "1164319621417095168",
+		"full_text": "As some of you have heard (https://t.co/p2cvISyXpK) a City of Santa Monica bus recently took off my car door as I got out of the vehicle on Westwood. My reporter training has since kicked in, and I can't wait to see my reaction in the driver's dash cam when the video is released. https://t.co/wzPuI0d4n1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/p2cvISyXpK",
+					"expanded_url": "https://twitter.com/elisewho/status/1159979059138351104?s=09",
+					"display_url": "twitter.com/elisewho/statu…",
+					"indices": [
+						27,
+						50
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1164319611195711500,
+					"id_str": "1164319611195711488",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/ECh-hH5XYAAyD_G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECh-hH5XYAAyD_G.jpg",
+					"url": "https://t.co/wzPuI0d4n1",
+					"display_url": "pic.twitter.com/wzPuI0d4n1",
+					"expanded_url": "https://twitter.com/stiles/status/1164319621417095168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1930,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1131,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 641,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1164319611195711500,
+					"id_str": "1164319611195711488",
+					"indices": [
+						281,
+						304
+					],
+					"media_url": "http://pbs.twimg.com/media/ECh-hH5XYAAyD_G.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ECh-hH5XYAAyD_G.jpg",
+					"url": "https://t.co/wzPuI0d4n1",
+					"display_url": "pic.twitter.com/wzPuI0d4n1",
+					"expanded_url": "https://twitter.com/stiles/status/1164319621417095168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1930,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1131,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 641,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA. Fan of structured beat reporting with data, visuals & code. Past: @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10953,
+			"friends_count": 1696,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6487,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14327,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1564124417",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "0654b676d0359a31",
+			"url": "https://api.twitter.com/1.1/geo/id/0654b676d0359a31.json",
+			"place_type": "city",
+			"name": "El Segundo",
+			"full_name": "El Segundo, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.429843,
+							33.901804
+						],
+						[
+							-118.370685,
+							33.901804
+						],
+						[
+							-118.370685,
+							33.931493
+						],
+						[
+							-118.429843,
+							33.931493
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1159979059138351000,
+		"quoted_status_id_str": "1159979059138351104",
+		"quoted_status_permalink": {
+			"url": "https://t.co/p2cvISyXpK",
+			"expanded": "https://twitter.com/elisewho/status/1159979059138351104?s=09",
+			"display": "twitter.com/elisewho/statu…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Aug 10 00:05:33 +0000 2019",
+			"id": 1159979059138351000,
+			"id_str": "1159979059138351104",
+			"full_text": "Also my husband was hit by a bus this wk, which um, in all my jokes/fantasies about my husband getting hit by a bus, I can't believe he really did. Ripped the door off the minivan but he's somehow unscathed! Ever the enterprising journo, he submitted FOIA request for bus-cam vid",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				279
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 16001350,
+				"id_str": "16001350",
+				"name": "Elise Hu",
+				"screen_name": "elisewho",
+				"location": "California, USA",
+				"description": "LA-based @NPR, covering the future. Ex-expat buro chief in Koreas & Japan. @TexasTribune & #Mizzou alum. Texan. Eater. Millennial ajumma 입니다. ehu@npr.org",
+				"url": "https://t.co/jq1dau4Q6g",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jq1dau4Q6g",
+								"expanded_url": "http://npr.org/futureyou",
+								"display_url": "npr.org/futureyou",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 41305,
+				"friends_count": 1967,
+				"listed_count": 1751,
+				"created_at": "Tue Aug 26 19:24:37 +0000 2008",
+				"favourites_count": 3278,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 30887,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "DDEDEE",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": true,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/886556243753447424/dlb8urBT_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/886556243753447424/dlb8urBT_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/16001350/1565204689",
+				"profile_link_color": "4E647A",
+				"profile_sidebar_border_color": "FFFFFF",
+				"profile_sidebar_fill_color": "F7F7F2",
+				"profile_text_color": "000000",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 17,
+			"favorite_count": 928,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 2,
+		"favorite_count": 18,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "As some of you have heard (https://t.co/p2cvISyXpK) a City of Santa Monica bus recently took off my car door as I got out of the vehicle on Westwood. My reporter training has since kicked in, and I can't wait to see my reaction in the driver's dash cam when the video is released. https://t.co/wzPuI0d4n1"
+	},
+	{
 		"created_at": "Wed Aug 21 21:50:21 +0000 2019",
 		"id": 1164293687674695700,
 		"id_str": "1164293687674695680",
