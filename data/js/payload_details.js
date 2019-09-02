94e5c3462458e437285a973cce110a785891cc83
Matt Stiles <mattstiles@gmail.com>
2019-09-01 23:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14133,
-	"created_at": "Sun Sep 01 20:20:05 +0000 2019",
+	"tweets": 14134,
+	"created_at": "Mon Sep 02 06:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 02 05:29:33 +0000 2019",
+		"id": 1168382805731971000,
+		"id_str": "1168395517669167106",
+		"full_text": "A small quake southeast of LA is an interesting test case for our Quakebot.\n\nhttps://t.co/OpHQNwWsgr\n\n@caseymmiller's new auto-generated shakemaps offer two zoom levels.\n\nWe've included both in this post, in hopes of giving both a \"near\" and a \"far\" context. We get it right? https://t.co/u9oZc1ugAa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "caseymmiller",
+					"name": "Casey Miller",
+					"id": 53286916,
+					"id_str": "53286916",
+					"indices": [
+						102,
+						115
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/OpHQNwWsgr",
+					"expanded_url": "https://www.latimes.com/california/story/2019-09-01/earthquake-reported-in-lake-elsinore-california",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1168382558238736400,
+					"id_str": "1168382558238736384",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EDbtvxGVAAAjntw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDbtvxGVAAAjntw.jpg",
+					"url": "https://t.co/u9oZc1ugAa",
+					"display_url": "pic.twitter.com/u9oZc1ugAa",
+					"expanded_url": "https://twitter.com/palewire/status/1168382805731971072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1168382558238736400,
+					"id_str": "1168382558238736384",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EDbtvxGVAAAjntw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDbtvxGVAAAjntw.jpg",
+					"url": "https://t.co/u9oZc1ugAa",
+					"display_url": "pic.twitter.com/u9oZc1ugAa",
+					"expanded_url": "https://twitter.com/palewire/status/1168382805731971072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1168382558247121000,
+					"id_str": "1168382558247120896",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EDbtvxIU8AA6-pw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDbtvxIU8AA6-pw.jpg",
+					"url": "https://t.co/u9oZc1ugAa",
+					"display_url": "pic.twitter.com/u9oZc1ugAa",
+					"expanded_url": "https://twitter.com/palewire/status/1168382805731971072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1600,
+							"h": 1600,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14559196,
+			"id_str": "14559196",
+			"name": "Ben Welsh",
+			"screen_name": "palewire",
+			"location": "Live and direct",
+			"description": "Editor, @LATdatadesk at @latimes · Organizer, https://t.co/lZvqTdDOmO · Archivist, https://t.co/CaO6K276g3 · Send tips to ben.welsh@latimes.com",
+			"url": "https://t.co/kogJ0fJceo",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/kogJ0fJceo",
+							"expanded_url": "http://palewi.re/who-is-ben-welsh/",
+							"display_url": "palewi.re/who-is-ben-wel…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/lZvqTdDOmO",
+							"expanded_url": "http://www.californiacivicdata.org",
+							"display_url": "californiacivicdata.org",
+							"indices": [
+								46,
+								69
+							]
+						},
+						{
+							"url": "https://t.co/CaO6K276g3",
+							"expanded_url": "http://pastpages.org",
+							"display_url": "pastpages.org",
+							"indices": [
+								83,
+								106
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 9448,
+			"friends_count": 3079,
+			"listed_count": 709,
+			"created_at": "Sun Apr 27 16:10:55 +0000 2008",
+			"favourites_count": 9,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 55355,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "FFFFFF",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/575168731417944064/T-S7fg3H_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14559196/1423764862",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDDDDD",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 12,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A small quake southeast of LA is an interesting test case for our Quakebot.\n\nhttps://t.co/OpHQNwWsgr\n\n@caseymmiller's new auto-generated shakemaps offer two zoom levels.\n\nWe've included both in this post, in hopes of giving both a \"near\" and a \"far\" context. We get it right? https://t.co/u9oZc1ugAa"
+	},
+	{
 		"created_at": "Sun Sep 01 20:01:39 +0000 2019",
 		"id": 1168252597636960300,
 		"id_str": "1168252597636960256",
