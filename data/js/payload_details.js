ec4afe186bdba8debc6f361b5c24c5751433a3e6
Matt Stiles <mattstiles@gmail.com>
2019-09-06 20:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14140,
-	"created_at": "Wed Sep 04 22:20:03 +0000 2019",
+	"tweets": 14141,
+	"created_at": "Sat Sep 07 03:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,108 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sat Sep 07 02:32:32 +0000 2019",
+		"id": 1170115531388719000,
+		"id_str": "1170162906853281792",
+		"full_text": "Let me assure you the hard working employees of the NWS had nothing to do with the utterly disgusting and disingenuous tweet sent out by NOAA management tonight #NOAA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			166
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NOAA",
+					"indices": [
+						161,
+						166
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 79524097,
+			"id_str": "79524097",
+			"name": "Dan Sobien",
+			"screen_name": "pres_nwseo",
+			"location": "Sarasota Florida",
+			"description": "President of the National Weather Service Employees Organization.",
+			"url": "https://t.co/Vi2Kl9sbwG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Vi2Kl9sbwG",
+							"expanded_url": "http://www.nwseo.org",
+							"display_url": "nwseo.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2313,
+			"friends_count": 340,
+			"listed_count": 32,
+			"created_at": "Sat Oct 03 17:47:16 +0000 2009",
+			"favourites_count": 69,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 132,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/2162630243/Dan_20Sobien_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/2162630243/Dan_20Sobien_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7862,
+		"favorite_count": 25390,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Let me assure you the hard working employees of the NWS had nothing to do with the utterly disgusting and disingenuous tweet sent out by NOAA management tonight #NOAA"
+	},
+	{
 		"created_at": "Wed Sep 04 21:41:30 +0000 2019",
 		"id": 1169364890554814500,
 		"id_str": "1169364890554814465",
