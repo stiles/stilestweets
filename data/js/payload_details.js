87e70418137fcd107bdbc346b13be1c73606c658
Matt Stiles <mattstiles@gmail.com>
2019-09-08 12:20:04 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14146,
-	"created_at": "Sun Sep 08 16:20:04 +0000 2019",
+	"tweets": 14148,
+	"created_at": "Sun Sep 08 19:20:03 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,405 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 08 18:33:17 +0000 2019",
+		"id": 1170767077927293000,
+		"id_str": "1170767077927292928",
+		"full_text": "BREAKING: Investigators serve search warrants on California boat fire owner  https://t.co/YMdJIVVguy https://t.co/gnAmZei3Wv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			100
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/YMdJIVVguy",
+					"expanded_url": "https://www.latimes.com/california/story/2019-09-08/california-boat-fire-investigators-search-warrants-conception-owner-truth-aquatics",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						77,
+						100
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1170767075259797500,
+					"id_str": "1170767075259797505",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/ED9mc7OVUAE4F12.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED9mc7OVUAE4F12.jpg",
+					"url": "https://t.co/gnAmZei3Wv",
+					"display_url": "pic.twitter.com/gnAmZei3Wv",
+					"expanded_url": "https://twitter.com/stiles/status/1170767077927292928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170767075259797500,
+					"id_str": "1170767075259797505",
+					"indices": [
+						101,
+						124
+					],
+					"media_url": "http://pbs.twimg.com/media/ED9mc7OVUAE4F12.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED9mc7OVUAE4F12.jpg",
+					"url": "https://t.co/gnAmZei3Wv",
+					"display_url": "pic.twitter.com/gnAmZei3Wv",
+					"expanded_url": "https://twitter.com/stiles/status/1170767077927292928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 453,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 840,
+							"h": 560,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10970,
+			"friends_count": 1713,
+			"listed_count": 879,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6509,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14354,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1567878553",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "BREAKING: Investigators serve search warrants on California boat fire owner  https://t.co/YMdJIVVguy https://t.co/gnAmZei3Wv"
+	},
+	{
+		"created_at": "Sun Sep 08 18:30:47 +0000 2019",
+		"id": 1170766448018387000,
+		"id_str": "1170766448018386944",
+		"full_text": "@latimes @carlosvlozano @flyLAXairport Mystery solved. It's an Atlas Air cargo plane that apparently hasn't gotten its livery yet. Its goes back and forth from LAX to Asia: https://t.co/zJ1Q82GyoP https://t.co/Qzo3r6FKUi",
+		"truncated": false,
+		"display_text_range": [
+			39,
+			196
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "carlosvlozano",
+					"name": "carlos valdez lozano",
+					"id": 1480484899,
+					"id_str": "1480484899",
+					"indices": [
+						9,
+						23
+					]
+				},
+				{
+					"screen_name": "flyLAXairport",
+					"name": "LAX Airport",
+					"id": 26886095,
+					"id_str": "26886095",
+					"indices": [
+						24,
+						38
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zJ1Q82GyoP",
+					"expanded_url": "https://www.flightradar24.com/data/aircraft/n445mc",
+					"display_url": "flightradar24.com/data/aircraft/…",
+					"indices": [
+						173,
+						196
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1170766443547246600,
+					"id_str": "1170766443547246592",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/ED9l4J6UwAAURdK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED9l4J6UwAAURdK.jpg",
+					"url": "https://t.co/Qzo3r6FKUi",
+					"display_url": "pic.twitter.com/Qzo3r6FKUi",
+					"expanded_url": "https://twitter.com/stiles/status/1170766448018386944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 724,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 410,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1453,
+							"h": 877,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170766443547246600,
+					"id_str": "1170766443547246592",
+					"indices": [
+						197,
+						220
+					],
+					"media_url": "http://pbs.twimg.com/media/ED9l4J6UwAAURdK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED9l4J6UwAAURdK.jpg",
+					"url": "https://t.co/Qzo3r6FKUi",
+					"display_url": "pic.twitter.com/Qzo3r6FKUi",
+					"expanded_url": "https://twitter.com/stiles/status/1170766448018386944/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 724,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 410,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1453,
+							"h": 877,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1170729459562860500,
+		"in_reply_to_status_id_str": "1170729459562860544",
+		"in_reply_to_user_id": 14790314,
+		"in_reply_to_user_id_str": "14790314",
+		"in_reply_to_screen_name": "stiles",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10970,
+			"friends_count": 1713,
+			"listed_count": 879,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6509,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14354,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1567878553",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@latimes @carlosvlozano @flyLAXairport Mystery solved. It's an Atlas Air cargo plane that apparently hasn't gotten its livery yet. Its goes back and forth from LAX to Asia: https://t.co/zJ1Q82GyoP https://t.co/Qzo3r6FKUi"
+	},
+	{
 		"created_at": "Sun Sep 08 16:03:48 +0000 2019",
 		"id": 1170729459562860500,
 		"id_str": "1170729459562860544",
