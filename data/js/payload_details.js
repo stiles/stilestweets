cfc1816958f14e54f6f104b13b0eafa57af625dc
Matt Stiles <mattstiles@gmail.com>
2019-09-25 20:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14222,
-	"created_at": "Thu Sep 26 01:20:05 +0000 2019",
+	"tweets": 14223,
+	"created_at": "Thu Sep 26 03:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,96 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Thu Sep 26 02:53:42 +0000 2019",
+		"id": 1176931086741278700,
+		"id_str": "1177053603011293184",
+		"full_text": "How committed am I? I gave up my life in NYC, leaving behind all my friends—and my partner!—to move to a place where I know virtually no one because I so strongly believe in this place and its future. I need our managers to show their commitment to us. \n#LATGuild",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			263
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LATGuild",
+					"indices": [
+						254,
+						263
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 39407743,
+			"id_str": "39407743",
+			"name": "Brittany Hite",
+			"screen_name": "brittanyhite",
+			"location": "Los Angeles, CA",
+			"description": "Newsroom project manager @latimes 🌴 Formerly of @WSJ, @WSJsoftball. Made in Indiana. Left my heart in 香港, my lungs in 北京, my coat in NYC. Live for 🍺,📚& ⚾️",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5598,
+			"friends_count": 980,
+			"listed_count": 307,
+			"created_at": "Tue May 12 02:12:40 +0000 2009",
+			"favourites_count": 991,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 6684,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ACDED6",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme18/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1172647477012393985/OSJKMs4T_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1172647477012393985/OSJKMs4T_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/39407743/1559331570",
+			"profile_link_color": "032461",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "F6F6F6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 12,
+		"favorite_count": 30,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "How committed am I? I gave up my life in NYC, leaving behind all my friends—and my partner!—to move to a place where I know virtually no one because I so strongly believe in this place and its future. I need our managers to show their commitment to us. \n#LATGuild"
+	},
+	{
 		"created_at": "Thu Sep 26 01:11:19 +0000 2019",
 		"id": 1177000181629423600,
 		"id_str": "1177027839083737088",
