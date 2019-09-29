3dcf9c81083601331fb5902342ae303db78a8728
Matt Stiles <mattstiles@gmail.com>
2019-09-29 09:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14243,
-	"created_at": "Sun Sep 29 03:20:06 +0000 2019",
+	"tweets": 14244,
+	"created_at": "Sun Sep 29 16:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,215 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Sun Sep 29 15:44:48 +0000 2019",
+		"id": 1178334615552921600,
+		"id_str": "1178334820209848321",
+		"full_text": "We are back at it today. 50 locations around the County open 10AM to 4PM. Stop by and check out #LACounty's new voting experience. Fun for everyone. Visit https://t.co/9E9L8jHgJU for a list of locations and all the info. #VSAP https://t.co/D2mA5mw9f3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			226
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LACounty",
+					"indices": [
+						96,
+						105
+					]
+				},
+				{
+					"text": "VSAP",
+					"indices": [
+						221,
+						226
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/9E9L8jHgJU",
+					"expanded_url": "http://www.lavote.net",
+					"display_url": "lavote.net",
+					"indices": [
+						155,
+						178
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1178334611597750300,
+					"id_str": "1178334611597750272",
+					"indices": [
+						227,
+						250
+					],
+					"media_url": "http://pbs.twimg.com/media/EFpJFg7U8AAYZa4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFpJFg7U8AAYZa4.jpg",
+					"url": "https://t.co/D2mA5mw9f3",
+					"display_url": "pic.twitter.com/D2mA5mw9f3",
+					"expanded_url": "https://twitter.com/LACountyRRCC/status/1178334615552921602/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1178334611597750300,
+					"id_str": "1178334611597750272",
+					"indices": [
+						227,
+						250
+					],
+					"media_url": "http://pbs.twimg.com/media/EFpJFg7U8AAYZa4.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFpJFg7U8AAYZa4.jpg",
+					"url": "https://t.co/D2mA5mw9f3",
+					"display_url": "pic.twitter.com/D2mA5mw9f3",
+					"expanded_url": "https://twitter.com/LACountyRRCC/status/1178334615552921602/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 21807663,
+			"id_str": "21807663",
+			"name": "Dean Logan, RR/CC",
+			"screen_name": "LACountyRRCC",
+			"location": "Los Angeles County",
+			"description": "L.A. County Registrar-Recorder/County Clerk | Elections? We conduct them.\nRecords? We have those.\nGetting Married? We do that too.\nhttps://t.co/yo9p8vsY6o",
+			"url": "http://t.co/VkLKHVYxBl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/VkLKHVYxBl",
+							"expanded_url": "http://www.lavote.net",
+							"display_url": "lavote.net",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/yo9p8vsY6o",
+							"expanded_url": "http://www.lavote.net/documents/media/RRCC_SMSDisclaimers.pdf",
+							"display_url": "lavote.net/documents/medi…",
+							"indices": [
+								131,
+								154
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 6188,
+			"friends_count": 1415,
+			"listed_count": 253,
+			"created_at": "Tue Feb 24 22:40:37 +0000 2009",
+			"favourites_count": 5120,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 11031,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/981222659189977088/jlnhwlt5_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/981222659189977088/jlnhwlt5_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/21807663/1547512642",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are back at it today. 50 locations around the County open 10AM to 4PM. Stop by and check out #LACounty's new voting experience. Fun for everyone. Visit https://t.co/9E9L8jHgJU for a list of locations and all the info. #VSAP https://t.co/D2mA5mw9f3"
+	},
+	{
 		"created_at": "Sun Sep 29 03:03:53 +0000 2019",
 		"id": 1178143330258972700,
 		"id_str": "1178143330258972673",
