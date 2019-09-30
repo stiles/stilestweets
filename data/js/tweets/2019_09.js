ebb8d816bbce390e920f2fedfb9a729a806ba9a2
Matt Stiles <mattstiles@gmail.com>
2019-09-30 08:20:09 -0700

    Added 1 tweets

--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,223 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 30 14:38:04 +0000 2019",
+		"id": 1178680417345396700,
+		"id_str": "1178680417345396737",
+		"full_text": "More than $2 million in campaign donations has flowed into L.A. County's hotly contested 2nd District supervisor race. \n\nMy latest story/graphic/interactive thingy... 📈📊🗺️ https://t.co/1hmvuwur6B https://t.co/WwLHr5I9ZV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			195
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/1hmvuwur6B",
+					"expanded_url": "https://www.latimes.com/projects/county-supervisor-2nd-district-campaign-donations/",
+					"display_url": "latimes.com/projects/count…",
+					"indices": [
+						172,
+						195
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1178680414702948400,
+					"id_str": "1178680414702948352",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EFuDl54UUAAFomZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFuDl54UUAAFomZ.jpg",
+					"url": "https://t.co/WwLHr5I9ZV",
+					"display_url": "pic.twitter.com/WwLHr5I9ZV",
+					"expanded_url": "https://twitter.com/stiles/status/1178680417345396737/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 438,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 438,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 438,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1178680414702948400,
+					"id_str": "1178680414702948352",
+					"indices": [
+						196,
+						219
+					],
+					"media_url": "http://pbs.twimg.com/media/EFuDl54UUAAFomZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFuDl54UUAAFomZ.jpg",
+					"url": "https://t.co/WwLHr5I9ZV",
+					"display_url": "pic.twitter.com/WwLHr5I9ZV",
+					"expanded_url": "https://twitter.com/stiles/status/1178680417345396737/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 480,
+							"h": 438,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 480,
+							"h": 438,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 480,
+							"h": 438,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYxdzj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYxdzj",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11027,
+			"friends_count": 1744,
+			"listed_count": 879,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6675,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14490,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1569721321",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9f369c7085001",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9f369c7085001.json",
+			"place_type": "poi",
+			"name": "Pacific Ocean",
+			"full_name": "Pacific Ocean",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-117.30947812689912,
+							33.131625148417044
+						],
+						[
+							-117.30947812689912,
+							33.131625148417044
+						],
+						[
+							-117.30947812689912,
+							33.131625148417044
+						],
+						[
+							-117.30947812689912,
+							33.131625148417044
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "More than $2 million in campaign donations has flowed into L.A. County's hotly contested 2nd District supervisor race. \n\nMy latest story/graphic/interactive thingy... 📈📊🗺️ https://t.co/1hmvuwur6B https://t.co/WwLHr5I9ZV"
+	},
+	{
 		"created_at": "Mon Sep 30 13:16:01 +0000 2019",
 		"id": 1178653553000403000,
 		"id_str": "1178659765662019584",
