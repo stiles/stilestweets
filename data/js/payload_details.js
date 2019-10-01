2da129f0aa5b5a95f6b5998fde5f3744e16fdbbd
Matt Stiles <mattstiles@gmail.com>
2019-10-01 10:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14260,
-	"created_at": "Tue Oct 01 05:20:05 +0000 2019",
+	"tweets": 14261,
+	"created_at": "Tue Oct 01 17:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 01 17:06:07 +0000 2019",
+		"id": 1179042902703394800,
+		"id_str": "1179080060994650112",
+		"full_text": "EXCLUSIVE: @LATimes has docs showing that 4 years ago, the FDA tried to ban vaping flavors to protect kids. But after aggressive lobbying from the vaping industry, senior Obama officials nixed the plan – and deleted much of the evidence that justified it. https://t.co/lheCUtuhYZ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						11,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lheCUtuhYZ",
+					"expanded_url": "https://www.latimes.com/politics/story/2019-10-01/vaping-flavors-obama-white-house-fda",
+					"display_url": "latimes.com/politics/story…",
+					"indices": [
+						256,
+						279
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 2838145504,
+			"id_str": "2838145504",
+			"name": "Emily Baumgaertner",
+			"screen_name": "Emily_Baum",
+			"location": "Los Angeles, CA",
+			"description": "Science/medicine reporter @LATimes. Formerly @NYTimes Washington and @GWPublicHealth. Send tips: emily.baumgaertner@latimes.com. Romans 5:8.",
+			"url": "https://t.co/sHpIDLDf58",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/sHpIDLDf58",
+							"expanded_url": "https://www.latimes.com/la-bio-emily-baumgaertner-staff.html",
+							"display_url": "latimes.com/la-bio-emily-b…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1887,
+			"friends_count": 757,
+			"listed_count": 76,
+			"created_at": "Mon Oct 20 14:08:41 +0000 2014",
+			"favourites_count": 823,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 954,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1110289075963981824/FSgM4mZK_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1110289075963981824/FSgM4mZK_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/2838145504/1553573115",
+			"profile_link_color": "01DFA5",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 751,
+		"favorite_count": 826,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "EXCLUSIVE: @LATimes has docs showing that 4 years ago, the FDA tried to ban vaping flavors to protect kids. But after aggressive lobbying from the vaping industry, senior Obama officials nixed the plan – and deleted much of the evidence that justified it. https://t.co/lheCUtuhYZ"
+	},
+	{
 		"created_at": "Tue Oct 01 05:05:06 +0000 2019",
 		"id": 1178898609388998700,
 		"id_str": "1178898609388998657",
