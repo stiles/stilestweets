fc82cf571cf0c6061ffc66267fb663ec4ba6af20
Matt Stiles <mattstiles@gmail.com>
2019-10-03 22:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14274,
-	"created_at": "Fri Oct 04 01:20:04 +0000 2019",
+	"tweets": 14275,
+	"created_at": "Fri Oct 04 05:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,112 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 04 04:44:15 +0000 2019",
+		"id": 1179852142040895500,
+		"id_str": "1179980527358472193",
+		"full_text": "do you like comprehensive, cited lists of everything related to the impeachment inquiry going back 10 months? \n\nwell, you're in luck. \n\nhttps://t.co/M5U49YoSrT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/M5U49YoSrT",
+					"expanded_url": "http://impeachment.wtf",
+					"display_url": "impeachment.wtf",
+					"indices": [
+						136,
+						159
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 24963237,
+			"id_str": "24963237",
+			"name": "Matt Kiser",
+			"screen_name": "Matt_Kiser",
+			"location": "Seattle, WA",
+			"description": "Making it up as we go along @WTFJHT and @CurrentStatus. Past product stuff @Algorithmia, @BusinessInsider, @Forbes, @SPIN, @KCSCradio.",
+			"url": "https://t.co/a1Hd8wHwhd",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/a1Hd8wHwhd",
+							"expanded_url": "https://whatthefuckjusthappenedtoday.com/",
+							"display_url": "whatthefuckjusthappenedtoday.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 12664,
+			"friends_count": 1581,
+			"listed_count": 446,
+			"created_at": "Tue Mar 17 21:52:59 +0000 2009",
+			"favourites_count": 14387,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 23148,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "4B596B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme14/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/898310899718512640/4wJap233_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/898310899718512640/4wJap233_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/24963237/1503009527",
+			"profile_link_color": "000000",
+			"profile_sidebar_border_color": "576469",
+			"profile_sidebar_fill_color": "CCCCCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 43,
+		"favorite_count": 82,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "do you like comprehensive, cited lists of everything related to the impeachment inquiry going back 10 months? \n\nwell, you're in luck. \n\nhttps://t.co/M5U49YoSrT"
+	},
+	{
 		"created_at": "Fri Oct 04 01:03:39 +0000 2019",
 		"id": 1179908796400820200,
 		"id_str": "1179925012066623488",
