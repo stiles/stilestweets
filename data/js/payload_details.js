2074157085aef7ce55766b7fbae13c5284cb7835
Matt Stiles <mattstiles@gmail.com>
2019-10-04 14:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14275,
-	"created_at": "Fri Oct 04 05:20:04 +0000 2019",
+	"tweets": 14276,
+	"created_at": "Fri Oct 04 21:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 04 21:08:17 +0000 2019",
+		"id": 1180228166205358000,
+		"id_str": "1180228166205358080",
+		"full_text": "Hey data science/news nerd Twitter: \n\nI just got a new Mac machine at work. I want to try to get the environment right this time. I use python, pandas, postGIS, etc.\n\nLooking for advice/guides like this (https://t.co/8VyV7F7h8Q) from @nprviz. \n\n🙏🙏",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "nprviz",
+					"name": "NPR visuals team",
+					"id": 582282558,
+					"id_str": "582282558",
+					"indices": [
+						234,
+						241
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/8VyV7F7h8Q",
+					"expanded_url": "https://blog.apps.npr.org/2013/06/06/how-to-setup-a-developers-environment.html",
+					"display_url": "blog.apps.npr.org/2013/06/06/how…",
+					"indices": [
+						204,
+						227
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYxdzj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYxdzj",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11030,
+			"friends_count": 1752,
+			"listed_count": 880,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6695,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14516,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1570138607",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hey data science/news nerd Twitter: \n\nI just got a new Mac machine at work. I want to try to get the environment right this time. I use python, pandas, postGIS, etc.\n\nLooking for advice/guides like this (https://t.co/8VyV7F7h8Q) from @nprviz. \n\n🙏🙏"
+	},
+	{
 		"created_at": "Fri Oct 04 04:44:15 +0000 2019",
 		"id": 1179852142040895500,
 		"id_str": "1179980527358472193",
