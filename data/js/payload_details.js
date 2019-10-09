4217723d4f00869a8ed38029bbf7a0c4384a940b
Matt Stiles <mattstiles@gmail.com>
2019-10-09 09:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14292,
-	"created_at": "Wed Oct 09 14:20:05 +0000 2019",
+	"tweets": 14293,
+	"created_at": "Wed Oct 09 16:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,214 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Wed Oct 09 15:58:03 +0000 2019",
+		"id": 1181948696755179500,
+		"id_str": "1181962034314801152",
+		"full_text": "#LASD would like to wish an easy fast to all observing the holy day of Yom Kippur today. #yomkippur https://t.co/668cdW54DG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			99
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "LASD",
+					"indices": [
+						0,
+						5
+					]
+				},
+				{
+					"text": "yomkippur",
+					"indices": [
+						89,
+						99
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1181948693865353200,
+					"id_str": "1181948693865353217",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EGcgEyiU0AE9-jS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGcgEyiU0AE9-jS.jpg",
+					"url": "https://t.co/668cdW54DG",
+					"display_url": "pic.twitter.com/668cdW54DG",
+					"expanded_url": "https://twitter.com/LASDHQ/status/1181948696755179521/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1181948693865353200,
+					"id_str": "1181948693865353217",
+					"indices": [
+						100,
+						123
+					],
+					"media_url": "http://pbs.twimg.com/media/EGcgEyiU0AE9-jS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGcgEyiU0AE9-jS.jpg",
+					"url": "https://t.co/668cdW54DG",
+					"display_url": "pic.twitter.com/668cdW54DG",
+					"expanded_url": "https://twitter.com/LASDHQ/status/1181948696755179521/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 46470122,
+			"id_str": "46470122",
+			"name": "LA County Sheriffs",
+			"screen_name": "LASDHQ",
+			"location": "Los Angeles County, California",
+			"description": "Los Angeles County Sheriff's Department (Headquarters). Official. #LASD https://t.co/icwmbwgpc3 https://t.co/aqLb8E8unL",
+			"url": "https://t.co/jr2qeQPnRS",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jr2qeQPnRS",
+							"expanded_url": "http://careers.lasd.org",
+							"display_url": "careers.lasd.org",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/icwmbwgpc3",
+							"expanded_url": "http://www.lasd.org",
+							"display_url": "lasd.org",
+							"indices": [
+								72,
+								95
+							]
+						},
+						{
+							"url": "https://t.co/aqLb8E8unL",
+							"expanded_url": "https://www.facebook.com/LosAngelesCountySheriffsDepartment",
+							"display_url": "facebook.com/LosAngelesCoun…",
+							"indices": [
+								96,
+								119
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 105858,
+			"friends_count": 175,
+			"listed_count": 1678,
+			"created_at": "Thu Jun 11 19:39:26 +0000 2009",
+			"favourites_count": 2490,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 20990,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "003300",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1160987349989224450/jH-1lgXm_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1160987349989224450/jH-1lgXm_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/46470122/1539889784",
+			"profile_link_color": "3B94D9",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "006600",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#LASD would like to wish an easy fast to all observing the holy day of Yom Kippur today. #yomkippur https://t.co/668cdW54DG"
+	},
+	{
 		"created_at": "Wed Oct 09 13:37:00 +0000 2019",
 		"id": 1181926537182023700,
 		"id_str": "1181926537182023681",
