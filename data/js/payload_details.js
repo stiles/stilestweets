87ee38e2093176d8bbf2d81234953de08417ed30
Matt Stiles <mattstiles@gmail.com>
2019-10-10 21:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14304,
-	"created_at": "Fri Oct 11 03:20:04 +0000 2019",
+	"tweets": 14305,
+	"created_at": "Fri Oct 11 04:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 11 04:02:03 +0000 2019",
+		"id": 1182506623241113600,
+		"id_str": "1182506623241113600",
+		"full_text": "Digging into @LAPDHQ's dataset of 2M+ crimes in the last decade, it seems officers (or clerks) are overusing noon as the time when crimes occur.\n\nI can understand why that might happen. Still, it creates a nice big middle finger for anyone hoping for useful time-series analysis. https://t.co/ZOmOTJ0WKO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LAPDHQ",
+					"name": "LAPD HQ",
+					"id": 115705583,
+					"id_str": "115705583",
+					"indices": [
+						13,
+						20
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1182506618740564000,
+					"id_str": "1182506618740563968",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EGkbgSqUYAAI_lR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGkbgSqUYAAI_lR.jpg",
+					"url": "https://t.co/ZOmOTJ0WKO",
+					"display_url": "pic.twitter.com/ZOmOTJ0WKO",
+					"expanded_url": "https://twitter.com/stiles/status/1182506623241113600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1589,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 342,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 604,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1182506618740564000,
+					"id_str": "1182506618740563968",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EGkbgSqUYAAI_lR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGkbgSqUYAAI_lR.jpg",
+					"url": "https://t.co/ZOmOTJ0WKO",
+					"display_url": "pic.twitter.com/ZOmOTJ0WKO",
+					"expanded_url": "https://twitter.com/stiles/status/1182506623241113600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1589,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 342,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 604,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYxdzj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYxdzj",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11037,
+			"friends_count": 1783,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6731,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14549,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1570732590",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Digging into @LAPDHQ's dataset of 2M+ crimes in the last decade, it seems officers (or clerks) are overusing noon as the time when crimes occur.\n\nI can understand why that might happen. Still, it creates a nice big middle finger for anyone hoping for useful time-series analysis. https://t.co/ZOmOTJ0WKO"
+	},
+	{
 		"created_at": "Fri Oct 11 02:56:05 +0000 2019",
 		"id": 1182490020722049000,
 		"id_str": "1182490020722049024",
