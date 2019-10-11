5489bbc5498b02db833e48de03c34edcd2f070ea
Matt Stiles <mattstiles@gmail.com>
2019-10-11 12:20:06 -0700

    Added 1 tweets

--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,185 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Fri Oct 11 18:47:15 +0000 2019",
+		"id": 1182677953399836700,
+		"id_str": "1182729389278158848",
+		"full_text": "Stay up-to-date with the #SaddleridgeFire using our active wildfire map. https://t.co/IE3tN78FjJ https://t.co/brMmMV8SVG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			96
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SaddleridgeFire",
+					"indices": [
+						25,
+						41
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IE3tN78FjJ",
+					"expanded_url": "https://www.latimes.com/wildfires-map/",
+					"display_url": "latimes.com/wildfires-map/",
+					"indices": [
+						73,
+						96
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1182677948031127600,
+					"id_str": "1182677948031127553",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EGm3U-AVUAEBJ4f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGm3U-AVUAEBJ4f.jpg",
+					"url": "https://t.co/brMmMV8SVG",
+					"display_url": "pic.twitter.com/brMmMV8SVG",
+					"expanded_url": "https://twitter.com/caseymmiller/status/1182677953399836672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 656,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1158,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1162,
+							"h": 1204,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1182677948031127600,
+					"id_str": "1182677948031127553",
+					"indices": [
+						97,
+						120
+					],
+					"media_url": "http://pbs.twimg.com/media/EGm3U-AVUAEBJ4f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGm3U-AVUAEBJ4f.jpg",
+					"url": "https://t.co/brMmMV8SVG",
+					"display_url": "pic.twitter.com/brMmMV8SVG",
+					"expanded_url": "https://twitter.com/caseymmiller/status/1182677953399836672/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 656,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1158,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1162,
+							"h": 1204,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 53286916,
+			"id_str": "53286916",
+			"name": "Casey Miller",
+			"screen_name": "caseymmiller",
+			"location": "Los Angeles, CA",
+			"description": "Disasters @latimes + @datagraphics. Prev @Mapbox & @voxmediainc. @UNCMJSchool Alum. Unofficial Bojangles’ brand rep. Stevie Nicks 5ever 🕊️🌈",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1764,
+			"friends_count": 940,
+			"listed_count": 80,
+			"created_at": "Fri Jul 03 03:52:17 +0000 2009",
+			"favourites_count": 9784,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 7426,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "ABB8C2",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1128851060322447361/vQaIXD97_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1128851060322447361/vQaIXD97_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/53286916/1489677936",
+			"profile_link_color": "A14EE5",
+			"profile_sidebar_border_color": "E5C99F",
+			"profile_sidebar_fill_color": "4E164A",
+			"profile_text_color": "C2A781",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 33,
+		"favorite_count": 38,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Stay up-to-date with the #SaddleridgeFire using our active wildfire map. https://t.co/IE3tN78FjJ https://t.co/brMmMV8SVG"
+	},
+	{
 		"created_at": "Fri Oct 11 17:51:24 +0000 2019",
 		"id": 1182712343421051000,
 		"id_str": "1182715337474637824",
