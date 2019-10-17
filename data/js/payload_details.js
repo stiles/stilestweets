5025bba3f32a8281be2e3bd665017f9a1ef07478
Matt Stiles <mattstiles@gmail.com>
2019-10-17 10:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14345,
-	"created_at": "Thu Oct 17 04:20:05 +0000 2019",
+	"tweets": 14346,
+	"created_at": "Thu Oct 17 17:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,221 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 17 16:42:56 +0000 2019",
+		"id": 1184872432852590600,
+		"id_str": "1184872432852590592",
+		"full_text": "Everyone in L.A. politics knows the term \"Democratic donor\" about Ed Buck, who's now facing charges related to drug overdoses at his W. Hollywood apt. \n\n@maloym &amp; @swetha_kan of @datagraphics have compiled a useful list of politicians who took his 💰: https://t.co/5Qnqd0EGWg https://t.co/1kYtwFOv6U",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "maloym",
+					"name": "Maloy Moore",
+					"id": 247293595,
+					"id_str": "247293595",
+					"indices": [
+						153,
+						160
+					]
+				},
+				{
+					"screen_name": "swetha_kan",
+					"name": "Swetha Kannan",
+					"id": 857276747376611300,
+					"id_str": "857276747376611328",
+					"indices": [
+						167,
+						178
+					]
+				},
+				{
+					"screen_name": "datagraphics",
+					"name": "Los Angeles Times Data and Graphics",
+					"id": 17918860,
+					"id_str": "17918860",
+					"indices": [
+						182,
+						195
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/5Qnqd0EGWg",
+					"expanded_url": "https://www.latimes.com/projects/ed-buck-donations/",
+					"display_url": "latimes.com/projects/ed-bu…",
+					"indices": [
+						255,
+						278
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1184872430700859400,
+					"id_str": "1184872430700859392",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EHGDMrAUEAATuN1.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHGDMrAUEAATuN1.png",
+					"url": "https://t.co/1kYtwFOv6U",
+					"display_url": "pic.twitter.com/1kYtwFOv6U",
+					"expanded_url": "https://twitter.com/stiles/status/1184872432852590592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 485,
+							"h": 631,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 485,
+							"h": 631,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 485,
+							"h": 631,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1184872430700859400,
+					"id_str": "1184872430700859392",
+					"indices": [
+						279,
+						302
+					],
+					"media_url": "http://pbs.twimg.com/media/EHGDMrAUEAATuN1.png",
+					"media_url_https": "https://pbs.twimg.com/media/EHGDMrAUEAATuN1.png",
+					"url": "https://t.co/1kYtwFOv6U",
+					"display_url": "pic.twitter.com/1kYtwFOv6U",
+					"expanded_url": "https://twitter.com/stiles/status/1184872432852590592/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 485,
+							"h": 631,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 485,
+							"h": 631,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 485,
+							"h": 631,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles, CA",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYxdzj",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYxdzj",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11050,
+			"friends_count": 1797,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 6794,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14603,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1571006629",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Everyone in L.A. politics knows the term \"Democratic donor\" about Ed Buck, who's now facing charges related to drug overdoses at his W. Hollywood apt. \n\n@maloym &amp; @swetha_kan of @datagraphics have compiled a useful list of politicians who took his 💰: https://t.co/5Qnqd0EGWg https://t.co/1kYtwFOv6U"
+	},
+	{
 		"created_at": "Thu Oct 17 03:45:20 +0000 2019",
 		"id": 1184676743149310000,
 		"id_str": "1184676743149309953",
