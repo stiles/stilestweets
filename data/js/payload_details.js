a246ea9f3bee7df4719d5c07c95ce645cee7c91d
Matt Stiles <mattstiles@gmail.com>
2019-10-22 09:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14367,
-	"created_at": "Tue Oct 22 15:20:05 +0000 2019",
+	"tweets": 14368,
+	"created_at": "Tue Oct 22 16:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,111 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Tue Oct 22 15:27:17 +0000 2019",
+		"id": 1186372736974835700,
+		"id_str": "1186665334427643905",
+		"full_text": "Millennials, quit whining. I paid off $150,000 in student loans and own a $400,000 home, because I SAVE. It’s not that hard. I\n-Make coffee at home\n-Bus instead of Uber\n-Shop sales\n-Had parents pay off my loans &amp; buy me a house because I’m daddy’s special boy\n-Got Hulu with ads",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			282
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 732678376306188300,
+			"id_str": "732678376306188289",
+			"name": "PolterGyver",
+			"screen_name": "TheAndrewNadeau",
+			"location": "Chicago, IL",
+			"description": "Producer and host of Spitfire Comedy https://t.co/tCv4Vg9PfO Staff Writer at @thedad",
+			"url": "https://t.co/jHnEdOaBPl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/jHnEdOaBPl",
+							"expanded_url": "https://twitter.com/AndrewNadeau0/timelines/1053883086360973312",
+							"display_url": "twitter.com/AndrewNadeau0/…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/tCv4Vg9PfO",
+							"expanded_url": "http://www.SpitfireComedyShow.com",
+							"display_url": "SpitfireComedyShow.com",
+							"indices": [
+								37,
+								60
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 64245,
+			"friends_count": 681,
+			"listed_count": 648,
+			"created_at": "Tue May 17 21:05:12 +0000 2016",
+			"favourites_count": 228639,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 48263,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1058007612657156096/nqWJe5yJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1058007612657156096/nqWJe5yJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/732678376306188289/1560484758",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 65218,
+		"favorite_count": 447851,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Millennials, quit whining. I paid off $150,000 in student loans and own a $400,000 home, because I SAVE. It’s not that hard. I\n-Make coffee at home\n-Bus instead of Uber\n-Shop sales\n-Had parents pay off my loans &amp; buy me a house because I’m daddy’s special boy\n-Got Hulu with ads"
+	},
+	{
 		"created_at": "Tue Oct 22 15:11:33 +0000 2019",
 		"id": 1186661375134683100,
 		"id_str": "1186661375134683136",
