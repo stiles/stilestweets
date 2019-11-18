dea9dadc35b553ede32bfde8ba94fcf2bd25371e
Matt Stiles <mattstiles@gmail.com>
2019-11-18 10:20:08 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14481,
-	"created_at": "Mon Nov 18 15:20:06 +0000 2019",
+	"tweets": 14482,
+	"created_at": "Mon Nov 18 18:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,113 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Mon Nov 18 17:26:59 +0000 2019",
+		"id": 1196478964241293300,
+		"id_str": "1196479928205660160",
+		"full_text": "Maybe the biggest story of our time. https://t.co/yuEU1sqeyJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			60
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/yuEU1sqeyJ",
+					"expanded_url": "https://www.nytimes.com/2019/11/18/us/polls-media-fake-news.html",
+					"display_url": "nytimes.com/2019/11/18/us/…",
+					"indices": [
+						37,
+						60
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 406632310,
+			"id_str": "406632310",
+			"name": "Alan Zarembo",
+			"screen_name": "AlanZarembo",
+			"location": "Los Angeles",
+			"description": "Editor @latimes of foreign and national stories. Former correspondent in Africa and Latin America.",
+			"url": "http://t.co/aQMsvKDtFN",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/aQMsvKDtFN",
+							"expanded_url": "http://www.latimes.com",
+							"display_url": "latimes.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1127,
+			"friends_count": 984,
+			"listed_count": 65,
+			"created_at": "Sun Nov 06 23:59:02 +0000 2011",
+			"favourites_count": 195,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 662,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/576441543311036416/H0qWIaHK_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/576441543311036416/H0qWIaHK_normal.jpeg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Maybe the biggest story of our time. https://t.co/yuEU1sqeyJ"
+	},
+	{
 		"created_at": "Mon Nov 18 15:17:02 +0000 2019",
 		"id": 1196213124002545700,
 		"id_str": "1196447225234124800",
