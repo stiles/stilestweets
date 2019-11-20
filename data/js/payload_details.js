d09cf37e6a00d340ef56ca491d1d2c5f67047ea1
Matt Stiles <mattstiles@gmail.com>
2019-11-20 09:20:08 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14489,
-	"created_at": "Wed Nov 20 04:20:05 +0000 2019",
+	"tweets": 14490,
+	"created_at": "Wed Nov 20 17:20:06 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,103 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Wed Nov 20 16:56:41 +0000 2019",
+		"id": 1197178745112735700,
+		"id_str": "1197197079338074113",
+		"full_text": "This is an extremely skillful and devastating interview by Democratic lawyer of Sondland.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			89
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 282545479,
+			"id_str": "282545479",
+			"name": "Michael Barbaro",
+			"screen_name": "mikiebarb",
+			"location": "New York, NY",
+			"description": "Host of The Daily.",
+			"url": "https://t.co/8JpN3gLjgE",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/8JpN3gLjgE",
+							"expanded_url": "https://www.nytimes.com/newsletters/the-daily",
+							"display_url": "nytimes.com/newsletters/th…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 200125,
+			"friends_count": 1650,
+			"listed_count": 3288,
+			"created_at": "Fri Apr 15 12:18:28 +0000 2011",
+			"favourites_count": 18086,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 47672,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1093249745957634050/qtkKg82m_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1093249745957634050/qtkKg82m_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/282545479/1538770528",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 261,
+		"favorite_count": 2658,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "This is an extremely skillful and devastating interview by Democratic lawyer of Sondland."
+	},
+	{
 		"created_at": "Wed Nov 20 04:09:43 +0000 2019",
 		"id": 1197004066032717800,
 		"id_str": "1197004066032717824",
