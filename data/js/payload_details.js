7fcdfb96c58f32019d1c43ad9795d77109b9620d
Matt Stiles <mattstiles@gmail.com>
2019-11-22 15:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14495,
-	"created_at": "Fri Nov 22 22:20:05 +0000 2019",
+	"tweets": 14496,
+	"created_at": "Fri Nov 22 23:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,220 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 23:12:41 +0000 2019",
+		"id": 1198016481197342700,
+		"id_str": "1198016481197342720",
+		"full_text": "Frozen II tickets in South Korea are super fancy. https://t.co/zRHyBiY1uV https://t.co/99ZtdXGXyV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			73
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zRHyBiY1uV",
+					"expanded_url": "https://www.reddit.com/r/mildlyinteresting/comments/e077dg/frozen_2_tickets_in_korea/",
+					"display_url": "reddit.com/r/mildlyintere…",
+					"indices": [
+						50,
+						73
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1198016478005481500,
+					"id_str": "1198016478005481472",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EKA1o66U0AAKeTu.png",
+					"media_url_https": "https://pbs.twimg.com/media/EKA1o66U0AAKeTu.png",
+					"url": "https://t.co/99ZtdXGXyV",
+					"display_url": "pic.twitter.com/99ZtdXGXyV",
+					"expanded_url": "https://twitter.com/stiles/status/1198016481197342720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 364,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 364,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 364,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1198016478005481500,
+					"id_str": "1198016478005481472",
+					"indices": [
+						74,
+						97
+					],
+					"media_url": "http://pbs.twimg.com/media/EKA1o66U0AAKeTu.png",
+					"media_url_https": "https://pbs.twimg.com/media/EKA1o66U0AAKeTu.png",
+					"url": "https://t.co/99ZtdXGXyV",
+					"display_url": "pic.twitter.com/99ZtdXGXyV",
+					"expanded_url": "https://twitter.com/stiles/status/1198016481197342720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 364,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 364,
+							"h": 510,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 364,
+							"h": 510,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"medium": {
+							"faces": []
+						},
+						"large": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11159,
+			"friends_count": 1848,
+			"listed_count": 880,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7014,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14773,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1573841271",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Frozen II tickets in South Korea are super fancy. https://t.co/zRHyBiY1uV https://t.co/99ZtdXGXyV"
+	},
+	{
 		"created_at": "Fri Nov 22 22:09:06 +0000 2019",
 		"id": 1198000250339889200,
 		"id_str": "1198000479348748289",
