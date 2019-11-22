9d8f25b8446cfba7652d42bc4f96d2752989471d
Matt Stiles <mattstiles@gmail.com>
2019-11-22 14:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14494,
-	"created_at": "Fri Nov 22 21:20:07 +0000 2019",
+	"tweets": 14495,
+	"created_at": "Fri Nov 22 22:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_11.js
+++ data/js/tweets/2019_11.js
@@ -1,6 +1,268 @@
 Grailbird.data.tweets_2019_11 = 
 [
 	{
+		"created_at": "Fri Nov 22 22:09:06 +0000 2019",
+		"id": 1198000250339889200,
+		"id_str": "1198000479348748289",
+		"full_text": "This is how this works:\n\n@DevinNunes spouts conspiracy theories and smears critics in Trump’s defense. @VDHanson promotes Nunes propaganda in rightwing media. I call them out (https://t.co/h19dXzVaJ9). Hanson attacks me. Nunes promotes Hanson’s attack. \n\nRinse and repeat. https://t.co/HgkHfXnsWl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			272
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "DevinNunes",
+					"name": "Devin Nunes",
+					"id": 54412900,
+					"id_str": "54412900",
+					"indices": [
+						25,
+						36
+					]
+				},
+				{
+					"screen_name": "VDHanson",
+					"name": "Victor Davis Hanson",
+					"id": 139550237,
+					"id_str": "139550237",
+					"indices": [
+						103,
+						112
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/h19dXzVaJ9",
+					"expanded_url": "https://wapo.st/2Kc8QIU",
+					"display_url": "wapo.st/2Kc8QIU",
+					"indices": [
+						176,
+						199
+					]
+				},
+				{
+					"url": "https://t.co/HgkHfXnsWl",
+					"expanded_url": "https://twitter.com/devinnunes/status/1196538760130965504",
+					"display_url": "twitter.com/devinnunes/sta…",
+					"indices": [
+						273,
+						296
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 28162211,
+			"id_str": "28162211",
+			"name": "Max Boot",
+			"screen_name": "MaxBoot",
+			"location": "New York",
+			"description": "CFR fellow, WaPo columnist, CNN analyst. Author: “The Road Not Taken,” “Corrosion of Conservatism.” All opinions mine; feel free to adopt them. RT≠endorsement.",
+			"url": "https://t.co/MkszeyR3j5",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/MkszeyR3j5",
+							"expanded_url": "http://www.maxboot.net",
+							"display_url": "maxboot.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 267499,
+			"friends_count": 1452,
+			"listed_count": 4121,
+			"created_at": "Wed Apr 01 18:31:03 +0000 2009",
+			"favourites_count": 11748,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 94090,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "D7482A",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1072335161365270528/Q7gzINJ2_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1072335161365270528/Q7gzINJ2_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/28162211/1556658769",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1196538760130965500,
+		"quoted_status_id_str": "1196538760130965504",
+		"quoted_status_permalink": {
+			"url": "https://t.co/HgkHfXnsWl",
+			"expanded": "https://twitter.com/devinnunes/status/1196538760130965504",
+			"display": "twitter.com/devinnunes/sta…"
+		},
+		"quoted_status": {
+			"created_at": "Mon Nov 18 21:20:45 +0000 2019",
+			"id": 1196538760130965500,
+			"id_str": "1196538760130965504",
+			"full_text": "The Wages of Trump Fixation – ⁦@VDHanson⁩  https://t.co/erStZYJSHm",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				66
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [
+					{
+						"screen_name": "VDHanson",
+						"name": "Victor Davis Hanson",
+						"id": 139550237,
+						"id_str": "139550237",
+						"indices": [
+							31,
+							40
+						]
+					}
+				],
+				"urls": [
+					{
+						"url": "https://t.co/erStZYJSHm",
+						"expanded_url": "https://amgreatness.com/2019/11/17/the-wages-of-trump-fixation/",
+						"display_url": "amgreatness.com/2019/11/17/the…",
+						"indices": [
+							43,
+							66
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 54412900,
+				"id_str": "54412900",
+				"name": "Devin Nunes",
+				"screen_name": "DevinNunes",
+				"location": "",
+				"description": "RTs do not equal endorsement.  This is my personal Twitter account.  If you have questions about my official activities please visit my website.",
+				"url": "http://t.co/nNrwohsqWS",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "http://t.co/nNrwohsqWS",
+								"expanded_url": "http://www.nunes.house.gov",
+								"display_url": "nunes.house.gov",
+								"indices": [
+									0,
+									22
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 703566,
+				"friends_count": 659,
+				"listed_count": 2857,
+				"created_at": "Tue Jul 07 02:04:34 +0000 2009",
+				"favourites_count": 185,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": true,
+				"statuses_count": 2468,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/738510058183593985/whpmEX7p_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/738510058183593985/whpmEX7p_normal.jpg",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1697,
+			"favorite_count": 4373,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 98,
+		"favorite_count": 397,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "This is how this works:\n\n@DevinNunes spouts conspiracy theories and smears critics in Trump’s defense. @VDHanson promotes Nunes propaganda in rightwing media. I call them out (https://t.co/h19dXzVaJ9). Hanson attacks me. Nunes promotes Hanson’s attack. \n\nRinse and repeat. https://t.co/HgkHfXnsWl"
+	},
+	{
 		"created_at": "Fri Nov 22 20:33:47 +0000 2019",
 		"id": 1197976492724015000,
 		"id_str": "1197976492724015105",
