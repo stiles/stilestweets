2cb4308d1f148714aeefff7eefd7bbb86b563368
Matt Stiles <mattstiles@gmail.com>
2019-11-30 23:20:05 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14517,
-	"created_at": "Sat Nov 30 04:20:07 +0000 2019",
+	"tweets": 14518,
+	"created_at": "Sun Dec 01 07:20:04 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_12.js
@@ -0,0 +1,111 @@
+Grailbird.data.tweets_2019_12 = 
+[
+	{
+		"created_at": "Sun Dec 01 07:14:37 +0000 2019",
+		"id": 1200834515507171300,
+		"id_str": "1201036863449886725",
+		"full_text": "really grateful that my first holiday story for the paper is about something that matters a lot to me: sparing a thought for the workers who make the holidays happen  https://t.co/2PU1Vo9aXd",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2PU1Vo9aXd",
+					"expanded_url": "https://www.latimes.com/business/story/2019-11-30/heres-what-holiday-retail-workers-wish-you-knew",
+					"display_url": "latimes.com/business/story…",
+					"indices": [
+						167,
+						190
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 3507702980,
+			"id_str": "3507702980",
+			"name": "Aida Ylanan",
+			"screen_name": "a1daylanan",
+			"location": "los angeles",
+			"description": "finding many ways to do the same thing / currently @datagraphics, formerly @ucla stats & english",
+			"url": "https://t.co/nK3z4cIV1K",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nK3z4cIV1K",
+							"expanded_url": "https://aidaylanan.github.io/",
+							"display_url": "aidaylanan.github.io",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 357,
+			"friends_count": 688,
+			"listed_count": 7,
+			"created_at": "Wed Sep 09 20:02:51 +0000 2015",
+			"favourites_count": 1776,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 177,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/978333812886183936/I7lzUgrG_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/978333812886183936/I7lzUgrG_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/3507702980/1534525722",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 86,
+		"favorite_count": 260,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "really grateful that my first holiday story for the paper is about something that matters a lot to me: sparing a thought for the workers who make the holidays happen  https://t.co/2PU1Vo9aXd"
+	}
+]
