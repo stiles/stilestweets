ead8954351def9a3fc56027c600438c918009dab
Matt Stiles <mattstiles@gmail.com>
2019-12-06 12:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14545,
-	"created_at": "Fri Dec 06 03:20:04 +0000 2019",
+	"tweets": 14546,
+	"created_at": "Fri Dec 06 20:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Fri Dec 06 19:58:27 +0000 2019",
+		"id": 1202762698124955600,
+		"id_str": "1203041029886107648",
+		"full_text": "Hi @MeCookieMonster,\n\nI'm a big fan of your work and what you've done for people who are generally blue. After all these years, you remain singularly focused on acquiring and devouring cookies. I admire that.\n\n@latimesfood has fresh cookie content for ya https://t.co/E9tVhLCrpA",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "MeCookieMonster",
+					"name": "Cookie Monster",
+					"id": 2372164754,
+					"id_str": "2372164754",
+					"indices": [
+						3,
+						19
+					]
+				},
+				{
+					"screen_name": "latimesfood",
+					"name": "L.A. Times Food",
+					"id": 17796018,
+					"id_str": "17796018",
+					"indices": [
+						210,
+						222
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/E9tVhLCrpA",
+					"expanded_url": "https://lat.ms/34WuObb",
+					"display_url": "lat.ms/34WuObb",
+					"indices": [
+						255,
+						278
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1072207332359041000,
+			"id_str": "1072207332359041024",
+			"name": "Brian Park",
+			"screen_name": "TheBrianPark",
+			"location": "Los Angeles, CA",
+			"description": "@latimes | California community college graduate | @normsrestaurant fan",
+			"url": "https://t.co/B0RvKygRn9",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/B0RvKygRn9",
+							"expanded_url": "https://www.latimes.com/",
+							"display_url": "latimes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 1034,
+			"friends_count": 2894,
+			"listed_count": 10,
+			"created_at": "Mon Dec 10 19:12:03 +0000 2018",
+			"favourites_count": 22603,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 6736,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "F5F8FA",
+			"profile_background_image_url": null,
+			"profile_background_image_url_https": null,
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171425636792954880/35qfi-A6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171425636792954880/35qfi-A6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1072207332359041024/1544471582",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 9,
+		"favorite_count": 47,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Hi @MeCookieMonster,\n\nI'm a big fan of your work and what you've done for people who are generally blue. After all these years, you remain singularly focused on acquiring and devouring cookies. I admire that.\n\n@latimesfood has fresh cookie content for ya https://t.co/E9tVhLCrpA"
+	},
+	{
 		"created_at": "Fri Dec 06 03:00:27 +0000 2019",
 		"id": 1202784840904347600,
 		"id_str": "1202784840904347648",
