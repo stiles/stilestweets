0610ec9162bb6dcd6c88dde76ccd89f02d26c910
Matt Stiles <mattstiles@gmail.com>
2019-12-09 13:20:06 -0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14553,
-	"created_at": "Mon Dec 09 18:20:04 +0000 2019",
+	"tweets": 14555,
+	"created_at": "Mon Dec 09 21:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,395 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Mon Dec 09 21:10:55 +0000 2019",
+		"id": 1204146429993730000,
+		"id_str": "1204146429993730048",
+		"full_text": "Wow, @OsopePatrisse. This is quite a cover. I know you won't, but you could just frame it and retire. \n\nStory: https://t.co/46NCvfhZbo https://t.co/3jXkRHr4Ki",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			134
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "OsopePatrisse",
+					"name": "patrisse cullors",
+					"id": 84801794,
+					"id_str": "84801794",
+					"indices": [
+						5,
+						19
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/46NCvfhZbo",
+					"expanded_url": "https://www.laweekly.com/fighting-for-what-matters-patrisse-cullors-approach-to-social-justice-is-artful-and-powerful/",
+					"display_url": "laweekly.com/fighting-for-w…",
+					"indices": [
+						111,
+						134
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1204146425543553000,
+					"id_str": "1204146425543553024",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/ELX8y1AUcAAQER_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELX8y1AUcAAQER_.jpg",
+					"url": "https://t.co/3jXkRHr4Ki",
+					"display_url": "pic.twitter.com/3jXkRHr4Ki",
+					"expanded_url": "https://twitter.com/stiles/status/1204146429993730048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 669,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 944,
+							"h": 959,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 944,
+							"h": 959,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 238,
+									"y": 139,
+									"h": 233,
+									"w": 233
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 337,
+									"y": 197,
+									"h": 330,
+									"w": 330
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 337,
+									"y": 197,
+									"h": 330,
+									"w": 330
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 337,
+									"y": 197,
+									"h": 330,
+									"w": 330
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1204146425543553000,
+					"id_str": "1204146425543553024",
+					"indices": [
+						135,
+						158
+					],
+					"media_url": "http://pbs.twimg.com/media/ELX8y1AUcAAQER_.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ELX8y1AUcAAQER_.jpg",
+					"url": "https://t.co/3jXkRHr4Ki",
+					"display_url": "pic.twitter.com/3jXkRHr4Ki",
+					"expanded_url": "https://twitter.com/stiles/status/1204146429993730048/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 669,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 944,
+							"h": 959,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 944,
+							"h": 959,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"small": {
+							"faces": [
+								{
+									"x": 238,
+									"y": 139,
+									"h": 233,
+									"w": 233
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 337,
+									"y": 197,
+									"h": 330,
+									"w": 330
+								}
+							]
+						},
+						"orig": {
+							"faces": [
+								{
+									"x": 337,
+									"y": 197,
+									"h": 330,
+									"w": 330
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 337,
+									"y": 197,
+									"h": 330,
+									"w": 330
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11171,
+			"friends_count": 1869,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7082,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14841,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1575520218",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Wow, @OsopePatrisse. This is quite a cover. I know you won't, but you could just frame it and retire. \n\nStory: https://t.co/46NCvfhZbo https://t.co/3jXkRHr4Ki"
+	},
+	{
+		"created_at": "Mon Dec 09 20:39:44 +0000 2019",
+		"id": 1204138582681636900,
+		"id_str": "1204138582681636864",
+		"full_text": "@espiers I hear you. Louie Gohmert represents my home congressional district.",
+		"truncated": false,
+		"display_text_range": [
+			9,
+			77
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "espiers",
+					"name": "Elizabeth Spiers",
+					"id": 14801863,
+					"id_str": "14801863",
+					"indices": [
+						0,
+						8
+					]
+				}
+			],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1204138038634397700,
+		"in_reply_to_status_id_str": "1204138038634397698",
+		"in_reply_to_user_id": 14801863,
+		"in_reply_to_user_id_str": "14801863",
+		"in_reply_to_screen_name": "espiers",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11171,
+			"friends_count": 1869,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7082,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14841,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1081631349084086273/z7eIlw5q_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1575520218",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"lang": "en",
+		"text": "@espiers I hear you. Louie Gohmert represents my home congressional district."
+	},
+	{
 		"created_at": "Mon Dec 09 17:50:54 +0000 2019",
 		"id": 1204090895102075000,
 		"id_str": "1204096094017212416",
