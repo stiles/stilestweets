4dc2483c5c278b332e5f72fa2f41170eee6008ce
Matt Stiles <mattstiles@gmail.com>
2019-12-19 09:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14573,
-	"created_at": "Wed Dec 18 21:20:05 +0000 2019",
+	"tweets": 14574,
+	"created_at": "Thu Dec 19 17:20:05 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,135 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 19 17:17:57 +0000 2019",
+		"id": 1207359161006096400,
+		"id_str": "1207711678927982594",
+		"full_text": "The @latimes is hiring a data and graphics journalist. This is an @LATGuild position.  https://t.co/d8vtQd9HJk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			110
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "latimes",
+					"name": "Los Angeles Times",
+					"id": 16664681,
+					"id_str": "16664681",
+					"indices": [
+						4,
+						12
+					]
+				},
+				{
+					"screen_name": "latguild",
+					"name": "L.A. Times Guild 🦅",
+					"id": 859504635773464600,
+					"id_str": "859504635773464576",
+					"indices": [
+						66,
+						75
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/d8vtQd9HJk",
+					"expanded_url": "https://nantmedia.wd5.myworkdayjobs.com/en-US/LATimesCareers/job/El-Segundo-CA/Graphics---Data-Journalist_REQ_000372",
+					"display_url": "nantmedia.wd5.myworkdayjobs.com/en-US/LATimesC…",
+					"indices": [
+						87,
+						110
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 859504635773464600,
+			"id_str": "859504635773464576",
+			"name": "L.A. Times Guild 🦅",
+			"screen_name": "latguild",
+			"location": "Los Angeles, CA",
+			"description": "Our mission is to safeguard the future of the Los Angeles Times and its journalists.",
+			"url": "https://t.co/R0OXgdbmkM",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/R0OXgdbmkM",
+							"expanded_url": "https://latguild.com",
+							"display_url": "latguild.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7237,
+			"friends_count": 3,
+			"listed_count": 137,
+			"created_at": "Tue May 02 20:27:28 +0000 2017",
+			"favourites_count": 5427,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 4771,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171167178277572608/2xMdrxay_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171167178277572608/2xMdrxay_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/859504635773464576/1559623312",
+			"profile_link_color": "FAC211",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 32,
+		"favorite_count": 33,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The @latimes is hiring a data and graphics journalist. This is an @LATGuild position.  https://t.co/d8vtQd9HJk"
+	},
+	{
 		"created_at": "Wed Dec 18 20:25:16 +0000 2019",
 		"id": 1207396431461376000,
 		"id_str": "1207396431461376000",
