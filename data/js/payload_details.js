b4a5e48f428a9af18bbca0a9651e26aaedf61cdb
Matt Stiles <mattstiles@gmail.com>
2019-12-25 09:20:05 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14584,
-	"created_at": "Tue Dec 24 16:20:04 +0000 2019",
+	"tweets": 14585,
+	"created_at": "Wed Dec 25 17:20:04 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,358 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Wed Dec 25 17:13:22 +0000 2019",
+		"id": 1209884855112044500,
+		"id_str": "1209884855112044544",
+		"full_text": "Merry Christmas to me!! I never have to steal gochujang from @KoreanAir again. https://t.co/VeiRLttanp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			78
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "KoreanAir",
+					"name": "대한항공",
+					"id": 89133606,
+					"id_str": "89133606",
+					"indices": [
+						61,
+						71
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1209884849361711000,
+					"id_str": "1209884849361711104",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/EMpf3DlU8AAFs98.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMpf3DlU8AAFs98.jpg",
+					"url": "https://t.co/VeiRLttanp",
+					"display_url": "pic.twitter.com/VeiRLttanp",
+					"expanded_url": "https://twitter.com/stiles/status/1209884855112044544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 208,
+									"y": 1486,
+									"h": 98,
+									"w": 98
+								},
+								{
+									"x": 444,
+									"y": 800,
+									"h": 680,
+									"w": 680
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 121,
+									"y": 870,
+									"h": 57,
+									"w": 57
+								},
+								{
+									"x": 260,
+									"y": 468,
+									"h": 398,
+									"w": 398
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 208,
+									"y": 1486,
+									"h": 98,
+									"w": 98
+								},
+								{
+									"x": 444,
+									"y": 800,
+									"h": 680,
+									"w": 680
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 69,
+									"y": 493,
+									"h": 32,
+									"w": 32
+								},
+								{
+									"x": 147,
+									"y": 265,
+									"h": 225,
+									"w": 225
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1209884849361711000,
+					"id_str": "1209884849361711104",
+					"indices": [
+						79,
+						102
+					],
+					"media_url": "http://pbs.twimg.com/media/EMpf3DlU8AAFs98.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EMpf3DlU8AAFs98.jpg",
+					"url": "https://t.co/VeiRLttanp",
+					"display_url": "pic.twitter.com/VeiRLttanp",
+					"expanded_url": "https://twitter.com/stiles/status/1209884855112044544/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						}
+					},
+					"features": {
+						"orig": {
+							"faces": [
+								{
+									"x": 208,
+									"y": 1486,
+									"h": 98,
+									"w": 98
+								},
+								{
+									"x": 444,
+									"y": 800,
+									"h": 680,
+									"w": 680
+								}
+							]
+						},
+						"medium": {
+							"faces": [
+								{
+									"x": 121,
+									"y": 870,
+									"h": 57,
+									"w": 57
+								},
+								{
+									"x": 260,
+									"y": 468,
+									"h": 398,
+									"w": 398
+								}
+							]
+						},
+						"large": {
+							"faces": [
+								{
+									"x": 208,
+									"y": 1486,
+									"h": 98,
+									"w": 98
+								},
+								{
+									"x": 444,
+									"y": 800,
+									"h": 680,
+									"w": 680
+								}
+							]
+						},
+						"small": {
+							"faces": [
+								{
+									"x": 69,
+									"y": 493,
+									"h": 32,
+									"w": 32
+								},
+								{
+									"x": 147,
+									"y": 265,
+									"h": 225,
+									"w": 225
+								}
+							]
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter @latimes covering @CountyofLA with data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11166,
+			"friends_count": 1870,
+			"listed_count": 874,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7145,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14872,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1576036583",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3b77caf94bfc81fe",
+			"url": "https://api.twitter.com/1.1/geo/id/3b77caf94bfc81fe.json",
+			"place_type": "city",
+			"name": "Los Angeles",
+			"full_name": "Los Angeles, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.668404,
+							33.704538
+						],
+						[
+							-118.155409,
+							33.704538
+						],
+						[
+							-118.155409,
+							34.337041
+						],
+						[
+							-118.668404,
+							34.337041
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Merry Christmas to me!! I never have to steal gochujang from @KoreanAir again. https://t.co/VeiRLttanp"
+	},
+	{
 		"created_at": "Tue Dec 24 15:55:49 +0000 2019",
 		"id": 1209502950034247700,
 		"id_str": "1209502950034247680",
