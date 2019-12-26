83261603fa1ec285c86b61f8e748238fc68ce30f
Matt Stiles <mattstiles@gmail.com>
2019-12-25 21:20:05 -0800

    Added 1 tweets

--- data/js/tweets/2019_12.js
+++ data/js/tweets/2019_12.js
@@ -1,6 +1,217 @@
 Grailbird.data.tweets_2019_12 = 
 [
 	{
+		"created_at": "Thu Dec 26 04:33:41 +0000 2019",
+		"id": 1210055872983908400,
+		"id_str": "1210056062897815553",
+		"full_text": "Life goals https://t.co/auksEuAD8T",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			10
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/auksEuAD8T",
+					"expanded_url": "https://twitter.com/r_andradefranco/status/1210055641987002368",
+					"display_url": "twitter.com/r_andradefranc…",
+					"indices": [
+						11,
+						34
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 15518170,
+			"id_str": "15518170",
+			"name": "Col. Gustavo Arellano",
+			"screen_name": "GustavoArellano",
+			"location": "Anacrime,OC,Zacatecas",
+			"description": "Con su pluma en su mano for @latimes. Investigations/tacos/history/OC/DESMADRE. Eternal Mexican, KY Col. ✉️: mexicanwithglasses@gmail.com & my newsletter below!",
+			"url": "https://t.co/h6ctxISTf1",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/h6ctxISTf1",
+							"expanded_url": "http://tinyletter.com/gustavoarellanosweekly",
+							"display_url": "tinyletter.com/gustavoarellan…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 23184,
+			"friends_count": 421,
+			"listed_count": 618,
+			"created_at": "Mon Jul 21 17:45:07 +0000 2008",
+			"favourites_count": 28,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 265006,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "9AE4E8",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1105308321668460545/G2xfLioi_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1105308321668460545/G2xfLioi_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/15518170/1545190490",
+			"profile_link_color": "0000FF",
+			"profile_sidebar_border_color": "87BC44",
+			"profile_sidebar_fill_color": "E0FF92",
+			"profile_text_color": "000000",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1210055641987002400,
+		"quoted_status_id_str": "1210055641987002368",
+		"quoted_status_permalink": {
+			"url": "https://t.co/auksEuAD8T",
+			"expanded": "https://twitter.com/r_andradefranco/status/1210055641987002368",
+			"display": "twitter.com/r_andradefranc…"
+		},
+		"quoted_status": {
+			"created_at": "Thu Dec 26 04:32:01 +0000 2019",
+			"id": 1210055641987002400,
+			"id_str": "1210055641987002368",
+			"full_text": "I call myself a pendejo, on average, 3 times a week. That's 12 times a month I talk down to myself.\n\nFor 2020, as part of being a better friend to myself, I'd like to knock that down to about 6 times a month.  \n\nI just need to focus on not making as many pendejadas.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				266
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 829549505401860100,
+				"id_str": "829549505401860096",
+				"name": "R Andrade Franco",
+				"screen_name": "R_AndradeFranco",
+				"location": "Arlington, TX",
+				"description": "Freelance Writer. PhD Candidate @SMUHistoryDept. Fronterizo from El Paso/Juárez. Robertojoseandradefranco@gmail.com",
+				"url": "https://t.co/jqdVFq5duE",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/jqdVFq5duE",
+								"expanded_url": "http://boxingandhistory.com",
+								"display_url": "boxingandhistory.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1539,
+				"friends_count": 1005,
+				"listed_count": 21,
+				"created_at": "Thu Feb 09 04:36:29 +0000 2017",
+				"favourites_count": 5430,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 4473,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/842699392876400641/WLOENlsL_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/842699392876400641/WLOENlsL_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/829549505401860096/1489750306",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"can_media_tag": true,
+				"followed_by": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 10,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Life goals https://t.co/auksEuAD8T"
+	},
+	{
 		"created_at": "Thu Dec 26 00:49:48 +0000 2019",
 		"id": 1209999719637770200,
 		"id_str": "1209999719637770240",
