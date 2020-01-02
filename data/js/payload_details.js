5c93ac708c2c84c0951494c68dd33ef5e56f3c6f
Matt Stiles <mattstiles@gmail.com>
2020-01-01 19:20:05 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14597,
-	"created_at": "Wed Jan 01 23:20:04 +0000 2020",
+	"tweets": 14598,
+	"created_at": "Thu Jan 02 03:20:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,410 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Thu Jan 02 03:11:16 +0000 2020",
+		"id": 1212556037791703000,
+		"id_str": "1212572034951069699",
+		"full_text": "Group photo of #NorthKorea's recent party plenum with interesting positioning of female party officials: Kim Yo Jong &amp; Hyon Song Wol (green), Choe Son Hui (yellow), unknown (blue) and several grouped in the middle (red). Grouping together in back also visible in April SPA photo https://t.co/xOa5DW6JZz https://t.co/Sp5iuKXTAF",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			306
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NorthKorea",
+					"indices": [
+						15,
+						26
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/xOa5DW6JZz",
+					"expanded_url": "https://twitter.com/NKResearcher/status/1117103773074305025",
+					"display_url": "twitter.com/NKResearcher/s…",
+					"indices": [
+						283,
+						306
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1212556028786512000,
+					"id_str": "1212556028786511872",
+					"indices": [
+						307,
+						330
+					],
+					"media_url": "http://pbs.twimg.com/media/ENPdSKYUEAALA66.png",
+					"media_url_https": "https://pbs.twimg.com/media/ENPdSKYUEAALA66.png",
+					"url": "https://t.co/Sp5iuKXTAF",
+					"display_url": "pic.twitter.com/Sp5iuKXTAF",
+					"expanded_url": "https://twitter.com/NKResearcher/status/1212556037791703041/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 303,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 303,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 258,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1212556028786512000,
+					"id_str": "1212556028786511872",
+					"indices": [
+						307,
+						330
+					],
+					"media_url": "http://pbs.twimg.com/media/ENPdSKYUEAALA66.png",
+					"media_url_https": "https://pbs.twimg.com/media/ENPdSKYUEAALA66.png",
+					"url": "https://t.co/Sp5iuKXTAF",
+					"display_url": "pic.twitter.com/Sp5iuKXTAF",
+					"expanded_url": "https://twitter.com/NKResearcher/status/1212556037791703041/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 800,
+							"h": 303,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 303,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 258,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					},
+					"features": {
+						"large": {
+							"faces": []
+						},
+						"medium": {
+							"faces": []
+						},
+						"orig": {
+							"faces": []
+						},
+						"small": {
+							"faces": []
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 719930110871076900,
+			"id_str": "719930110871076864",
+			"name": "Martin Weiser",
+			"screen_name": "NKResearcher",
+			"location": "Seoul",
+			"description": "#DPRK historian, translator, freelance researcher, creator of  DPRK Legislation Database",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 557,
+			"friends_count": 754,
+			"listed_count": 17,
+			"created_at": "Tue Apr 12 16:48:08 +0000 2016",
+			"favourites_count": 268,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1255,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+			"profile_link_color": "91D2FA",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": true,
+			"can_media_tag": true,
+			"followed_by": true,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1117103773074305000,
+		"quoted_status_id_str": "1117103773074305025",
+		"quoted_status_permalink": {
+			"url": "https://t.co/xOa5DW6JZz",
+			"expanded": "https://twitter.com/NKResearcher/status/1117103773074305025",
+			"display": "twitter.com/NKResearcher/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Apr 13 16:34:28 +0000 2019",
+			"id": 1117103773074305000,
+			"id_str": "1117103773074305025",
+			"full_text": "Most female delegates visible thanks to colorful clothes and different hairstyles (but probably not all). None in first 2 rows and several clusters in back. Begs question who women in front are and how \"backbenchers\" positioned. For latter, might just be friends and acquaintances https://t.co/dCmPIE1F2i",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				280
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [],
+				"media": [
+					{
+						"id": 1117102543467962400,
+						"id_str": "1117102543467962370",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/D4C-2DkX4AI6krv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D4C-2DkX4AI6krv.jpg",
+						"url": "https://t.co/dCmPIE1F2i",
+						"display_url": "pic.twitter.com/dCmPIE1F2i",
+						"expanded_url": "https://twitter.com/NKResearcher/status/1117103773074305025/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 118,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 209,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 356,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"extended_entities": {
+				"media": [
+					{
+						"id": 1117102543467962400,
+						"id_str": "1117102543467962370",
+						"indices": [
+							281,
+							304
+						],
+						"media_url": "http://pbs.twimg.com/media/D4C-2DkX4AI6krv.jpg",
+						"media_url_https": "https://pbs.twimg.com/media/D4C-2DkX4AI6krv.jpg",
+						"url": "https://t.co/dCmPIE1F2i",
+						"display_url": "pic.twitter.com/dCmPIE1F2i",
+						"expanded_url": "https://twitter.com/NKResearcher/status/1117103773074305025/photo/1",
+						"type": "photo",
+						"sizes": {
+							"thumb": {
+								"w": 150,
+								"h": 150,
+								"resize": "crop"
+							},
+							"small": {
+								"w": 680,
+								"h": 118,
+								"resize": "fit"
+							},
+							"medium": {
+								"w": 1200,
+								"h": 209,
+								"resize": "fit"
+							},
+							"large": {
+								"w": 2048,
+								"h": 356,
+								"resize": "fit"
+							}
+						},
+						"features": {
+							"orig": {
+								"faces": []
+							},
+							"small": {
+								"faces": []
+							},
+							"medium": {
+								"faces": []
+							},
+							"large": {
+								"faces": []
+							}
+						}
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": 1116879268963860500,
+			"in_reply_to_status_id_str": "1116879268963860482",
+			"in_reply_to_user_id": 719930110871076900,
+			"in_reply_to_user_id_str": "719930110871076864",
+			"in_reply_to_screen_name": "NKResearcher",
+			"user": {
+				"id": 719930110871076900,
+				"id_str": "719930110871076864",
+				"name": "Martin Weiser",
+				"screen_name": "NKResearcher",
+				"location": "Seoul",
+				"description": "#DPRK historian, translator, freelance researcher, creator of  DPRK Legislation Database",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 557,
+				"friends_count": 754,
+				"listed_count": 17,
+				"created_at": "Tue Apr 12 16:48:08 +0000 2016",
+				"favourites_count": 268,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": false,
+				"verified": false,
+				"statuses_count": 1255,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+				"profile_image_url_https": "https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png",
+				"profile_link_color": "91D2FA",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": false,
+				"default_profile": false,
+				"default_profile_image": true,
+				"can_media_tag": true,
+				"followed_by": true,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 1,
+			"favorite_count": 3,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 9,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Group photo of #NorthKorea's recent party plenum with interesting positioning of female party officials: Kim Yo Jong &amp; Hyon Song Wol (green), Choe Son Hui (yellow), unknown (blue) and several grouped in the middle (red). Grouping together in back also visible in April SPA photo https://t.co/xOa5DW6JZz https://t.co/Sp5iuKXTAF"
+	},
+	{
 		"created_at": "Wed Jan 01 23:16:58 +0000 2020",
 		"id": 1212513071685521400,
 		"id_str": "1212513071685521408",
