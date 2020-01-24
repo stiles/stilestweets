5a92ab8ca995302d8b7fce3794cead62f1aab8e3
Matt Stiles <mattstiles@gmail.com>
2020-01-23 23:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14655,
-	"created_at": "Fri Jan 24 00:20:05 +0000 2020",
+	"tweets": 14656,
+	"created_at": "Fri Jan 24 07:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_01.js
+++ data/js/tweets/2020_01.js
@@ -1,6 +1,103 @@
 Grailbird.data.tweets_2020_01 = 
 [
 	{
+		"created_at": "Fri Jan 24 07:06:07 +0000 2020",
+		"id": 1220449792368828400,
+		"id_str": "1220603669978042368",
+		"full_text": "Have you ever felt anxious about going to or even outright avoided a restaurant/store of your particular ancestry bc you feel you don’t have an adequate grasp of the language and that there’s an expectation (whether internal or external) that you should? DM me! It’s for a piece.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 47717100,
+			"id_str": "47717100",
+			"name": "Lucas Kwan Peterson",
+			"screen_name": "lucaspeterson",
+			"location": "Los Angeles, CA",
+			"description": "Columnist @latimesfood. Previously @nytimes, @eater. Watch my new show OFF MENU on YouTube (Link below) lucas.peterson@latimes.com",
+			"url": "https://t.co/TAc7QhHS7q",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/TAc7QhHS7q",
+							"expanded_url": "http://youtube.com/latimesfood",
+							"display_url": "youtube.com/latimesfood",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 19020,
+			"friends_count": 989,
+			"listed_count": 173,
+			"created_at": "Tue Jun 16 19:23:13 +0000 2009",
+			"favourites_count": 6111,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3590,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "709397",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme6/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/847662917428195328/semhCmsJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/847662917428195328/semhCmsJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/47717100/1472961265",
+			"profile_link_color": "FF3300",
+			"profile_sidebar_border_color": "86A4A6",
+			"profile_sidebar_fill_color": "A0C5C7",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"can_media_tag": false,
+			"followed_by": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 20,
+		"favorite_count": 149,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Have you ever felt anxious about going to or even outright avoided a restaurant/store of your particular ancestry bc you feel you don’t have an adequate grasp of the language and that there’s an expectation (whether internal or external) that you should? DM me! It’s for a piece."
+	},
+	{
 		"created_at": "Fri Jan 24 00:19:33 +0000 2020",
 		"id": 1220501353945895000,
 		"id_str": "1220501353945894912",
