c18f0c85e49fe0cffbea9bf61ac68e79acfb1043
Matt Stiles <mattstiles@gmail.com>
2020-02-07 18:20:05 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14716,
-	"created_at": "Sat Feb 08 01:20:05 +0000 2020",
+	"tweets": 14717,
+	"created_at": "Sat Feb 08 02:20:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,224 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sat Feb 08 02:02:55 +0000 2020",
+		"id": 1225963186341335000,
+		"id_str": "1225963186341335041",
+		"full_text": "It's OK, ahjussi. Our languages are a tough marriage. https://t.co/bopB7J8Avc",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/bopB7J8Avc",
+					"expanded_url": "https://twitter.com/TheBrianPark/status/1225962395962507265",
+					"display_url": "twitter.com/TheBrianPark/s…",
+					"indices": [
+						54,
+						77
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Finding and telling stories w/ data, visuals & code. Dad. Texan. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11245,
+			"friends_count": 1919,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7400,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15029,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1580144526",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1225962395962507300,
+		"quoted_status_id_str": "1225962395962507265",
+		"quoted_status_permalink": {
+			"url": "https://t.co/bopB7J8Avc",
+			"expanded": "https://twitter.com/TheBrianPark/status/1225962395962507265",
+			"display": "twitter.com/TheBrianPark/s…"
+		},
+		"quoted_status": {
+			"created_at": "Sat Feb 08 01:59:47 +0000 2020",
+			"id": 1225962395962507300,
+			"id_str": "1225962395962507265",
+			"full_text": "My favorite TikTok right now https://t.co/ED809nG6UG",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				52
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": [
+					{
+						"url": "https://t.co/ED809nG6UG",
+						"expanded_url": "https://www.tiktok.com/@erickim926/video/6786340117494746373",
+						"display_url": "tiktok.com/@erickim926/vi…",
+						"indices": [
+							29,
+							52
+						]
+					}
+				]
+			},
+			"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 1072207332359041000,
+				"id_str": "1072207332359041024",
+				"name": "Brian Park",
+				"screen_name": "TheBrianPark",
+				"location": "Los Angeles, CA",
+				"description": "@latimes | California community college graduate | @normsrestaurant fan",
+				"url": "https://t.co/B0RvKygRn9",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/B0RvKygRn9",
+								"expanded_url": "https://www.latimes.com/",
+								"display_url": "latimes.com",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1149,
+				"friends_count": 2977,
+				"listed_count": 11,
+				"created_at": "Mon Dec 10 19:12:03 +0000 2018",
+				"favourites_count": 27806,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": false,
+				"statuses_count": 8260,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "F5F8FA",
+				"profile_background_image_url": null,
+				"profile_background_image_url_https": null,
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1171425636792954880/35qfi-A6_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171425636792954880/35qfi-A6_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/1072207332359041024/1544471582",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": true,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 0,
+			"favorite_count": 2,
+			"favorited": false,
+			"retweeted": false,
+			"possibly_sensitive": false,
+			"lang": "en"
+		},
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It's OK, ahjussi. Our languages are a tough marriage. https://t.co/bopB7J8Avc"
+	},
+	{
 		"created_at": "Sat Feb 08 01:16:18 +0000 2020",
 		"id": 1225929653329617000,
 		"id_str": "1225951456139694080",
