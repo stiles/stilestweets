413c7a322e44107197b3f71700d2bd1bf73dc96d
Matt Stiles <mattstiles@gmail.com>
2020-02-11 21:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14747,
-	"created_at": "Tue Feb 11 23:20:05 +0000 2020",
+	"tweets": 14748,
+	"created_at": "Wed Feb 12 05:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Wed Feb 12 04:37:43 +0000 2020",
+		"id": 1227451695665467400,
+		"id_str": "1227451695665467393",
+		"full_text": "@A_L Only one is competitive, and we're trying: \nhttps://t.co/31gO4iH2qT\nhttps://t.co/eYJ2WQishO\nhttps://t.co/fHYCtcIn87",
+		"truncated": false,
+		"display_text_range": [
+			5,
+			120
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "A_L",
+					"name": "Al Shaw",
+					"id": 7865282,
+					"id_str": "7865282",
+					"indices": [
+						0,
+						4
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/31gO4iH2qT",
+					"expanded_url": "https://www.latimes.com/california/story/2020-01-04/supervisor-endorsement-los-county-democrat-party-herb-wesson-jan-perry-holly-mitchell",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						49,
+						72
+					]
+				},
+				{
+					"url": "https://t.co/eYJ2WQishO",
+					"expanded_url": "https://www.latimes.com/california/story/2019-10-01/county-supervisor-2nd-district-campaign-donations",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						73,
+						96
+					]
+				},
+				{
+					"url": "https://t.co/fHYCtcIn87",
+					"expanded_url": "https://www.latimes.com/california/story/2019-09-29/gentrification-housing-homeless-supervisors-county-election",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						97,
+						120
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1227448809233535000,
+		"in_reply_to_status_id_str": "1227448809233534976",
+		"in_reply_to_user_id": 7865282,
+		"in_reply_to_user_id_str": "7865282",
+		"in_reply_to_screen_name": "A_L",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California w/ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics). 我喜欢牛肉面",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11247,
+			"friends_count": 1931,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7458,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15071,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1581444344",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@A_L Only one is competitive, and we're trying: \nhttps://t.co/31gO4iH2qT\nhttps://t.co/eYJ2WQishO\nhttps://t.co/fHYCtcIn87"
+	},
+	{
 		"created_at": "Tue Feb 11 22:52:30 +0000 2020",
 		"id": 1227364815909212200,
 		"id_str": "1227364815909212160",
