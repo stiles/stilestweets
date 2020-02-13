b0b51a4ac52b55cb81e7a539a81cd22b07a049cb
Matt Stiles <mattstiles@gmail.com>
2020-02-12 20:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14749,
-	"created_at": "Wed Feb 12 20:20:04 +0000 2020",
+	"tweets": 14750,
+	"created_at": "Thu Feb 13 04:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,237 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Thu Feb 13 04:12:35 +0000 2020",
+		"id": 1227807758394654700,
+		"id_str": "1227807758394654720",
+		"full_text": "@petchmo @flightradar24 For example: The photo on the left shows traffic on a Thursday morning from a few months ago, Dec. 12, compared with today. Same time: at 04:00 UTC. https://t.co/9sUGAO3JhF",
+		"truncated": false,
+		"display_text_range": [
+			24,
+			172
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "petchmo",
+					"name": "Ian Petchenik",
+					"id": 214613364,
+					"id_str": "214613364",
+					"indices": [
+						0,
+						8
+					]
+				},
+				{
+					"screen_name": "flightradar24",
+					"name": "Flightradar24",
+					"id": 134196350,
+					"id_str": "134196350",
+					"indices": [
+						9,
+						23
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1227807332639207400,
+					"id_str": "1227807332639207430",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/EQoMQ4qUcAYTGpc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQoMQ4qUcAYTGpc.jpg",
+					"url": "https://t.co/9sUGAO3JhF",
+					"display_url": "pic.twitter.com/9sUGAO3JhF",
+					"expanded_url": "https://twitter.com/stiles/status/1227807758394654720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 633,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1292,
+							"h": 682,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 359,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1227807332639207400,
+					"id_str": "1227807332639207430",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/EQoMQ4qUcAYTGpc.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQoMQ4qUcAYTGpc.jpg",
+					"url": "https://t.co/9sUGAO3JhF",
+					"display_url": "pic.twitter.com/9sUGAO3JhF",
+					"expanded_url": "https://twitter.com/stiles/status/1227807758394654720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 633,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1292,
+							"h": 682,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 359,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1227807332706349000,
+					"id_str": "1227807332706349057",
+					"indices": [
+						173,
+						196
+					],
+					"media_url": "http://pbs.twimg.com/media/EQoMQ46U8AEEWgw.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EQoMQ46U8AEEWgw.jpg",
+					"url": "https://t.co/9sUGAO3JhF",
+					"display_url": "pic.twitter.com/9sUGAO3JhF",
+					"expanded_url": "https://twitter.com/stiles/status/1227807758394654720/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1292,
+							"h": 682,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 633,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 359,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1227804485889585200,
+		"in_reply_to_status_id_str": "1227804485889585152",
+		"in_reply_to_user_id": 14790314,
+		"in_reply_to_user_id_str": "14790314",
+		"in_reply_to_screen_name": "stiles",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California w/ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics). 我喜欢牛肉面",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11246,
+			"friends_count": 1933,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7461,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15075,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1581444344",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "@petchmo @flightradar24 For example: The photo on the left shows traffic on a Thursday morning from a few months ago, Dec. 12, compared with today. Same time: at 04:00 UTC. https://t.co/9sUGAO3JhF"
+	},
+	{
 		"created_at": "Wed Feb 12 19:43:10 +0000 2020",
 		"id": 1227679556800151600,
 		"id_str": "1227679556800151552",
