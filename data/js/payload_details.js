e1ddab304c806fd117b669470c04e8f5ba575bfc
Matt Stiles <mattstiles@gmail.com>
2020-02-16 11:20:06 -0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14767,
-	"created_at": "Sun Feb 16 15:20:07 +0000 2020",
+	"tweets": 14768,
+	"created_at": "Sun Feb 16 19:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_02.js
+++ data/js/tweets/2020_02.js
@@ -1,6 +1,144 @@
 Grailbird.data.tweets_2020_02 = 
 [
 	{
+		"created_at": "Sun Feb 16 19:12:50 +0000 2020",
+		"id": 1229070609130279000,
+		"id_str": "1229121474327269376",
+		"full_text": "Please read this. It is one of the more important stories I have written recently: “He was my big, strong brother': Inside one Iowa family's anguish amid a rise in farmer suicides https://t.co/e0OyGQlwJE",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/e0OyGQlwJE",
+					"expanded_url": "https://www.latimes.com/world-nation/story/2020-02-16/he-was-my-big-strong-brother-inside-one-iowa-familys-anguish-over-farmer-suicides",
+					"display_url": "latimes.com/world-nation/s…",
+					"indices": [
+						180,
+						203
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 106291885,
+			"id_str": "106291885",
+			"name": "Seema",
+			"screen_name": "LATSeema",
+			"location": "",
+			"description": "LA Times political writer. Wannabe golfer. Patron of stray kittens. Bleed orange. RTs ≠ endorsements",
+			"url": "https://t.co/ifWgDURyxw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ifWgDURyxw",
+							"expanded_url": "http://www.latimes.com",
+							"display_url": "latimes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22740,
+			"friends_count": 3280,
+			"listed_count": 877,
+			"created_at": "Tue Jan 19 02:52:20 +0000 2010",
+			"favourites_count": 5918,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 17893,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1171819086449278976/If6wXMnp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1171819086449278976/If6wXMnp_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "8fa6d7a33b83ef26",
+			"url": "https://api.twitter.com/1.1/geo/id/8fa6d7a33b83ef26.json",
+			"place_type": "city",
+			"name": "Paradise",
+			"full_name": "Paradise, NV",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-115.2092535,
+							35.984784
+						],
+						[
+							-115.0610763,
+							35.984784
+						],
+						[
+							-115.0610763,
+							36.137145
+						],
+						[
+							-115.2092535,
+							36.137145
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 29,
+		"favorite_count": 36,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Please read this. It is one of the more important stories I have written recently: “He was my big, strong brother': Inside one Iowa family's anguish amid a rise in farmer suicides https://t.co/e0OyGQlwJE"
+	},
+	{
 		"created_at": "Sun Feb 16 14:54:29 +0000 2020",
 		"id": 1229056462078365700,
 		"id_str": "1229056462078365696",
