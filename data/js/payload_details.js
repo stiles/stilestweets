21617b4c60cbe2d9f49b527cb311eefb2e38cc1b
Matt Stiles <mattstiles@gmail.com>
2020-03-09 13:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14852,
-	"created_at": "Mon Mar 09 04:20:04 +0000 2020",
+	"tweets": 14853,
+	"created_at": "Mon Mar 09 20:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Mon Mar 09 19:45:57 +0000 2020",
+		"id": 1237093999929725000,
+		"id_str": "1237102343478370305",
+		"full_text": "That was quick https://t.co/ScKVubzPx2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			14
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1237093995144040400,
+					"id_str": "1237093995144040448",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ESsKbyZUUAAT3r3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESsKbyZUUAAT3r3.jpg",
+					"url": "https://t.co/ScKVubzPx2",
+					"display_url": "pic.twitter.com/ScKVubzPx2",
+					"expanded_url": "https://twitter.com/ReporterClaudia/status/1237093999929724928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1237093995144040400,
+					"id_str": "1237093995144040448",
+					"indices": [
+						15,
+						38
+					],
+					"media_url": "http://pbs.twimg.com/media/ESsKbyZUUAAT3r3.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ESsKbyZUUAAT3r3.jpg",
+					"url": "https://t.co/ScKVubzPx2",
+					"display_url": "pic.twitter.com/ScKVubzPx2",
+					"expanded_url": "https://twitter.com/ReporterClaudia/status/1237093999929724928/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 301189264,
+			"id_str": "301189264",
+			"name": "Claudia Peschiutta",
+			"screen_name": "ReporterClaudia",
+			"location": "Los Angeles",
+			"description": "News with a side of humor. I work @KNX1070. Hablo español. RTs are not endorsements. Tips? DM me.",
+			"url": "https://t.co/nJac11hlei",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nJac11hlei",
+							"expanded_url": "http://knx1070.com",
+							"display_url": "knx1070.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9089,
+			"friends_count": 1422,
+			"listed_count": 380,
+			"created_at": "Thu May 19 01:33:28 +0000 2011",
+			"favourites_count": 14091,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 35009,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1106781873223553025/BOU1Q9Ij_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1106781873223553025/BOU1Q9Ij_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/301189264/1407910303",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 7,
+		"favorite_count": 26,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "That was quick https://t.co/ScKVubzPx2"
+	},
+	{
 		"created_at": "Mon Mar 09 03:31:28 +0000 2020",
 		"id": 1236856890862325800,
 		"id_str": "1236857105404973057",
