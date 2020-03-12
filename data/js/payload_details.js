e2e5bddd1727efce4b1986891ce0b4d80504ed44
Matt Stiles <mattstiles@gmail.com>
2020-03-11 17:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14867,
-	"created_at": "Wed Mar 11 22:20:05 +0000 2020",
+	"tweets": 14868,
+	"created_at": "Thu Mar 12 00:20:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Wed Mar 11 23:41:47 +0000 2020",
+		"id": 1237886469668102100,
+		"id_str": "1237886469668102145",
+		"full_text": "A big group of LA-area members of Congress have called for an investigation into the voting issues LA County experienced during the March 3 primary. Letter to @LACountyRRCC: https://t.co/9Nn4zFQfnN\n\nICYMI: the @LACountyBOS did this yesterday. Our story:  https://t.co/wJeZjwzSqz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LACountyRRCC",
+					"name": "Dean Logan, RR/CC",
+					"id": 21807663,
+					"id_str": "21807663",
+					"indices": [
+						159,
+						172
+					]
+				},
+				{
+					"screen_name": "LACountyBOS",
+					"name": "LACountyBOS",
+					"id": 1144740456586047500,
+					"id_str": "1144740456586047489",
+					"indices": [
+						210,
+						222
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/9Nn4zFQfnN",
+					"expanded_url": "https://lowenthal.house.gov/sites/lowenthal.house.gov/files/ASL%20Letter%20to%20LA%20Registrar-Recorder%20-%20Final.pdf",
+					"display_url": "lowenthal.house.gov/sites/lowentha…",
+					"indices": [
+						174,
+						197
+					]
+				},
+				{
+					"url": "https://t.co/wJeZjwzSqz",
+					"expanded_url": "https://www.latimes.com/california/story/2020-03-10/election-los-angeles-county-voting-problems-dean-logan?_amp=true&__twitter_impression=true",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						255,
+						278
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California. I ❤️ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11276,
+			"friends_count": 1976,
+			"listed_count": 879,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7665,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15224,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1209725366132043776/WUotZHM9_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1583580925",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3b77caf94bfc81fe",
+			"url": "https://api.twitter.com/1.1/geo/id/3b77caf94bfc81fe.json",
+			"place_type": "city",
+			"name": "Los Angeles",
+			"full_name": "Los Angeles, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.668404,
+							33.704538
+						],
+						[
+							-118.155409,
+							33.704538
+						],
+						[
+							-118.155409,
+							34.337041
+						],
+						[
+							-118.668404,
+							34.337041
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "A big group of LA-area members of Congress have called for an investigation into the voting issues LA County experienced during the March 3 primary. Letter to @LACountyRRCC: https://t.co/9Nn4zFQfnN\n\nICYMI: the @LACountyBOS did this yesterday. Our story:  https://t.co/wJeZjwzSqz"
+	},
+	{
 		"created_at": "Wed Mar 11 21:57:50 +0000 2020",
 		"id": 1237856696787030000,
 		"id_str": "1237860307843739649",
