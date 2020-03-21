7983dd982ab6b1e426480c184712be9866053d35
Matt Stiles <mattstiles@gmail.com>
2020-03-20 23:20:06 -0700

    Added 1 tweets

--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Sat Mar 21 06:07:30 +0000 2020",
+		"id": 1241235838354083800,
+		"id_str": "1241245026199302144",
+		"full_text": "Currently feeling very grateful 1) for having a job that keeps me busy and occupied during this stressful time 2) that I can do it from home 3) for our @datagraphics bunch. \n\nAnyway we just published this! \nhttps://t.co/P0agH0oe3e",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			230
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "datagraphics",
+					"name": "Los Angeles Times Data and Graphics",
+					"id": 17918860,
+					"id_str": "17918860",
+					"indices": [
+						152,
+						165
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/P0agH0oe3e",
+					"expanded_url": "https://www.latimes.com/projects/los-angeles-coronavirus-slowdown-maps",
+					"display_url": "latimes.com/projects/los-a…",
+					"indices": [
+						207,
+						230
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 192995488,
+			"id_str": "192995488",
+			"name": "Priya Krishnakumar",
+			"screen_name": "priyakkumar",
+			"location": "",
+			"description": "Graphics maps n’ data @latimes. Very loud typer. Cake is a breakfast food!! priya@latimes.com",
+			"url": "https://t.co/w8ovcvH7jJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/w8ovcvH7jJ",
+							"expanded_url": "http://priyakrishnakumar.com",
+							"display_url": "priyakrishnakumar.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 5785,
+			"friends_count": 721,
+			"listed_count": 115,
+			"created_at": "Mon Sep 20 18:18:50 +0000 2010",
+			"favourites_count": 8977,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 2430,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "EBEBEB",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme7/bg.gif",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1174894404911292416/t8_zGAb7_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1174894404911292416/t8_zGAb7_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/192995488/1496358615",
+			"profile_link_color": "009FBF",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "EBBE3D",
+			"profile_text_color": "EED68C",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 17,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Currently feeling very grateful 1) for having a job that keeps me busy and occupied during this stressful time 2) that I can do it from home 3) for our @datagraphics bunch. \n\nAnyway we just published this! \nhttps://t.co/P0agH0oe3e"
+	},
+	{
 		"created_at": "Sat Mar 21 03:43:41 +0000 2020",
 		"id": 1241208835076804600,
 		"id_str": "1241208835076804608",
