2ae83e20e0ccc64d0001abc5d00cd96d0cc9d895
Matt Stiles <mattstiles@gmail.com>
2020-03-22 21:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 14931,
-	"created_at": "Mon Mar 23 00:20:05 +0000 2020",
+	"tweets": 14932,
+	"created_at": "Mon Mar 23 04:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_03.js
+++ data/js/tweets/2020_03.js
@@ -1,6 +1,199 @@
 Grailbird.data.tweets_2020_03 = 
 [
 	{
+		"created_at": "Mon Mar 23 03:31:06 +0000 2020",
+		"id": 1241930445987573800,
+		"id_str": "1241930445987573760",
+		"full_text": "My new favorite person in the world, @angelaishere, today graciously delivered us (at a careful distance) hearty portions of the best ramyun and jajangmyeon! So kind. \n\nA few bags a day keeps the #COVID19 away! https://t.co/UjtFyHfoEx",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "COVID19",
+					"indices": [
+						196,
+						204
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "angelaishere",
+					"name": "Angela Kim",
+					"id": 16252393,
+					"id_str": "16252393",
+					"indices": [
+						37,
+						50
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1241930405466398700,
+					"id_str": "1241930405466398720",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/ETw5H62U4AAzBnP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETw5H62U4AAzBnP.jpg",
+					"url": "https://t.co/UjtFyHfoEx",
+					"display_url": "pic.twitter.com/UjtFyHfoEx",
+					"expanded_url": "https://twitter.com/stiles/status/1241930445987573760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1241930405466398700,
+					"id_str": "1241930405466398720",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/ETw5H62U4AAzBnP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ETw5H62U4AAzBnP.jpg",
+					"url": "https://t.co/UjtFyHfoEx",
+					"display_url": "pic.twitter.com/UjtFyHfoEx",
+					"expanded_url": "https://twitter.com/stiles/status/1241930445987573760/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California. I ❤️ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11341,
+			"friends_count": 2000,
+			"listed_count": 880,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 7785,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15306,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1583580925",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "My new favorite person in the world, @angelaishere, today graciously delivered us (at a careful distance) hearty portions of the best ramyun and jajangmyeon! So kind. \n\nA few bags a day keeps the #COVID19 away! https://t.co/UjtFyHfoEx"
+	},
+	{
 		"created_at": "Sun Mar 22 23:44:18 +0000 2020",
 		"id": 1241246742701457400,
 		"id_str": "1241873366501298176",
