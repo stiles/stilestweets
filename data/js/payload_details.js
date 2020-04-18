d9f38a3efc573bbdcc121cfb91e98395d05fd4db
Matt Stiles <mattstiles@gmail.com>
2020-04-18 08:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15056,
-	"created_at": "Sat Apr 18 14:20:05 +0000 2020",
+	"tweets": 15057,
+	"created_at": "Sat Apr 18 15:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 18 14:35:39 +0000 2020",
+		"id": 1251519768944767000,
+		"id_str": "1251519768944766976",
+		"full_text": "There's been an uptick in residential fires around L.A. as people stay home for coronavirus. Here's the @LACOFD's instructional manual for kids, in case your little ones are running low on coloring book pages: https://t.co/2L0TX9NLHO https://t.co/mAzrBF4eS8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			233
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "LACOFD",
+					"name": "LACoFD",
+					"id": 308751479,
+					"id_str": "308751479",
+					"indices": [
+						104,
+						111
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/2L0TX9NLHO",
+					"expanded_url": "https://www.fire.lacounty.gov/wp-content/uploads/2020/01/FIRE-Coloring-Book_regular-book_Small_013120.pdf",
+					"display_url": "fire.lacounty.gov/wp-content/upl…",
+					"indices": [
+						210,
+						233
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1251519439314403300,
+					"id_str": "1251519439314403328",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/EV5KTJnUwAA0V3c.png",
+					"media_url_https": "https://pbs.twimg.com/media/EV5KTJnUwAA0V3c.png",
+					"url": "https://t.co/mAzrBF4eS8",
+					"display_url": "pic.twitter.com/mAzrBF4eS8",
+					"expanded_url": "https://twitter.com/stiles/status/1251519768944766976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 613,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 755,
+							"h": 681,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 755,
+							"h": 681,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1251519439314403300,
+					"id_str": "1251519439314403328",
+					"indices": [
+						234,
+						257
+					],
+					"media_url": "http://pbs.twimg.com/media/EV5KTJnUwAA0V3c.png",
+					"media_url_https": "https://pbs.twimg.com/media/EV5KTJnUwAA0V3c.png",
+					"url": "https://t.co/mAzrBF4eS8",
+					"display_url": "pic.twitter.com/mAzrBF4eS8",
+					"expanded_url": "https://twitter.com/stiles/status/1251519768944766976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 613,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 755,
+							"h": 681,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 755,
+							"h": 681,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California. I ❤️ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11377,
+			"friends_count": 2034,
+			"listed_count": 878,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8006,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15467,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1586574222",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "There's been an uptick in residential fires around L.A. as people stay home for coronavirus. Here's the @LACOFD's instructional manual for kids, in case your little ones are running low on coloring book pages: https://t.co/2L0TX9NLHO https://t.co/mAzrBF4eS8"
+	},
+	{
 		"created_at": "Sat Apr 18 14:11:08 +0000 2020",
 		"id": 1251513599089995800,
 		"id_str": "1251513599089995781",
