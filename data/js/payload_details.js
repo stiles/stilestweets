5c655265a43492a94dde5be0134ec0e7065e4feb
Matt Stiles <mattstiles@gmail.com>
2020-04-21 14:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15072,
-	"created_at": "Tue Apr 21 17:20:06 +0000 2020",
+	"tweets": 15073,
+	"created_at": "Tue Apr 21 21:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,256 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Tue Apr 21 21:03:12 +0000 2020",
+		"id": 1252704460029091800,
+		"id_str": "1252704460029091841",
+		"full_text": "I can't this guy hasn't deleted this tweet yet! He's had 16 hours to Google \"math\". https://t.co/IySfbFzaIC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			83
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/IySfbFzaIC",
+					"expanded_url": "https://twitter.com/mitchellvii/status/1252453257080909824",
+					"display_url": "twitter.com/mitchellvii/st…",
+					"indices": [
+						84,
+						107
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California. I ❤️ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11386,
+			"friends_count": 2040,
+			"listed_count": 876,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8035,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15486,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1586574222",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "3b77caf94bfc81fe",
+			"url": "https://api.twitter.com/1.1/geo/id/3b77caf94bfc81fe.json",
+			"place_type": "city",
+			"name": "Los Angeles",
+			"full_name": "Los Angeles, CA",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.668404,
+							33.704538
+						],
+						[
+							-118.155409,
+							33.704538
+						],
+						[
+							-118.155409,
+							34.337041
+						],
+						[
+							-118.668404,
+							34.337041
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1252453257080909800,
+		"quoted_status_id_str": "1252453257080909824",
+		"quoted_status_permalink": {
+			"url": "https://t.co/IySfbFzaIC",
+			"expanded": "https://twitter.com/mitchellvii/status/1252453257080909824",
+			"display": "twitter.com/mitchellvii/st…"
+		},
+		"quoted_status": {
+			"created_at": "Tue Apr 21 04:25:00 +0000 2020",
+			"id": 1252453257080909800,
+			"id_str": "1252453257080909824",
+			"full_text": "Interesting that ActBlue raised an average donation of $30.38.\n\nThis would imply people are donating in uneven numbers, including pennies, which would be odd. OR it would indicate untraceable foreign donations and an exchange rate translation, which would be illegal.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				267
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 17980523,
+				"id_str": "17980523",
+				"name": "Bill Mitchell",
+				"screen_name": "mitchellvii",
+				"location": "Miami, FL",
+				"description": "Host of YourVoice™ America at https://t.co/fXUiBUULvs,  Mon-Frid 10am ET #TrustTrump #MAGA #Trump2020 #2ASupporter #TrustThePlan",
+				"url": "https://t.co/9CnMPoreW4",
+				"entities": {
+					"url": {
+						"urls": [
+							{
+								"url": "https://t.co/9CnMPoreW4",
+								"expanded_url": "https://www.yourvoiceamerica.tv",
+								"display_url": "yourvoiceamerica.tv",
+								"indices": [
+									0,
+									23
+								]
+							}
+						]
+					},
+					"description": {
+						"urls": [
+							{
+								"url": "https://t.co/fXUiBUULvs",
+								"expanded_url": "http://yourvoiceamerica.tv",
+								"display_url": "yourvoiceamerica.tv",
+								"indices": [
+									30,
+									53
+								]
+							}
+						]
+					}
+				},
+				"protected": false,
+				"followers_count": 568963,
+				"friends_count": 10134,
+				"listed_count": 3929,
+				"created_at": "Tue Dec 09 01:54:21 +0000 2008",
+				"favourites_count": 27615,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 168738,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "000000",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1180905016326004736/7yT_G3ZX_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1180905016326004736/7yT_G3ZX_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/17980523/1527114235",
+				"profile_link_color": "ABB8C2",
+				"profile_sidebar_border_color": "000000",
+				"profile_sidebar_fill_color": "000000",
+				"profile_text_color": "000000",
+				"profile_use_background_image": false,
+				"has_extended_profile": true,
+				"default_profile": false,
+				"default_profile_image": false,
+				"following": false,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 2204,
+			"favorite_count": 6653,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I can't this guy hasn't deleted this tweet yet! He's had 16 hours to Google \"math\". https://t.co/IySfbFzaIC"
+	},
+	{
 		"created_at": "Tue Apr 21 17:16:07 +0000 2020",
 		"id": 1252647316055453700,
 		"id_str": "1252647316055453696",
