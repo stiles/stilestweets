dbd4fd0200b45941942d413dc178ca8b93b7a618
Matt Stiles <mattstiles@gmail.com>
2020-04-25 15:20:07 -0700

    Added 1 tweets

--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 25 21:24:42 +0000 2020",
+		"id": 1254159422630183000,
+		"id_str": "1254159422630182913",
+		"full_text": "I've gone down another Kim Jong Un rabbit hole and find myself staring at satellite imagery of his compound in Wonsan, estimating the width of boat houses so I can figure out the size of his yacht underneath. \n\nYou can, too: https://t.co/tigGiYhFPD https://t.co/aphEm9enMl",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			248
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tigGiYhFPD",
+					"expanded_url": "https://www.google.com/maps/place/Yodok+Concentration+Camp/@39.1921594,127.4031379,662m/data=!3m1!1e3!4m5!3m4!1s0x5fd41ce2d92913f5:0x3f4f46d8ffdaa71a!8m2!3d39.6741014!4d126.8513914",
+					"display_url": "google.com/maps/place/Yod…",
+					"indices": [
+						225,
+						248
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1254158872471715800,
+					"id_str": "1254158872471715840",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/EWeq2YWUwAAIZRb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWeq2YWUwAAIZRb.jpg",
+					"url": "https://t.co/aphEm9enMl",
+					"display_url": "pic.twitter.com/aphEm9enMl",
+					"expanded_url": "https://twitter.com/stiles/status/1254159422630182913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 415,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1251,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 733,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1254158872471715800,
+					"id_str": "1254158872471715840",
+					"indices": [
+						249,
+						272
+					],
+					"media_url": "http://pbs.twimg.com/media/EWeq2YWUwAAIZRb.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EWeq2YWUwAAIZRb.jpg",
+					"url": "https://t.co/aphEm9enMl",
+					"display_url": "pic.twitter.com/aphEm9enMl",
+					"expanded_url": "https://twitter.com/stiles/status/1254159422630182913/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 415,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1251,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 733,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California. I ❤️ data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11388,
+			"friends_count": 2042,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8065,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15509,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1586574222",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I've gone down another Kim Jong Un rabbit hole and find myself staring at satellite imagery of his compound in Wonsan, estimating the width of boat houses so I can figure out the size of his yacht underneath. \n\nYou can, too: https://t.co/tigGiYhFPD https://t.co/aphEm9enMl"
+	},
+	{
 		"created_at": "Sat Apr 25 20:56:26 +0000 2020",
 		"id": 1254152310118215700,
 		"id_str": "1254152310118215680",
