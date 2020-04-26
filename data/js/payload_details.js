5326b200c95bc57fdc22028a749fcc4536c5f7c1
Matt Stiles <mattstiles@gmail.com>
2020-04-25 17:20:08 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15093,
-	"created_at": "Sat Apr 25 23:20:07 +0000 2020",
+	"tweets": 15094,
+	"created_at": "Sun Apr 26 00:20:07 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_04.js
+++ data/js/tweets/2020_04.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_04 = 
 [
 	{
+		"created_at": "Sat Apr 25 23:36:43 +0000 2020",
+		"id": 1254190134070820900,
+		"id_str": "1254192649109561344",
+		"full_text": "An intriguing possibility if Kim Jong Un is truly ill. In North Korea, the Fourth Man Could Be a Woman https://t.co/lvrIHwLbEe via @NewYorker",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			141
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "NewYorker",
+					"name": "The New Yorker",
+					"id": 14677919,
+					"id_str": "14677919",
+					"indices": [
+						131,
+						141
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/lvrIHwLbEe",
+					"expanded_url": "https://www.newyorker.com/news/daily-comment/in-north-korea-the-fourth-man-could-be-a-woman?utm_source=twitter&utm_medium=social&utm_campaign=onsite-share&utm_brand=the-new-yorker&utm_social-type=earned",
+					"display_url": "newyorker.com/news/daily-com…",
+					"indices": [
+						103,
+						126
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com\" rel=\"nofollow\">Twitter Web Client</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 27571535,
+			"id_str": "27571535",
+			"name": "Barbara Demick",
+			"screen_name": "BarbaraDemick",
+			"location": "New York, USA",
+			"description": "Former Beijing bureau chief Los Angeles Times; author Nothing to Envy: Ordinary Lives in North Korea and Logavina Street: Life and Death in a Sarajevo Neighborh",
+			"url": "http://t.co/KsFqx4opZh",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "http://t.co/KsFqx4opZh",
+							"expanded_url": "http://www.nothingtoenvy.com",
+							"display_url": "nothingtoenvy.com",
+							"indices": [
+								0,
+								22
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 20712,
+			"friends_count": 919,
+			"listed_count": 945,
+			"created_at": "Mon Mar 30 03:07:16 +0000 2009",
+			"favourites_count": 633,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 4583,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/146159221/Onthebussmall_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/146159221/Onthebussmall_normal.jpg",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "ED1D1D",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "0F0D0D",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "An intriguing possibility if Kim Jong Un is truly ill. In North Korea, the Fourth Man Could Be a Woman https://t.co/lvrIHwLbEe via @NewYorker"
+	},
+	{
 		"created_at": "Sat Apr 25 23:09:42 +0000 2020",
 		"id": 1254185846799908900,
 		"id_str": "1254185846799908864",
