f48f14946c510bada3b7665ff999c09ae36144ee
Matt Stiles <mattstiles@gmail.com>
2020-04-30 20:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15119,
-	"created_at": "Thu Apr 30 23:20:05 +0000 2020",
+	"tweets": 15120,
+	"created_at": "Fri May 01 03:20:05 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_05.js
@@ -0,0 +1,248 @@
+Grailbird.data.tweets_2020_05 = 
+[
+	{
+		"created_at": "Fri May 01 02:39:39 +0000 2020",
+		"id": 1256045690393817000,
+		"id_str": "1256050622198984704",
+		"full_text": "Nothing quite like getting OFF the night shift. Congrats @jaclyncosgrove on yer last night before taking over LA County beat. Here's to a magical 11:01 p.m. from all who came before u incl. me @hbecerraLATimes @AleneTchek @MattHjourno @jvgarrison @LAJourno @MatthewLait https://t.co/0qbyCQySx8",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			269
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jaclyncosgrove",
+					"name": "Jaclyn Cosgrove",
+					"id": 15467750,
+					"id_str": "15467750",
+					"indices": [
+						57,
+						72
+					]
+				},
+				{
+					"screen_name": "hbecerraLATimes",
+					"name": "Hector Becerra",
+					"id": 19074004,
+					"id_str": "19074004",
+					"indices": [
+						193,
+						209
+					]
+				},
+				{
+					"screen_name": "AleneTchek",
+					"name": "Alene Tchekmedyian",
+					"id": 26102955,
+					"id_str": "26102955",
+					"indices": [
+						210,
+						221
+					]
+				},
+				{
+					"screen_name": "MattHjourno",
+					"name": "Matt Hamilton",
+					"id": 76459927,
+					"id_str": "76459927",
+					"indices": [
+						222,
+						234
+					]
+				},
+				{
+					"screen_name": "jvgarrison",
+					"name": "jessica garrison",
+					"id": 22546566,
+					"id_str": "22546566",
+					"indices": [
+						235,
+						246
+					]
+				},
+				{
+					"screen_name": "LAJourno",
+					"name": "robert lopez",
+					"id": 16933383,
+					"id_str": "16933383",
+					"indices": [
+						247,
+						256
+					]
+				},
+				{
+					"screen_name": "MatthewLait",
+					"name": "Matt Lait",
+					"id": 145781306,
+					"id_str": "145781306",
+					"indices": [
+						257,
+						269
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1256045593392058400,
+					"id_str": "1256045593392058368",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5ez_fUEAAi3Mt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5ez_fUEAAi3Mt.jpg",
+					"url": "https://t.co/0qbyCQySx8",
+					"display_url": "pic.twitter.com/0qbyCQySx8",
+					"expanded_url": "https://twitter.com/shelbygrad/status/1256045690393817090/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 488,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1298,
+							"h": 932,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 862,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256045593392058400,
+					"id_str": "1256045593392058368",
+					"indices": [
+						270,
+						293
+					],
+					"media_url": "http://pbs.twimg.com/media/EW5ez_fUEAAi3Mt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EW5ez_fUEAAi3Mt.jpg",
+					"url": "https://t.co/0qbyCQySx8",
+					"display_url": "pic.twitter.com/0qbyCQySx8",
+					"expanded_url": "https://twitter.com/shelbygrad/status/1256045690393817090/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 488,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1298,
+							"h": 932,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 862,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14827461,
+			"id_str": "14827461",
+			"name": "Shelby Grad",
+			"screen_name": "shelbygrad",
+			"location": "🌴Los Angeles💥",
+			"description": "Deputy Managing Editor at @latimes.",
+			"url": "https://t.co/emWaPa3SBu",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/emWaPa3SBu",
+							"expanded_url": "http://www.latimes.com/california",
+							"display_url": "latimes.com/california",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14551,
+			"friends_count": 1019,
+			"listed_count": 520,
+			"created_at": "Mon May 19 02:25:01 +0000 2008",
+			"favourites_count": 6538,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 45896,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1176642755608449025/HU_Ji59-_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1176642755608449025/HU_Ji59-_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14827461/1583199878",
+			"profile_link_color": "1B95E0",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 4,
+		"favorite_count": 28,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Nothing quite like getting OFF the night shift. Congrats @jaclyncosgrove on yer last night before taking over LA County beat. Here's to a magical 11:01 p.m. from all who came before u incl. me @hbecerraLATimes @AleneTchek @MattHjourno @jvgarrison @LAJourno @MatthewLait https://t.co/0qbyCQySx8"
+	}
+]
