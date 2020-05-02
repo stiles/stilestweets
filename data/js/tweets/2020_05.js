58b3b54db7b124e333b20b01f3401c6bc5303845
Matt Stiles <mattstiles@gmail.com>
2020-05-02 09:20:05 -0700

    Added 1 tweets

--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sat May 02 15:20:21 +0000 2020",
+		"id": 1256604445632155600,
+		"id_str": "1256604445632155649",
+		"full_text": "Just noticed this super useful little floating directory on @observablehq notebooks. Helps navigation on the page tremendously. Great feature. https://t.co/oqWsdKCMVe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			142
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "observablehq",
+					"name": "Observable",
+					"id": 905255756789825500,
+					"id_str": "905255756789825536",
+					"indices": [
+						60,
+						73
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1256604413193379800,
+					"id_str": "1256604413193379842",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/EXBbDleUYAI_Wtc.png",
+					"media_url_https": "https://pbs.twimg.com/media/EXBbDleUYAI_Wtc.png",
+					"url": "https://t.co/oqWsdKCMVe",
+					"display_url": "pic.twitter.com/oqWsdKCMVe",
+					"expanded_url": "https://twitter.com/stiles/status/1256604445632155649/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 206,
+							"h": 471,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 206,
+							"h": 471,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 206,
+							"h": 471,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1256604413193379800,
+					"id_str": "1256604413193379842",
+					"indices": [
+						143,
+						166
+					],
+					"media_url": "http://pbs.twimg.com/media/EXBbDleUYAI_Wtc.png",
+					"media_url_https": "https://pbs.twimg.com/media/EXBbDleUYAI_Wtc.png",
+					"url": "https://t.co/oqWsdKCMVe",
+					"display_url": "pic.twitter.com/oqWsdKCMVe",
+					"expanded_url": "https://twitter.com/stiles/status/1256604445632155649/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 206,
+							"h": 471,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 206,
+							"h": 471,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 206,
+							"h": 471,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California with data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11410,
+			"friends_count": 2054,
+			"listed_count": 877,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8155,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15562,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1586574222",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just noticed this super useful little floating directory on @observablehq notebooks. Helps navigation on the page tremendously. Great feature. https://t.co/oqWsdKCMVe"
+	},
+	{
 		"created_at": "Sat May 02 15:04:22 +0000 2020",
 		"id": 1256461425020555300,
 		"id_str": "1256600426859782144",
