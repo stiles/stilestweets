857d9e8592fb61fac527475369c4728d9ac538af
Matt Stiles <mattstiles@gmail.com>
2020-05-13 17:20:06 -0700

    Added 1 tweets

--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Thu May 14 00:07:28 +0000 2020",
+		"id": 1260723368577253400,
+		"id_str": "1260723368577253377",
+		"full_text": "I wanted to test @Google Meet. Daughter wanted to test a virtual @classroomscreen. Both seem to work. https://t.co/C1JKFaCvQz",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			101
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Google",
+					"name": "Google",
+					"id": 20536157,
+					"id_str": "20536157",
+					"indices": [
+						17,
+						24
+					]
+				},
+				{
+					"screen_name": "Classroomscreen",
+					"name": "Classroomscreen",
+					"id": 836564621729345500,
+					"id_str": "836564621729345537",
+					"indices": [
+						65,
+						81
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1260722014144192500,
+					"id_str": "1260722014144192513",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/EX77_gWUYAEMX_o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX77_gWUYAEMX_o.jpg",
+					"url": "https://t.co/C1JKFaCvQz",
+					"display_url": "pic.twitter.com/C1JKFaCvQz",
+					"expanded_url": "https://twitter.com/stiles/status/1260723368577253377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1138,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1260722014144192500,
+					"id_str": "1260722014144192513",
+					"indices": [
+						102,
+						125
+					],
+					"media_url": "http://pbs.twimg.com/media/EX77_gWUYAEMX_o.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EX77_gWUYAEMX_o.jpg",
+					"url": "https://t.co/C1JKFaCvQz",
+					"display_url": "pic.twitter.com/C1JKFaCvQz",
+					"expanded_url": "https://twitter.com/stiles/status/1260723368577253377/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 667,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1138,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 378,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California with data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11452,
+			"friends_count": 2069,
+			"listed_count": 860,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8359,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15689,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1589078367",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I wanted to test @Google Meet. Daughter wanted to test a virtual @classroomscreen. Both seem to work. https://t.co/C1JKFaCvQz"
+	},
+	{
 		"created_at": "Wed May 13 23:07:58 +0000 2020",
 		"id": 1260708391812399000,
 		"id_str": "1260708391812399104",
@@ -200,7 +395,7 @@ Grailbird.data.tweets_2020_05 =
 			"created_at": "Tue May 12 03:43:18 +0000 2020",
 			"id": 1260052908546027500,
 			"id_str": "1260052908546027520",
-			"full_text": "@sally_ann_cole @SupJaniceHahn Where has aerosol superspreader come from? 😂 it barely exists in the air, almost every single case has been from contact. It’s a novel virus. Not airborne. 🤦🏼‍♀️",
+			"full_text": "@sally_ann_cole @SupJaniceHahn Where has aerosol superspreader come from? 😂 it barely exists in the air, almost every single case has been from contact. It’s a novel virus. Not airborne. 🤦🏼♀️",
 			"truncated": false,
 			"display_text_range": [
 				31,
