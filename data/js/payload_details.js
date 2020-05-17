e24f53d6caa4fa5e22cc52771f7b885bddb2386c
Matt Stiles <mattstiles@gmail.com>
2020-05-17 10:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15246,
-	"created_at": "Sun May 17 16:20:05 +0000 2020",
+	"tweets": 15247,
+	"created_at": "Sun May 17 17:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,180 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 17 16:21:00 +0000 2020",
+		"id": 1262055527510495200,
+		"id_str": "1262055527510495232",
+		"full_text": "Kitteh, there is no escaping The 'Rona. https://t.co/x0H1E8QUh6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			39
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1262055351135821800,
+					"id_str": "1262055351135821825",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/EYO4p7tU4AE5g9F.png",
+					"media_url_https": "https://pbs.twimg.com/media/EYO4p7tU4AE5g9F.png",
+					"url": "https://t.co/x0H1E8QUh6",
+					"display_url": "pic.twitter.com/x0H1E8QUh6",
+					"expanded_url": "https://twitter.com/stiles/status/1262055527510495232/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 468,
+							"h": 734,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 468,
+							"h": 734,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 434,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1262055351135821800,
+					"id_str": "1262055351135821825",
+					"indices": [
+						40,
+						63
+					],
+					"media_url": "http://pbs.twimg.com/media/EYO4p7tU4AE5g9F.png",
+					"media_url_https": "https://pbs.twimg.com/media/EYO4p7tU4AE5g9F.png",
+					"url": "https://t.co/x0H1E8QUh6",
+					"display_url": "pic.twitter.com/x0H1E8QUh6",
+					"expanded_url": "https://twitter.com/stiles/status/1262055527510495232/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 468,
+							"h": 734,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 468,
+							"h": 734,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 434,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California with data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11462,
+			"friends_count": 2076,
+			"listed_count": 859,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8396,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15714,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1589731465",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Kitteh, there is no escaping The 'Rona. https://t.co/x0H1E8QUh6"
+	},
+	{
 		"created_at": "Sun May 17 16:14:31 +0000 2020",
 		"id": 1262053741554888700,
 		"id_str": "1262053895771054081",
