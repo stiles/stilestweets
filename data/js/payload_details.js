893756aad4d8a4ea8fd0f7b2c8ad681060e26375
Matt Stiles <mattstiles@gmail.com>
2020-05-17 14:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15248,
-	"created_at": "Sun May 17 18:20:06 +0000 2020",
+	"tweets": 15249,
+	"created_at": "Sun May 17 21:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 17 21:08:33 +0000 2020",
+		"id": 1261850125359440000,
+		"id_str": "1262127894693740546",
+		"full_text": "Kim Jong Un appearances in April/May period each year:\n\n2012: 48\n2013: 37\n2014: 38\n2015: 27\n2016: 25\n2017: 21\n2018: 25\n2019: 27\n2020: 4\n\nYes, May still has two more weeks to go... but the number of public appearances are EXTREMELY low ATM. \n\nThis is not business as normal, IMO.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			278
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 1607602940,
+			"id_str": "1607602940",
+			"name": "Chad O'Carroll",
+			"screen_name": "chadocl",
+			"location": "",
+			"description": "CEO of Korea Risk Group, which produces North Korea publications NK Pro and NK News. \n\nDisclaimer: I post personally. \nMy views are my own, not those of KRG.",
+			"url": "https://t.co/p2yQx0AkyB",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/p2yQx0AkyB",
+							"expanded_url": "http://www.korearisk.com",
+							"display_url": "korearisk.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14003,
+			"friends_count": 710,
+			"listed_count": 481,
+			"created_at": "Sat Jul 20 06:25:11 +0000 2013",
+			"favourites_count": 6544,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 682,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1A1B1F",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme9/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1251004064718036992/xgg2RjWp_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1251004064718036992/xgg2RjWp_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/1607602940/1423747730",
+			"profile_link_color": "ABB8C2",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 46,
+		"favorite_count": 143,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "Kim Jong Un appearances in April/May period each year:\n\n2012: 48\n2013: 37\n2014: 38\n2015: 27\n2016: 25\n2017: 21\n2018: 25\n2019: 27\n2020: 4\n\nYes, May still has two more weeks to go... but the number of public appearances are EXTREMELY low ATM. \n\nThis is not business as normal, IMO."
+	},
+	{
 		"created_at": "Sun May 17 17:22:06 +0000 2020",
 		"id": 1262070906567790600,
 		"id_str": "1262070906567790592",
