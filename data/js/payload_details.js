ab2ea8255e389e99a1c37637da5996b5466ac6bc
Matt Stiles <mattstiles@gmail.com>
2020-05-18 14:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15252,
-	"created_at": "Mon May 18 19:20:06 +0000 2020",
+	"tweets": 15253,
+	"created_at": "Mon May 18 21:20:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,200 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Mon May 18 21:09:51 +0000 2020",
+		"id": 1262490429334917000,
+		"id_str": "1262490606250541061",
+		"full_text": "Kind of like bringing Omarosa back, just to keep things interesting. https://t.co/G7zcwxExSX",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			68
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/G7zcwxExSX",
+					"expanded_url": "https://twitter.com/maggieNYT/status/1262489352174661633",
+					"display_url": "twitter.com/maggieNYT/stat…",
+					"indices": [
+						69,
+						92
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 80903194,
+			"id_str": "80903194",
+			"name": "Timothy Aeppel",
+			"screen_name": "TimAeppel",
+			"location": "New York",
+			"description": "Writer @Reuters. Formerly @WSJ. Sometimes my name autocorrects as Zeppelin.",
+			"url": "https://t.co/9tUN9lH7t6",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/9tUN9lH7t6",
+							"expanded_url": "http://www.reuters.com",
+							"display_url": "reuters.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 9466,
+			"friends_count": 930,
+			"listed_count": 368,
+			"created_at": "Thu Oct 08 17:56:15 +0000 2009",
+			"favourites_count": 4034,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 10450,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/725357699643412481/r-KflWlY_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/725357699643412481/r-KflWlY_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/80903194/1579036300",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": true,
+		"quoted_status_id": 1262489352174661600,
+		"quoted_status_id_str": "1262489352174661633",
+		"quoted_status_permalink": {
+			"url": "https://t.co/G7zcwxExSX",
+			"expanded": "https://twitter.com/maggieNYT/status/1262489352174661633",
+			"display": "twitter.com/maggieNYT/stat…"
+		},
+		"quoted_status": {
+			"created_at": "Mon May 18 21:04:52 +0000 2020",
+			"id": 1262489352174661600,
+			"id_str": "1262489352174661633",
+			"full_text": "The excitement about the reveal: “I was just waiting to see your eyes light up when I said this. When I announced this. Yeah, I've taken it for 1 and a half weeks now. And I'm still here. I'm still here,” Trump said.",
+			"truncated": false,
+			"display_text_range": [
+				0,
+				216
+			],
+			"entities": {
+				"hashtags": [],
+				"symbols": [],
+				"user_mentions": [],
+				"urls": []
+			},
+			"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+			"in_reply_to_status_id": null,
+			"in_reply_to_status_id_str": null,
+			"in_reply_to_user_id": null,
+			"in_reply_to_user_id_str": null,
+			"in_reply_to_screen_name": null,
+			"user": {
+				"id": 93069110,
+				"id_str": "93069110",
+				"name": "Maggie Haberman",
+				"screen_name": "maggieNYT",
+				"location": "",
+				"description": "White House correspondent for NYTimes, analyst for CNN. RTs don't imply agreement. maggie.haberman@nytimes.com",
+				"url": null,
+				"entities": {
+					"description": {
+						"urls": []
+					}
+				},
+				"protected": false,
+				"followers_count": 1334161,
+				"friends_count": 4318,
+				"listed_count": 15207,
+				"created_at": "Fri Nov 27 23:14:06 +0000 2009",
+				"favourites_count": 60686,
+				"utc_offset": null,
+				"time_zone": null,
+				"geo_enabled": true,
+				"verified": true,
+				"statuses_count": 232729,
+				"lang": null,
+				"contributors_enabled": false,
+				"is_translator": false,
+				"is_translation_enabled": false,
+				"profile_background_color": "C0DEED",
+				"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+				"profile_background_tile": false,
+				"profile_image_url": "http://pbs.twimg.com/profile_images/1213848155210563585/cu54X160_normal.jpg",
+				"profile_image_url_https": "https://pbs.twimg.com/profile_images/1213848155210563585/cu54X160_normal.jpg",
+				"profile_banner_url": "https://pbs.twimg.com/profile_banners/93069110/1583538786",
+				"profile_link_color": "1DA1F2",
+				"profile_sidebar_border_color": "C0DEED",
+				"profile_sidebar_fill_color": "DDEEF6",
+				"profile_text_color": "333333",
+				"profile_use_background_image": true,
+				"has_extended_profile": false,
+				"default_profile": true,
+				"default_profile_image": false,
+				"following": true,
+				"follow_request_sent": false,
+				"notifications": false,
+				"translator_type": "none"
+			},
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"contributors": null,
+			"is_quote_status": false,
+			"retweet_count": 93,
+			"favorite_count": 384,
+			"favorited": false,
+			"retweeted": false,
+			"lang": "en"
+		},
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Kind of like bringing Omarosa back, just to keep things interesting. https://t.co/G7zcwxExSX"
+	},
+	{
 		"created_at": "Mon May 18 18:25:24 +0000 2020",
 		"id": 1262083583893803000,
 		"id_str": "1262449220729987076",
