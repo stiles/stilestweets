fd9850b8b791505891e302ac4f7ff15103ada299
Matt Stiles <mattstiles@gmail.com>
2020-05-21 19:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15268,
-	"created_at": "Fri May 22 00:20:07 +0000 2020",
+	"tweets": 15269,
+	"created_at": "Fri May 22 02:20:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,123 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Fri May 22 02:08:38 +0000 2020",
+		"id": 1263652326293688300,
+		"id_str": "1263652961957187590",
+		"full_text": "Just a reminder that LA County residents can get free access to the @Headspace meditation/mindfulness app for the rest of the year. It didn't stop my mind from racing at 3a but I did find it soothing. https://t.co/zvaCXADjU5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			224
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "Headspace",
+					"name": "Headspace",
+					"id": 402025521,
+					"id_str": "402025521",
+					"indices": [
+						68,
+						78
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/zvaCXADjU5",
+					"expanded_url": "https://bit.ly/3gdvD5y",
+					"display_url": "bit.ly/3gdvD5y",
+					"indices": [
+						201,
+						224
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 301189264,
+			"id_str": "301189264",
+			"name": "Claudia Peschiutta",
+			"screen_name": "ReporterClaudia",
+			"location": "Los Angeles",
+			"description": "News with a side of humor. I work @KNX1070. Hablo español. RTs are not endorsements. Tips? DM me.",
+			"url": "https://t.co/nJac11hlei",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/nJac11hlei",
+							"expanded_url": "http://knx1070.com",
+							"display_url": "knx1070.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 10178,
+			"friends_count": 1440,
+			"listed_count": 366,
+			"created_at": "Thu May 19 01:33:28 +0000 2011",
+			"favourites_count": 15681,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 36664,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "B2DFDA",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme13/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1106781873223553025/BOU1Q9Ij_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1106781873223553025/BOU1Q9Ij_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/301189264/1407910303",
+			"profile_link_color": "93A644",
+			"profile_sidebar_border_color": "EEEEEE",
+			"profile_sidebar_fill_color": "FFFFFF",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just a reminder that LA County residents can get free access to the @Headspace meditation/mindfulness app for the rest of the year. It didn't stop my mind from racing at 3a but I did find it soothing. https://t.co/zvaCXADjU5"
+	},
+	{
 		"created_at": "Fri May 22 00:07:06 +0000 2020",
 		"id": 1263619267083157500,
 		"id_str": "1263622378271264775",
