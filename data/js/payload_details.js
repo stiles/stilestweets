cc4d6b3097c79f319ec9d25eebbc9a5cdd709732
Matt Stiles <mattstiles@gmail.com>
2020-05-31 14:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15314,
-	"created_at": "Sun May 31 20:20:08 +0000 2020",
+	"tweets": 15315,
+	"created_at": "Sun May 31 21:20:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_05.js
+++ data/js/tweets/2020_05.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_05 = 
 [
 	{
+		"created_at": "Sun May 31 21:12:02 +0000 2020",
+		"id": 1267155051287416800,
+		"id_str": "1267202200461172741",
+		"full_text": "If you’ve been out protesting in Los Angeles, I’d like to talk to you about how you weighed the COVID-19 risk. Please let me know if you’re up for an interview at Emily.alpert@latimes.com or via DM here.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			203
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 25458792,
+			"id_str": "25458792",
+			"name": "Emily Alpert Reyes",
+			"screen_name": "AlpertReyes",
+			"location": "Los Angeles",
+			"description": "L.A.Times reporter covering L.A. city government and politics. DMs are open or reach me at emily.alpert@latimes.com. I'm on furlough Friday, May 29.",
+			"url": "https://t.co/ifWgDURyxw",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/ifWgDURyxw",
+							"expanded_url": "http://www.latimes.com",
+							"display_url": "latimes.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11583,
+			"friends_count": 3031,
+			"listed_count": 526,
+			"created_at": "Fri Mar 20 04:19:07 +0000 2009",
+			"favourites_count": 3981,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 3598,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/955240465175003137/XQS_1ax3_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/955240465175003137/XQS_1ax3_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/25458792/1496177808",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 66,
+		"favorite_count": 145,
+		"favorited": false,
+		"retweeted": true,
+		"lang": "en",
+		"text": "If you’ve been out protesting in Los Angeles, I’d like to talk to you about how you weighed the COVID-19 risk. Please let me know if you’re up for an interview at Emily.alpert@latimes.com or via DM here."
+	},
+	{
 		"created_at": "Sun May 31 19:56:53 +0000 2020",
 		"id": 1267183286771581000,
 		"id_str": "1267183286771580933",
