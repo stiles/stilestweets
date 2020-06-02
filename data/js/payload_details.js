1c2e0637f6bab7dc28a194899ec4860e939a7254
Matt Stiles <mattstiles@gmail.com>
2020-06-01 17:20:05 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15321,
-	"created_at": "Mon Jun 01 22:20:03 +0000 2020",
+	"tweets": 15322,
+	"created_at": "Tue Jun 02 00:20:03 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 01 23:56:06 +0000 2020",
+		"id": 1267605875943850000,
+		"id_str": "1267605875943849984",
+		"full_text": "(Or at least where protests were planned). \n\nSame over Van Nuys: https://t.co/p0ajqJgJ9X https://t.co/pI2PRjMDfQ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			88
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/p0ajqJgJ9X",
+					"expanded_url": "https://www.flightradar24.com/N662PD/249c9fe3",
+					"display_url": "flightradar24.com/N662PD/249c9fe3",
+					"indices": [
+						65,
+						88
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1267605589644861400,
+					"id_str": "1267605589644861440",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdwkVYU4AAn1oG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZdwkVYU4AAn1oG.jpg",
+					"url": "https://t.co/pI2PRjMDfQ",
+					"display_url": "pic.twitter.com/pI2PRjMDfQ",
+					"expanded_url": "https://twitter.com/stiles/status/1267605875943849984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1858,
+							"h": 976,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1267605589644861400,
+					"id_str": "1267605589644861440",
+					"indices": [
+						89,
+						112
+					],
+					"media_url": "http://pbs.twimg.com/media/EZdwkVYU4AAn1oG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EZdwkVYU4AAn1oG.jpg",
+					"url": "https://t.co/pI2PRjMDfQ",
+					"display_url": "pic.twitter.com/pI2PRjMDfQ",
+					"expanded_url": "https://twitter.com/stiles/status/1267605875943849984/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1858,
+							"h": 976,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": 1267604889980399600,
+		"in_reply_to_status_id_str": "1267604889980399616",
+		"in_reply_to_user_id": 14790314,
+		"in_reply_to_user_id_str": "14790314",
+		"in_reply_to_screen_name": "stiles",
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. Stories about LA & California with data, visuals & code. Past @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics).",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11467,
+			"friends_count": 2096,
+			"listed_count": 857,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8544,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15802,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1590888911",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "(Or at least where protests were planned). \n\nSame over Van Nuys: https://t.co/p0ajqJgJ9X https://t.co/pI2PRjMDfQ"
+	},
+	{
 		"created_at": "Mon Jun 01 21:28:41 +0000 2020",
 		"id": 1267568775693398000,
 		"id_str": "1267568775693398016",
