7a2e857d9249c4e169f2c5f874511c7da4db504f
Matt Stiles <mattstiles@gmail.com>
2020-06-15 10:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15402,
-	"created_at": "Mon Jun 15 14:20:06 +0000 2020",
+	"tweets": 15403,
+	"created_at": "Mon Jun 15 17:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,213 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 15 16:29:14 +0000 2020",
+		"id": 1272566847154446300,
+		"id_str": "1272566847154446341",
+		"full_text": "Oh, good morning, Porto! We are thankful you have nice waves (and open parking lot) this morning. https://t.co/Cuz5zqo7Vk",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			97
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1272566839344681000,
+					"id_str": "1272566839344680971",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EakQzEwU0AsXclC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EakQzEwU0AsXclC.jpg",
+					"url": "https://t.co/Cuz5zqo7Vk",
+					"display_url": "pic.twitter.com/Cuz5zqo7Vk",
+					"expanded_url": "https://twitter.com/stiles/status/1272566847154446341/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 996,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1699,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 564,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1272566839344681000,
+					"id_str": "1272566839344680971",
+					"indices": [
+						98,
+						121
+					],
+					"media_url": "http://pbs.twimg.com/media/EakQzEwU0AsXclC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EakQzEwU0AsXclC.jpg",
+					"url": "https://t.co/Cuz5zqo7Vk",
+					"display_url": "pic.twitter.com/Cuz5zqo7Vk",
+					"expanded_url": "https://twitter.com/stiles/status/1272566847154446341/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 996,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1699,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 564,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/android\" rel=\"nofollow\">Twitter for Android</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. @datagraphics adjacent. LA & CA stories w/ data, visuals & code. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics)",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11463,
+			"friends_count": 2123,
+			"listed_count": 857,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8695,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15905,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1590888911",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": {
+			"id": "07d9f31c56088003",
+			"url": "https://api.twitter.com/1.1/geo/id/07d9f31c56088003.json",
+			"place_type": "poi",
+			"name": "El Porto Beach",
+			"full_name": "El Porto Beach",
+			"country_code": "US",
+			"country": "United States",
+			"contained_within": [],
+			"bounding_box": {
+				"type": "Polygon",
+				"coordinates": [
+					[
+						[
+							-118.42137891801008,
+							33.904107722711345
+						],
+						[
+							-118.42137891801008,
+							33.904107722711345
+						],
+						[
+							-118.42137891801008,
+							33.904107722711345
+						],
+						[
+							-118.42137891801008,
+							33.904107722711345
+						]
+					]
+				]
+			},
+			"attributes": {}
+		},
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Oh, good morning, Porto! We are thankful you have nice waves (and open parking lot) this morning. https://t.co/Cuz5zqo7Vk"
+	},
+	{
 		"created_at": "Mon Jun 15 13:59:33 +0000 2020",
 		"id": 1272529179485978600,
 		"id_str": "1272529179485978625",
