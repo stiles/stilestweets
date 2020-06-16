573c69debfe4875fa04099d14c783ead5ce4222c
Matt Stiles <mattstiles@gmail.com>
2020-06-16 07:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15408,
-	"created_at": "Tue Jun 16 02:20:05 +0000 2020",
+	"tweets": 15409,
+	"created_at": "Tue Jun 16 14:20:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,191 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Tue Jun 16 13:43:53 +0000 2020",
+		"id": 1272887625028735000,
+		"id_str": "1272887625028734976",
+		"full_text": "Thanks, @CodeGrepper https://t.co/Qv3rO28xXB",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			20
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "CodeGrepper",
+					"name": "Grepper",
+					"id": 1172589427203297300,
+					"id_str": "1172589427203297280",
+					"indices": [
+						8,
+						20
+					]
+				}
+			],
+			"urls": [],
+			"media": [
+				{
+					"id": 1272887361664360400,
+					"id_str": "1272887361664360452",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/Eao0T7cWoAQiZdB.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eao0T7cWoAQiZdB.png",
+					"url": "https://t.co/Qv3rO28xXB",
+					"display_url": "pic.twitter.com/Qv3rO28xXB",
+					"expanded_url": "https://twitter.com/stiles/status/1272887625028734976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 325,
+							"h": 255,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 325,
+							"h": 255,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 325,
+							"h": 255,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1272887361664360400,
+					"id_str": "1272887361664360452",
+					"indices": [
+						21,
+						44
+					],
+					"media_url": "http://pbs.twimg.com/media/Eao0T7cWoAQiZdB.png",
+					"media_url_https": "https://pbs.twimg.com/media/Eao0T7cWoAQiZdB.png",
+					"url": "https://t.co/Qv3rO28xXB",
+					"display_url": "pic.twitter.com/Qv3rO28xXB",
+					"expanded_url": "https://twitter.com/stiles/status/1272887625028734976/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 325,
+							"h": 255,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 325,
+							"h": 255,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 325,
+							"h": 255,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. @datagraphics adjacent. LA & CA stories w/ data, visuals & code. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics)",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11463,
+			"friends_count": 2123,
+			"listed_count": 857,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8692,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15905,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1590888911",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thanks, @CodeGrepper https://t.co/Qv3rO28xXB"
+	},
+	{
 		"created_at": "Tue Jun 16 02:02:34 +0000 2020",
 		"id": 1272711134290645000,
 		"id_str": "1272711134290644992",
