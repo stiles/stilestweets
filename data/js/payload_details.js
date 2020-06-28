b0dd888e3b138abdf33aa8aa455f144571aab538
Matt Stiles <mattstiles@gmail.com>
2020-06-28 13:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15479,
-	"created_at": "Sun Jun 28 17:20:06 +0000 2020",
+	"tweets": 15480,
+	"created_at": "Sun Jun 28 20:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Sun Jun 28 19:56:58 +0000 2020",
+		"id": 1277330168621568000,
+		"id_str": "1277330168621568000",
+		"full_text": "Here we go. Again. \nhttps://t.co/k7H2TFTItH https://t.co/Wboq7cM1UW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			43
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/k7H2TFTItH",
+					"expanded_url": "https://www.latimes.com/california/story/2020-06-28/gavin-newsom-orders-bars-closed-in-7-california-counties-due-to-coronavirus-spread",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						20,
+						43
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1277330013730168800,
+					"id_str": "1277330013730168832",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/Ebn84UFU4AAR-kE.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ebn84UFU4AAR-kE.png",
+					"url": "https://t.co/Wboq7cM1UW",
+					"display_url": "pic.twitter.com/Wboq7cM1UW",
+					"expanded_url": "https://twitter.com/stiles/status/1277330168621568000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 530,
+							"h": 192,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 192,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 530,
+							"h": 192,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1277330013730168800,
+					"id_str": "1277330013730168832",
+					"indices": [
+						44,
+						67
+					],
+					"media_url": "http://pbs.twimg.com/media/Ebn84UFU4AAR-kE.png",
+					"media_url_https": "https://pbs.twimg.com/media/Ebn84UFU4AAR-kE.png",
+					"url": "https://t.co/Wboq7cM1UW",
+					"display_url": "pic.twitter.com/Wboq7cM1UW",
+					"expanded_url": "https://twitter.com/stiles/status/1277330168621568000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 530,
+							"h": 192,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 530,
+							"h": 192,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 530,
+							"h": 192,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. @datagraphics adjacent. LA & CA stories w/ data, visuals & code. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics)",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11478,
+			"friends_count": 2137,
+			"listed_count": 859,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8899,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15990,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1590888911",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Here we go. Again. \nhttps://t.co/k7H2TFTItH https://t.co/Wboq7cM1UW"
+	},
+	{
 		"created_at": "Sun Jun 28 16:34:24 +0000 2020",
 		"id": 1277279189163364400,
 		"id_str": "1277279189163364354",
