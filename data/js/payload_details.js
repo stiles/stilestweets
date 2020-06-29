5aaddfd3b2dd0446541c04bca4a0e65c0e0d37c7
Matt Stiles <mattstiles@gmail.com>
2020-06-29 14:20:10 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15484,
-	"created_at": "Mon Jun 29 20:20:06 +0000 2020",
+	"tweets": 15485,
+	"created_at": "Mon Jun 29 21:20:08 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_06.js
+++ data/js/tweets/2020_06.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2020_06 = 
 [
 	{
+		"created_at": "Mon Jun 29 20:44:31 +0000 2020",
+		"id": 1277704522886836200,
+		"id_str": "1277704522886836224",
+		"full_text": "As social unrest swept L.A., a curious trend emerged: Calls to LAPD plunged. \n\nMy latest: https://t.co/3sXQrnlNfj https://t.co/tmrvvKF6UJ",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			113
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3sXQrnlNfj",
+					"expanded_url": "https://www.latimes.com/california/story/2020-06-29/protests-police-calls-plunge",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						90,
+						113
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1277703925345312800,
+					"id_str": "1277703925345312769",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/EbtQ81ZUYAEyXBJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/EbtQ81ZUYAEyXBJ.png",
+					"url": "https://t.co/tmrvvKF6UJ",
+					"display_url": "pic.twitter.com/tmrvvKF6UJ",
+					"expanded_url": "https://twitter.com/stiles/status/1277704522886836224/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 696,
+							"h": 496,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 696,
+							"h": 496,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1277703925345312800,
+					"id_str": "1277703925345312769",
+					"indices": [
+						114,
+						137
+					],
+					"media_url": "http://pbs.twimg.com/media/EbtQ81ZUYAEyXBJ.png",
+					"media_url_https": "https://pbs.twimg.com/media/EbtQ81ZUYAEyXBJ.png",
+					"url": "https://t.co/tmrvvKF6UJ",
+					"display_url": "pic.twitter.com/tmrvvKF6UJ",
+					"expanded_url": "https://twitter.com/stiles/status/1277704522886836224/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 485,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 696,
+							"h": 496,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 696,
+							"h": 496,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://mobile.twitter.com\" rel=\"nofollow\">Twitter Web App</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 14790314,
+			"id_str": "14790314",
+			"name": "Matt Stiles",
+			"screen_name": "stiles",
+			"location": "Los Angeles",
+			"description": "Reporter, @latimes. @datagraphics adjacent. LA & CA stories w/ data, visuals & code. Past: @latimes (Koreas), @wsj (econ), @NPR (viz), @TexasTribune (politics)",
+			"url": "https://t.co/PJEacYfCaJ",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/PJEacYfCaJ",
+							"expanded_url": "http://mattstiles.me",
+							"display_url": "mattstiles.me",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 11473,
+			"friends_count": 2140,
+			"listed_count": 859,
+			"created_at": "Thu May 15 19:12:30 +0000 2008",
+			"favourites_count": 8911,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 15994,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "030F1B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme8/bg.gif",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1241140089914179585/_X4i0NXe_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/14790314/1590888911",
+			"profile_link_color": "B12E35",
+			"profile_sidebar_border_color": "3268C2",
+			"profile_sidebar_fill_color": "A9D4FF",
+			"profile_text_color": "6E6E6E",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 37,
+		"favorite_count": 74,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "As social unrest swept L.A., a curious trend emerged: Calls to LAPD plunged. \n\nMy latest: https://t.co/3sXQrnlNfj https://t.co/tmrvvKF6UJ"
+	},
+	{
 		"created_at": "Mon Jun 29 19:55:12 +0000 2020",
 		"id": 1277692111945470000,
 		"id_str": "1277692111945469952",
