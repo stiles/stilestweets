42949f6a595b30d90b8e12a0b8a65d142ee0ca26
Matt Stiles <mattstiles@gmail.com>
2020-07-01 08:20:08 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15492,
-	"created_at": "Tue Jun 30 21:20:08 +0000 2020",
+	"tweets": 15493,
+	"created_at": "Wed Jul 01 15:20:06 +0000 2020",
 	"lang": "en"
 }
---
+++ data/js/tweets/2020_07.js
@@ -0,0 +1,97 @@
+Grailbird.data.tweets_2020_07 = 
+[
+	{
+		"created_at": "Wed Jul 01 15:01:28 +0000 2020",
+		"id": 1278333358393696300,
+		"id_str": "1278342965530779651",
+		"full_text": "The books specifically about South Korea in my head are:\n\n- History of K-pop (am writing this now)\n- S Korea's Democracy Movement\n- Korea in the inter-war period (from 1945 to 55)\n- Modern development of Seoul\n- History of specific Korean dishes (like kimchi or bibimbap)",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": 1278332105265684500,
+		"in_reply_to_status_id_str": "1278332105265684480",
+		"in_reply_to_user_id": 266269231,
+		"in_reply_to_user_id_str": "266269231",
+		"in_reply_to_screen_name": "AskAKorean",
+		"user": {
+			"id": 266269231,
+			"id_str": "266269231",
+			"name": "T.K. of AAK!",
+			"screen_name": "AskAKorean",
+			"location": "Eastern",
+			"description": "Blogger for Ask a Korean!",
+			"url": "https://t.co/2UhjoLCVhK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/2UhjoLCVhK",
+							"expanded_url": "http://www.askakorean.net",
+							"display_url": "askakorean.net",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 22481,
+			"friends_count": 3328,
+			"listed_count": 382,
+			"created_at": "Mon Mar 14 23:43:47 +0000 2011",
+			"favourites_count": 25244,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": false,
+			"statuses_count": 1013,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1273995831226650624/BysuHXeO_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1273995831226650624/BysuHXeO_normal.jpg",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 5,
+		"favorite_count": 33,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "The books specifically about South Korea in my head are:\n\n- History of K-pop (am writing this now)\n- S Korea's Democracy Movement\n- Korea in the inter-war period (from 1945 to 55)\n- Modern development of Seoul\n- History of specific Korean dishes (like kimchi or bibimbap)"
+	}
+]
