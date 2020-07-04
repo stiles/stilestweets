d2233fba0efa9fe994f67f9adfeebd8c1c2967dc
Matt Stiles <mattstiles@gmail.com>
2020-07-04 10:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15515,
-	"created_at": "Sat Jul 04 16:20:04 +0000 2020",
+	"tweets": 15516,
+	"created_at": "Sat Jul 04 17:20:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,101 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sat Jul 04 16:48:36 +0000 2020",
+		"id": 1278907728388329500,
+		"id_str": "1279457092772880384",
+		"full_text": "On the other hand, Parents who think I’m a bad parent becuase sometimes I don’t like being a parent can, I don’t know, [fuck off] maybe.",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			136
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": []
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": 1278897149242114000,
+		"in_reply_to_status_id_str": "1278897149242114048",
+		"in_reply_to_user_id": 40353407,
+		"in_reply_to_user_id_str": "40353407",
+		"in_reply_to_screen_name": "ElieNYC",
+		"user": {
+			"id": 40353407,
+			"id_str": "40353407",
+			"name": "Elie Mystal",
+			"screen_name": "ElieNYC",
+			"location": "NYC",
+			"description": "Justice Correspondent: @thenation Alignment: Neutral Good. Str:12 Dex:8 Con:15 Int:13 Wis:10 Char:14 Class: Paladin. Strong Against: Republicans.",
+			"url": "https://t.co/cr4eb6JcYl",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/cr4eb6JcYl",
+							"expanded_url": "https://www.thenation.com/authors/elie-mystal/",
+							"display_url": "thenation.com/authors/elie-m…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 144604,
+			"friends_count": 3474,
+			"listed_count": 1336,
+			"created_at": "Fri May 15 22:47:11 +0000 2009",
+			"favourites_count": 39859,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 30193,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "1F2323",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1156046777390456833/ooPBpYnJ_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1156046777390456833/ooPBpYnJ_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/40353407/1506879032",
+			"profile_link_color": "0084B4",
+			"profile_sidebar_border_color": "BDDCAD",
+			"profile_sidebar_fill_color": "DDFFCC",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 73,
+		"favorite_count": 3232,
+		"favorited": true,
+		"retweeted": true,
+		"lang": "en",
+		"text": "On the other hand, Parents who think I’m a bad parent becuase sometimes I don’t like being a parent can, I don’t know, [fuck off] maybe."
+	},
+	{
 		"created_at": "Sat Jul 04 16:03:08 +0000 2020",
 		"id": 1279445651298771000,
 		"id_str": "1279445651298770944",
