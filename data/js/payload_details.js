258d854a29a8658077d6484aa0c48345cb1e6295
Matt Stiles <mattstiles@gmail.com>
2020-07-05 07:20:05 -0700

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15520,
-	"created_at": "Sun Jul 05 08:20:04 +0000 2020",
+	"tweets": 15522,
+	"created_at": "Sun Jul 05 14:20:04 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,240 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Sun Jul 05 14:06:29 +0000 2020",
+		"id": 1279766928005816300,
+		"id_str": "1279778682853187588",
+		"full_text": "“You started the #corona!” Yes, numbers are exploding. I still know five people afraid to file anti-Asian hate incident reports so it’s crucial to write about those willing to talk about being targets in this climate of wild #abuse and #fear. My story: https://t.co/NzcR18zmfa",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "corona",
+					"indices": [
+						17,
+						24
+					]
+				},
+				{
+					"text": "abuse",
+					"indices": [
+						225,
+						231
+					]
+				},
+				{
+					"text": "fear",
+					"indices": [
+						236,
+						241
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NzcR18zmfa",
+					"expanded_url": "https://www.latimes.com/california/story/2020-07-05/anti-asian-hate-newsom-help",
+					"display_url": "latimes.com/california/sto…",
+					"indices": [
+						253,
+						276
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 73708365,
+			"id_str": "73708365",
+			"name": "ANH DO",
+			"screen_name": "newsterrier",
+			"location": "California",
+			"description": "@LATimes Metro reporter. I dig #foodies, #travel, #dogs of all #stripes, #animal #rescue. Life = art. Storytelling = value.",
+			"url": "https://t.co/b69cSsRCZy",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/b69cSsRCZy",
+							"expanded_url": "http://www.latimes.com/la-bio-anh-do-staff.html",
+							"display_url": "latimes.com/la-bio-anh-do-…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 2377,
+			"friends_count": 549,
+			"listed_count": 134,
+			"created_at": "Sat Sep 12 18:54:09 +0000 2009",
+			"favourites_count": 672,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": false,
+			"verified": true,
+			"statuses_count": 9902,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "8B542B",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": true,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1144295610654191616/VSBiSD1t_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1144295610654191616/VSBiSD1t_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/73708365/1538936436",
+			"profile_link_color": "4A913C",
+			"profile_sidebar_border_color": "FFFFFF",
+			"profile_sidebar_fill_color": "E1E8A9",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“You started the #corona!” Yes, numbers are exploding. I still know five people afraid to file anti-Asian hate incident reports so it’s crucial to write about those willing to talk about being targets in this climate of wild #abuse and #fear. My story: https://t.co/NzcR18zmfa"
+	},
+	{
+		"created_at": "Sun Jul 05 13:45:39 +0000 2020",
+		"id": 1279437826006933500,
+		"id_str": "1279773437909544960",
+		"full_text": "In Lubbock, TX, coronavirus cases are spiking. \n\n23 members of Texas Tech's football team tested positive.\n\nThe mayor is avoiding his office after his daughter tested positive, to keep from possibly exposing staff. \n\nStill, some don't believe it's a risk:\n\nhttps://t.co/OerW76dWxY",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			280
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/OerW76dWxY",
+					"expanded_url": "https://www.nytimes.com/2020/07/04/us/coronavirus-texas-lubbock.html",
+					"display_url": "nytimes.com/2020/07/04/us/…",
+					"indices": [
+						257,
+						280
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://about.twitter.com/products/tweetdeck\" rel=\"nofollow\">TweetDeck</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 16395525,
+			"id_str": "16395525",
+			"name": "J. David Goodman",
+			"screen_name": "jdavidgoodman",
+			"location": "New York City",
+			"description": "I cover money in politics for The New York Times. “Seriously my friend?\" - Mayor Bill de Blasio",
+			"url": "https://t.co/Mk5oLDWaEG",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/Mk5oLDWaEG",
+							"expanded_url": "http://topics.nytimes.com/top/reference/timestopics/people/g/j_david_goodman/index.html",
+							"display_url": "topics.nytimes.com/top/reference/…",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 14505,
+			"friends_count": 1115,
+			"listed_count": 599,
+			"created_at": "Sun Sep 21 21:43:07 +0000 2008",
+			"favourites_count": 1830,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 13067,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1108367478089023489/aRnEjPiy_normal.png",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1108367478089023489/aRnEjPiy_normal.png",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/16395525/1472829503",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": false,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 36,
+		"favorite_count": 59,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In Lubbock, TX, coronavirus cases are spiking. \n\n23 members of Texas Tech's football team tested positive.\n\nThe mayor is avoiding his office after his daughter tested positive, to keep from possibly exposing staff. \n\nStill, some don't believe it's a risk:\n\nhttps://t.co/OerW76dWxY"
+	},
+	{
 		"created_at": "Sun Jul 05 08:01:38 +0000 2020",
 		"id": 1279669621432320000,
 		"id_str": "1279686864849367042",
