96567727bb711e4ea1b3badb04f866340180b66d
Matt Stiles <mattstiles@gmail.com>
2020-07-14 08:20:06 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15567,
-	"created_at": "Mon Jul 13 21:20:05 +0000 2020",
+	"tweets": 15568,
+	"created_at": "Tue Jul 14 15:20:05 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,122 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Tue Jul 14 14:42:42 +0000 2020",
+		"id": 1283048368311357400,
+		"id_str": "1283049285391654912",
+		"full_text": "Joe Biden just hit 92% to win the election in our model. We project he’ll win the popular vote by 8 points (with a margin of error of 6) and 350 electoral votes (+/- 85). This is not 2016, folks; we gave Clinton a ~65% chance at this point last time.\n\nhttps://t.co/O6Lknvo6Kp",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/O6Lknvo6Kp",
+					"expanded_url": "https://projects.economist.com/us-2020-forecast/president",
+					"display_url": "projects.economist.com/us-2020-foreca…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/#!/download/ipad\" rel=\"nofollow\">Twitter for iPad</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 57029492,
+			"id_str": "57029492",
+			"name": "G. Elliott Morris",
+			"screen_name": "gelliottmorris",
+			"location": "Washington, DC",
+			"description": "Data-driven journalist @TheEconomist. Writing a book about polls and democracy @wwnorton (out fall 2021). Blog/newsletter: https://t.co/jhDR4O2kWn.",
+			"url": "https://t.co/zlUGH48jh4",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/zlUGH48jh4",
+							"expanded_url": "http://gelliottmorris.com",
+							"display_url": "gelliottmorris.com",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": [
+						{
+							"url": "https://t.co/jhDR4O2kWn",
+							"expanded_url": "http://thecrosstab.substack.com",
+							"display_url": "thecrosstab.substack.com",
+							"indices": [
+								123,
+								146
+							]
+						}
+					]
+				}
+			},
+			"protected": false,
+			"followers_count": 55407,
+			"friends_count": 467,
+			"listed_count": 1515,
+			"created_at": "Wed Jul 15 14:24:06 +0000 2009",
+			"favourites_count": 33386,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 40658,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme15/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/1261405247345491969/dAMVuJ5d_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/1261405247345491969/dAMVuJ5d_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/57029492/1579119881",
+			"profile_link_color": "3498DB",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": true,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 81,
+		"favorite_count": 331,
+		"favorited": false,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Joe Biden just hit 92% to win the election in our model. We project he’ll win the popular vote by 8 points (with a margin of error of 6) and 350 electoral votes (+/- 85). This is not 2016, folks; we gave Clinton a ~65% chance at this point last time.\n\nhttps://t.co/O6Lknvo6Kp"
+	},
+	{
 		"created_at": "Mon Jul 13 21:02:36 +0000 2020",
 		"id": 1282771807745962000,
 		"id_str": "1282782503405670400",
