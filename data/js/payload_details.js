3e80fc8f4968d8472879a0bbee98837b85c1753d
Matt Stiles <mattstiles@gmail.com>
2020-07-14 22:20:07 -0700

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 15578,
-	"created_at": "Wed Jul 15 04:20:44 +0000 2020",
+	"tweets": 15579,
+	"created_at": "Wed Jul 15 05:20:06 +0000 2020",
 	"lang": "en"
 }
--- data/js/tweets/2020_07.js
+++ data/js/tweets/2020_07.js
@@ -1,6 +1,175 @@
 Grailbird.data.tweets_2020_07 = 
 [
 	{
+		"created_at": "Wed Jul 15 04:31:45 +0000 2020",
+		"id": 1283228474614767600,
+		"id_str": "1283257923854954496",
+		"full_text": "I’m not sure we have enough side dishes.. #banchanjoy https://t.co/FaN84fUMEe",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			53
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "banchanjoy",
+					"indices": [
+						42,
+						53
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1283228459192291300,
+					"id_str": "1283228459192291330",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/Ec7xfBvUEAIRdCr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ec7xfBvUEAIRdCr.jpg",
+					"url": "https://t.co/FaN84fUMEe",
+					"display_url": "pic.twitter.com/FaN84fUMEe",
+					"expanded_url": "https://twitter.com/BBCLBicker/status/1283228474614767618/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1283228459192291300,
+					"id_str": "1283228459192291330",
+					"indices": [
+						54,
+						77
+					],
+					"media_url": "http://pbs.twimg.com/media/Ec7xfBvUEAIRdCr.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Ec7xfBvUEAIRdCr.jpg",
+					"url": "https://t.co/FaN84fUMEe",
+					"display_url": "pic.twitter.com/FaN84fUMEe",
+					"expanded_url": "https://twitter.com/BBCLBicker/status/1283228474614767618/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 240756474,
+			"id_str": "240756474",
+			"name": "Laura Bicker",
+			"screen_name": "BBCLBicker",
+			"location": "Seoul, Republic of Korea",
+			"description": "BBC Seoul Correspondent, former Washington Correspondent. Scots lass and lover of outdoor sports & triathlon. Views my own. RTs not endorsements.",
+			"url": null,
+			"entities": {
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 60846,
+			"friends_count": 2120,
+			"listed_count": 968,
+			"created_at": "Thu Jan 20 17:32:34 +0000 2011",
+			"favourites_count": 18159,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": true,
+			"statuses_count": 14948,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "C0DEED",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/436620937019277312/eUJwrSaX_normal.jpeg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/436620937019277312/eUJwrSaX_normal.jpeg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/240756474/1521203376",
+			"profile_link_color": "1DA1F2",
+			"profile_sidebar_border_color": "C0DEED",
+			"profile_sidebar_fill_color": "DDEEF6",
+			"profile_text_color": "333333",
+			"profile_use_background_image": true,
+			"has_extended_profile": true,
+			"default_profile": true,
+			"default_profile_image": false,
+			"following": true,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 59,
+		"favorite_count": 403,
+		"favorited": true,
+		"retweeted": true,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "I’m not sure we have enough side dishes.. #banchanjoy https://t.co/FaN84fUMEe"
+	},
+	{
 		"created_at": "Wed Jul 15 04:20:08 +0000 2020",
 		"id": 1283233300123877400,
 		"id_str": "1283255000718663681",
